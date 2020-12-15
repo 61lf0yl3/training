747ca9e97228a6c7a96de140f7cbf066abb40e77
61lf0yl3 <chingiz.samigullin97@gmail.com>
2020-12-15 15:05:40 +0600

    NumPairsDivisibleBy60

--- leetcode/decemberchallenge/numPairsDivisibleBy60.go
+++ leetcode/decemberchallenge/numPairsDivisibleBy60.go
@@ -5,19 +5,22 @@ import "fmt"
 // NumPairsDivisibleBy60 ...
 func NumPairsDivisibleBy60(time []int) int {
 	//timeleft := []int{}
-	// for i, v := range time {
-	// 	time[i] = v % 60
-	// }
+	//fmt.Println(time)
 	res := 0
-	for i1 := 0; i1 < len(time)-1; i1++ {
-		for i2 := i1 + 1; i2 < len(time); i2++ {
-			//fmt.Println(time[i1], time[i2], "\n")
-			if (time[i1]+time[i2])%60 == 0 {
-				res++
-			}
-		}
+	m := make(map[int]int)
+	for _, v := range time {
+		//time[i] = v % 60
+		m[v%60]++
 	}
+	fmt.Println(m)
+	// for i1 := 0; i1 < len(time)-1; i1++ {
+	// 	for i2 := i1 + 1; i2 < len(time); i2++ {
+	// 		//fmt.Println(time[i1], time[i2], "\n")
+	// 		if (time[i1]+time[i2])%60 == 0 {
+	// 			res++
+	// 		}
+	// 	}
+	// }
 
-	fmt.Println(time)
 	return res
 }
