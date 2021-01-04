8f9703861f40a1a6a628d7c0d51fb66101952e4a
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-01-04 11:06:13 +0600

    CountArrangement

--- leetcode/januarychallenge/countArrangement.go
+++ leetcode/januarychallenge/countArrangement.go
@@ -1,14 +1,38 @@
 package januarychallenge
 
 // CountArrangement ..
-func CountArrangement(n int) int {
-	// res := n
-	// for i := n / 2; i > 1; i-- {
-	// 	res *= i
-	// }
-	// return res
-	res := []int{1, 2, 3, 8, 10, 36, 41, 132, 250, 700, 750, 4010, 4237, 10680, 24679}
-	return res[n-1]
+func CountArrangement(N int) int {
+	//res := []int{1, 2, 3, 8, 10, 36, 41, 132, 250, 700, 750, 4010, 4237, 10680, 24679}
+	// return res[n-1]
+
+	memo := make([][]int, N)
+	for i := range memo {
+		memo[i] = make([]int, 1<<N)
+		for j := range memo[i] {
+			memo[i][j] = -1
+		}
+	}
+	return helper(N, 0, 0, memo)
+}
+
+func helper(n, i, chosen int, memo [][]int) int {
+	if i == n {
+		return 1
+	}
+	if memo[i][chosen] != -1 {
+		return memo[i][chosen]
+	}
+	ans := 0
+	for d := 0; d < n; d++ {
+		if chosen&(1<<d) != 0 {
+			continue
+		}
+		if (d+1)%(i+1) == 0 || (i+1)%(d+1) == 0 {
+			ans += helper(n, i+1, chosen|(1<<d), memo)
+		}
+	}
+	memo[i][chosen] = ans
+	return ans
 }
 
 // func Factorial(n int) int {
