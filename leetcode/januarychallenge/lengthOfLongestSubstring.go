4537b603ebe5c8be10e8dcf661cfe144126522dd
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-01-08 12:12:26 +0600

    LengthOfLongestSubstring

--- leetcode/januarychallenge/lengthOfLongestSubstring.go
+++ leetcode/januarychallenge/lengthOfLongestSubstring.go
@@ -1,16 +1,41 @@
 package januarychallenge
 
-func LengthOfLongestSubstring(s string) int {
-	ascii := make([]int, 127)
-	counter1 := 0
-	for i := 0; i < len(s); i++ {
-		if ascii[int(rune(s[i]))] == 1 {
+//my own sollution
+// func LengthOfLongestSubstring(s string) int {
+// 	ascii := make([]int, 127)
+// 	counter1 := 0
+// 	for i := 0; i < len(s); i++ {
+// 		if ascii[int(rune(s[i]))] == 1 {
 
-		} else if ascii[int(rune(s[i]))] == 0 {
-			ascii[int(rune(s[i]))] = 1
-			counter1++
+// 		} else if ascii[int(rune(s[i]))] == 0 {
+// 			ascii[int(rune(s[i]))] = 1
+// 			counter1++
+// 		}
+
+// 	}
+// 	return counter1
+// }
+
+// LengthOfLongestSubstring my own sollution from leetcode
+func LengthOfLongestSubstring(s string) int {
+	start := 0
+	res := 0
+	m := make(map[byte]int)
+	for i := 0; i < len(s); i++ {
+		end := i
+		elem, ok := m[s[i]]
+		if !ok {
+			m[s[i]] = i
+		} else if ok {
+			if elem+1 >= start {
+				start = elem + 1
+			}
+			m[s[i]] = i
+		}
+		if res < end-start+1 {
+			res = end - start + 1
 		}
 
 	}
-	return counter1
+	return res
 }
