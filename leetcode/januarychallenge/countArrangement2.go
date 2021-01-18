1fe91931259eca5d26d84536808b93b22d11f38a
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-01-18 13:03:36 +0600

    .

--- leetcode/januarychallenge/countArrangement2.go
+++ leetcode/januarychallenge/countArrangement2.go
@@ -2,29 +2,30 @@ package januarychallenge
 
 var count int
 
-func countArrangement(n int) int {
+// CountArrangement ...
+func CountArrangement(n int) int {
 	arr := make([]int, n)
 	for i := 0; i < n; i++ {
 		arr[i] = i + 1
 	}
-	check(arr, 0)
+	Check(arr, 0)
 
 	return count
 }
 
-func check(arr []int, j int) {
+func Check(arr []int, j int) {
 	if j == len(arr) {
 		count++
 	}
 	for i := j; i < len(arr); i++ {
-		swap(arr, i, j)
+		Swap(arr, i, j)
 		if (arr[j]%(j+1) == 0) || ((j+1)%(arr[j]) == 0) {
-			check(arr, j+1)
+			Check(arr, j+1)
 		}
-		swap(arr, i, j)
+		Swap(arr, i, j)
 	}
 }
 
-func swap(arr []int, i int, j int) {
+func Swap(arr []int, i int, j int) {
 	arr[i], arr[j] = arr[j], arr[i]
 }
