06aca92d6215ab1b08fb71a5be0abb3598ab762c
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-01-23 19:57:55 +0600

    .

--- leetcode/decemberchallenge/canPlaceFlowers.go
+++ leetcode/decemberchallenge/canPlaceFlowers.go
@@ -1,24 +1,24 @@
 package decemberchallenge
 
 //CanPlaceFlowers ..
-func CanPlaceFlowers(flowerbed []int, n int) bool {
-	for i := 0; i < len(flowerbed); i++ {
-		if len(flowerbed) > 2 {
+// func CanPlaceFlowers(flowerbed []int, n int) bool {
+// 	for i := 0; i < len(flowerbed); i++ {
+// 		if len(flowerbed) > 2 {
 
-			if i == 0 && flowerbed[i] == 0 && flowerbed[i+1] == 0 {
-				n--
-			}
-			if i > 0 && flowerbed[i] == 0 && flowerbed[i+1] == 0 && flowerbed[i-1] == 0 {
-				n--
-				i++
-			}
-			if i == len(flowerbed)-2 && flowerbed[i] == 0 && flowerbed[i+1] == 0 {
-				n--
-			}
-			if n == 0 {
-				return true
-			}
-		}
-	}
-	return false
-}
+// 			if i == 0 && flowerbed[i] == 0 && flowerbed[i+1] == 0 {
+// 				n--
+// 			}
+// 			if i > 0 && flowerbed[i] == 0 && flowerbed[i+1] == 0 && flowerbed[i-1] == 0 {
+// 				n--
+// 				i++
+// 			}
+// 			if i == len(flowerbed)-2 && flowerbed[i] == 0 && flowerbed[i+1] == 0 {
+// 				n--
+// 			}
+// 			if n == 0 {
+// 				return true
+// 			}
+// 		}
+// 	}
+// 	return false
+// }
