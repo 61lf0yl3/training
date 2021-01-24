a76eb3adb9c0b01fab7ce9f9783b3700f8795a31
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-01-24 23:04:07 +0600

    .

--- leetcode/januarychallenge/lengthOfLongestSubstring.go
+++ leetcode/januarychallenge/lengthOfLongestSubstring.go
@@ -16,28 +16,28 @@ func LengthOfLongestSubstring1(s string) int {
 	return counter1
 }
 
-// LengthOfLongestSubstring my own solution2
-// func LengthOfLongestSubstring(s string) int {
-// 	start := 0
-// 	res := 0
-// 	m := make(map[byte]int)
-// 	for i := 0; i < len(s); i++ {
-// 		end := i
-// 		elem, ok := m[s[i]]
-// 		if !ok {
-// 			m[s[i]] = i
-// 		} else if ok {
-// 			if elem+1 >= start {
-// 				start = elem + 1
-// 			}
-// 			m[s[i]] = i
-// 		}
-// 		if res < end-start+1 {
-// 			res = end - start + 1
-// 		}
+//LengthOfLongestSubstring my own solution2
+func LengthOfLongestSubstring(s string) int {
+	start := 0
+	res := 0
+	m := make(map[byte]int)
+	for i := 0; i < len(s); i++ {
+		end := i
+		elem, ok := m[s[i]]
+		if !ok {
+			m[s[i]] = i
+		} else if ok {
+			if elem+1 >= start {
+				start = elem + 1
+			}
+			m[s[i]] = i
+		}
+		if res < end-start+1 {
+			res = end - start + 1
+		}
 
-// 	}
-// 	return res
+	}
+	return res
 }
 
 // leetcode solution
