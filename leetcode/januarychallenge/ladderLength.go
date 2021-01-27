67d4b9b7122244e11a73abceaeb77b3c986f707a
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-01-27 19:10:21 +0600

    .

--- leetcode/januarychallenge/ladderLength.go
+++ leetcode/januarychallenge/ladderLength.go
@@ -1,46 +1,46 @@
-package januarychallenge
+// package januarychallenge
 
-//LadderLength ...
-func LadderLength(beginWord string, endWord string, wordList []string) int {
-	wordListHashSet := make(map[string]bool)
+// //LadderLength ...
+// func LadderLength(beginWord string, endWord string, wordList []string) int {
+// 	wordListHashSet := make(map[string]bool)
 
-	for _, v := range wordList {
-		wordListHashSet[v] = true
-	}
+// 	for _, v := range wordList {
+// 		wordListHashSet[v] = true
+// 	}
 
-	queue := []string{beginWord}
-	var ladder int
+// 	queue := []string{beginWord}
+// 	var ladder int
 
-	for len(queue) > 0 {
-		levelsize := len(queue)
-		ladder++
+// 	for len(queue) > 0 {
+// 		levelsize := len(queue)
+// 		ladder++
 
-		for k := 0; k < levelsize; k++ {
-			word := queue[0]
-			queue = queue[1:]
+// 		for k := 0; k < levelsize; k++ {
+// 			word := queue[0]
+// 			queue = queue[1:]
 
-			for i := 0; i < len(word); i++ {
-				curr := []byte(word)
-				temp := word
+// 			for i := 0; i < len(word); i++ {
+// 				curr := []byte(word)
+// 				temp := word
 
-				for j := 'a'; j <= 'z'; j++ {
-					curr[i] = byte(j)
-					word = string(curr)
+// 				for j := 'a'; j <= 'z'; j++ {
+// 					curr[i] = byte(j)
+// 					word = string(curr)
 
-					if wordListHashSet[word] {
-						queue = append(queue, word)
-						delete(wordListHashSet, word)
+// 					if wordListHashSet[word] {
+// 						queue = append(queue, word)
+// 						delete(wordListHashSet, word)
 
-						if word == endWord {
-							return ladder + 1
-						}
-					}
-				}
-				word = temp
-			}
-		}
+// 						if word == endWord {
+// 							return ladder + 1
+// 						}
+// 					}
+// 				}
+// 				word = temp
+// 			}
+// 		}
 
-	}
+// 	}
 
-	return 0
-}
+// 	return 0
+// }
