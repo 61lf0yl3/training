4b5b37bb29ebfd87ff47068c7298876257854660
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-01-30 12:30:21 +0600

    .

--- leetcode/januarychallenge/mergeTwoLists.go
+++ leetcode/januarychallenge/mergeTwoLists.go
@@ -43,18 +43,18 @@ func SortListInsert(l *ListNode, dataref int) *ListNode {
 	return l
 }
 
-// func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
-//     if l1 == nil{
-//         return l2
-//     }
-//     if l2 == nil{
-//         return l1
-//     }
-//     if l1.Val < l2.Val {
-//         l1.Next = mergeTwoLists(l1.Next, l2)
-//         return l1
-//     }else {
-//         l2.Next = mergeTwoLists(l2.Next, l1)
-//         return l2
-//     }
-// }
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+	if l1.Val < l2.Val {
+		l1.Next = mergeTwoLists(l1.Next, l2)
+		return l1
+	} else {
+		l2.Next = mergeTwoLists(l2.Next, l1)
+		return l2
+	}
+}
