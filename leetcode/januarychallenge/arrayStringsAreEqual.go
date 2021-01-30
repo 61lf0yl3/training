e14561bf22852520b3f8374f6ba442e92090f335
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-01-30 12:28:32 +0600

    .

--- leetcode/januarychallenge/arrayStringsAreEqual.go
+++ leetcode/januarychallenge/arrayStringsAreEqual.go
@@ -1,6 +1,7 @@
 package januarychallenge
 
 import (
+	"fmt"
 	"strings"
 )
 
@@ -8,8 +9,8 @@ import (
 func ArrayStringsAreEqual(word1 []string, word2 []string) bool {
 	word1str := strings.Join(word1, " ")
 	word2str := strings.Join(word2, " ")
-	// fmt.Println(word1str)
-	// fmt.Println(word2str)
+	fmt.Println(word1str)
+	fmt.Println(word2str)
 	return true
 	return strings.Join(word1, "") == strings.Join(word2, "")
 }
