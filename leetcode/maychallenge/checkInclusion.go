47054aba1515380da615008c6a89dbcb2782d8d0
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-02-04 09:36:52 +0600

    .

--- leetcode/maychallenge/checkInclusion.go
+++ leetcode/maychallenge/checkInclusion.go
@@ -49,30 +49,30 @@ func checkInclusion(s1 string, s2 string) bool {
 	return false
 }
 
-// func checkInclusion(s1 string, s2 string) bool {
-// 	uniqChars := make([]int, 26)
+func checkInclusion1(s1 string, s2 string) bool {
+	uniqChars := make([]int, 26)
 
-// 	for i := range s1 {
-// 		uniqChars[s1[i]-'a']++
-// 	}
+	for i := range s1 {
+		uniqChars[s1[i]-'a']++
+	}
 
-// 	currSet := make([]int, 26)
+	currSet := make([]int, 26)
 
-// 	for i := range s2 {
+	for i := range s2 {
 
-// 		currSet[s2[i]-'a']++
+		currSet[s2[i]-'a']++
 
-// 		if i >= len(s1) {
-// 			currSet[s2[i-len(s1)]-'a']--
-// 		}
-// 		//fmt.Println(currSet, uniqChars)
-// 		if true == deepEqual(currSet, uniqChars) {
-// 			return true
-// 		}
-// 	}
+		if i >= len(s1) {
+			currSet[s2[i-len(s1)]-'a']--
+		}
+		//fmt.Println(currSet, uniqChars)
+		if true == deepEqual(currSet, uniqChars) {
+			return true
+		}
+	}
 
-// 	return false
-// }
+	return false
+}
 
 // func deepEqual(a, b []int) bool {
 // 	for i := 0; i < 26; i++ {
