9eac00f143edfeebfe08b7e449a74a35cf1d4539
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-02-05 14:35:39 +0600

    .

--- leetcode/maychallenge/checkStraightLine.go
+++ leetcode/maychallenge/checkStraightLine.go
@@ -1,11 +1,9 @@
 package main
 
-import "fmt"
-
-func main() {
-	coordinates := [][]int{{1, 2}, {2, 3}, {3, 4}, {4, 5}, {5, 6}, {6, 7}}
-	fmt.Println(checkStraightLine(coordinates))
-}
+// func main() {
+// 	coordinates := [][]int{{1, 2}, {2, 3}, {3, 4}, {4, 5}, {5, 6}, {6, 7}}
+// 	fmt.Println(checkStraightLine(coordinates))
+// }
 
 func checkStraightLine(coordinates [][]int) bool {
 	if len(coordinates) == 2 {
