6c6a4498a82accf8b90d8f0939a9c4d8991e02a0
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-03-21 10:38:00 +0600

    .

--- leetcode/maychallenge/maxProfit.go
+++ leetcode/maychallenge/maxProfit.go
@@ -1,13 +1,11 @@
 package main
 
-import "fmt"
-
-func main() {
-	prices := []int{7, 1, 5, 3, 6, 4}
-	//prices := []int{1, 2, 3, 4, 5}
-	//prices := []int{7, 6, 4, 3, 1}
-	fmt.Println(maxProfit(prices))
-}
+// func main() {
+// 	prices := []int{7, 1, 5, 3, 6, 4}
+// 	//prices := []int{1, 2, 3, 4, 5}
+// 	//prices := []int{7, 6, 4, 3, 1}
+// 	fmt.Println(maxProfit(prices))
+// }
 
 func maxProfit(prices []int) int {
 	var maxprofit = 0
