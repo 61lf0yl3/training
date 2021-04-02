b4aad38e434a27c912b9918bdf7deb16e37824c5
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-04-02 15:51:21 +0600

    findErrorNum

--- leetcode/marchchallenge/findErrorNums.go
+++ leetcode/marchchallenge/findErrorNums.go
@@ -33,3 +33,34 @@ func findErrorNums(nums []int) []int {
 		return res
 	}
 }
+func findErrorNums2(nums []int) []int {
+
+	dup := 0
+	for i := 0; i < len(nums); i++ {
+		n := abs(nums[i])
+
+		if nums[n-1] < 0 {
+			dup = n
+			// NOTICE: nums[n-1]<0 的时候，不能再取反为正数了
+		} else {
+			nums[n-1] = -nums[n-1]
+		}
+	}
+
+	mis := 0
+	for i, v := range nums {
+		if v > 0 {
+			mis = i + 1
+			break
+		}
+	}
+
+	return []int{dup, mis}
+}
+
+func abs(a int) int {
+	if a > 0 {
+		return a
+	}
+	return -a
+}
