68c45b481caef03a9890ffc64131b102215b695b
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-04-25 10:49:28 +0600

    buildTree

--- leetcode/junechallenge/buildTree.go
+++ leetcode/junechallenge/buildTree.go
@@ -33,3 +33,25 @@ func buildTree(preorder []int, inorder []int) *TreeNode {
 	res.Right = buildTree(preorder[idx+1:], inorder[idx+1:])
 	return res
 }
+
+func buildTree2(preorder []int, inorder []int) *TreeNode {
+	inPos := make(map[int]int, len(inorder))
+
+	for i := range inorder {
+		inPos[inorder[i]] = i
+	}
+	return help(preorder, 0, len(preorder)-1, 0, inPos)
+}
+
+func help(pre []int, preStart, preEnd, inStart int, inPos map[int]int) *TreeNode {
+	if preStart > preEnd {
+		return nil
+	}
+	root := &TreeNode{Val: pre[preStart]}
+	rootIndex := inPos[pre[preStart]]
+	preLength := rootIndex - inStart
+	root.Left = help(pre, preStart+1, preStart+preLength, inStart, inPos)
+	root.Right = help(pre, preStart+preLength+1, preEnd, rootIndex+1, inPos)
+
+	return root
+}
