62b178c6f28f5e12ca286a1aa29d1602c6e00b4c
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-05-05 15:44:55 +0600

    runningSum

---
+++ leetcode/maychallenge/runningSum.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{1, 2, 3, 4}
+	nums = []int{1, 1, 1, 1, 1}
+	nums = []int{3, 1, 2, 10, 1}
+	fmt.Println(runningSum(nums))
+}
+
+func runningSum(nums []int) []int {
+	res := []int{}
+	sum := 0
+	for i := 0; i < len(nums); i++ {
+		sum += nums[i]
+		//fmt.Println(sum)
+		res = append(res, sum)
+	}
+	return res
+}
