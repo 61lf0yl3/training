65a1372ca46cfeedb96062139aec413edf4b71b6
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-05-17 10:25:47 +0600

    superpalindromesInRange

--- leetcode/maychallenge/superpalindromesInRange.go
+++ leetcode/maychallenge/superpalindromesInRange.go
@@ -1,9 +1,39 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math"
+	"strconv"
+)
 
+func main() {
+	left := "4"
+	right := "1000"
+	fmt.Println(superpalindromesInRange(left, right))
 }
 
 func superpalindromesInRange(left string, right string) int {
+	rightint, _ := strconv.Atoi(right)
+	leftint, _ := strconv.Atoi(left)
+	var i int = int(math.Round(math.Sqrt(float64(leftint))))
+	var res int
+	for {
+		if i*i > rightint {
+			break
+		}
+		if isPalindrome(strconv.Itoa(i)) && isPalindrome(strconv.Itoa(i*i)) {
+			res++
+		}
+		i++
+	}
+	return res
+}
 
+func isPalindrome(input string) bool {
+	for i := 0; i < len(input)/2; i++ {
+		if input[i] != input[len(input)-i-1] {
+			return false
+		}
+	}
+	return true
 }
