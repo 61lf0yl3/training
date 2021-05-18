dc6e32e7a1b2404c0d9a931a4d3fa27c986ba67b
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-05-18 12:51:14 +0600

    isPossible

--- leetcode/maychallenge/isPossible.go
+++ leetcode/maychallenge/isPossible.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	target := []int{9, 3, 5}
-	fmt.Println(isPossible(target))
+	fmt.Println(isPossible2(target))
 }
 
 func isPossible(target []int) bool {
@@ -18,7 +18,7 @@ func isPossible(target []int) bool {
 		if sum-target[i]+1 != target[i] {
 			return false
 		}
-		sum = sum - target[i]
+		sum = sum - target[i] + 1
 	}
 	return true
 }
@@ -29,4 +29,26 @@ func Sum(arr []int) int {
 		sum += nb
 	}
 	return sum
+}
+
+func isPossible2(target []int) bool {
+	sum := 0
+	max := 0
+
+	for i := 0; i < len(target); i++ {
+		sum += target[i]
+		if target[max] < target[i] {
+			max = i
+		}
+	}
+
+	diff := sum - target[max]
+	if target[max] == 1 || diff == 1 {
+		return true
+	}
+	if diff > target[max] || diff == 0 || target[max]%diff == 0 {
+		return false
+	}
+	target[max] %= diff
+	return isPossible2(target)
 }
