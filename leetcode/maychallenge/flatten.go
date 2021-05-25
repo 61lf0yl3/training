0e254af10fef1f3faeb8d2fc077c71fc0e763cc6
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-05-25 22:56:04 +0600

    flatten

--- leetcode/maychallenge/flatten.go
+++ leetcode/maychallenge/flatten.go
@@ -7,5 +7,17 @@ type TreeNode struct {
 }
 
 func flatten(root *TreeNode) {
-
+	node := root
+	for node != nil {
+		if node.Left != nil {
+			tmp := node.Left
+			for tmp.Right != nil {
+				tmp = tmp.Right
+			}
+			tmp.Right = node.Right
+			node.Right = node.Left
+			node.Left = nil
+		}
+		node = node.Right
+	}
 }
