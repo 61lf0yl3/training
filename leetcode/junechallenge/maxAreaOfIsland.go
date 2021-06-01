5187684db62516e6f8d7db0d5b83f549388784ad
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-06-01 15:35:09 +0600

    maxAreaOfIsland

--- leetcode/junechallenge/maxAreaOfIsland.go
+++ leetcode/junechallenge/maxAreaOfIsland.go
@@ -11,32 +11,40 @@ func maxAreaOfIsland(grid [][]int) int {
 	res := 0
 	for y := range grid {
 		for x := range grid[y] {
+			curr := 0
 			if grid[y][x] == 1 {
-				res++
-				BFS(grid, y, x, res)
+				//res++
+				BFS(grid, y, x, &curr)
+			}
+			if curr > res {
+				res = curr
 			}
 		}
 	}
 	return res
 }
 
-func BFS(grid [][]int, y, x, res int) {
+func BFS(grid [][]int, y, x int, curr *int) {
 	PrintGrid(grid)
-	fmt.Println(res)
+	fmt.Println(*curr)
 	fmt.Println()
 	if y < 0 || y >= len(grid) || x < 0 || x >= len(grid[y]) || grid[y][x] == 0 {
 		return
 	}
+	if grid[y][x] == 1 {
+		*curr++
+	}
 	grid[y][x] = 0
-	BFS(grid, y+1, x, res) // check up
-	BFS(grid, y-1, x, res) // check down
-	BFS(grid, y, x+1, res) // check right
-	BFS(grid, y, x-1, res) // check left
+	BFS(grid, y+1, x, curr) // check up
+	BFS(grid, y-1, x, curr) // check down
+	BFS(grid, y, x+1, curr) // check right
+	BFS(grid, y, x-1, curr) // check left
 }
 
 func PrintGrid(grid [][]int) {
 	for y := range grid {
 		for x := range grid[y] {
+			a
 			fmt.Print(grid[y][x])
 		}
 		fmt.Println()
