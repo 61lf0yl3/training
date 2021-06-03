617fa4e072cbf6c3eb0c28f150ad321ff039baa2
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-06-04 00:02:21 +0600

    maxArea

--- leetcode/junechallenge/maxArea.go
+++ leetcode/junechallenge/maxArea.go
@@ -31,3 +31,30 @@ func maxDiff(arr []int, nb int) int {
 	}
 	return res
 }
+
+func maxArea2(h int, w int, hcuts []int, vcuts []int) int {
+	sort.Ints(hcuts)
+	sort.Ints(vcuts)
+
+	var maxw = hcuts[0] - 0
+	for i, c := range hcuts[1:] {
+		if c-hcuts[i] > maxw {
+			maxw = c - hcuts[i]
+		}
+	}
+	if h-hcuts[len(hcuts)-1] > maxw {
+		maxw = h - hcuts[len(hcuts)-1]
+	}
+	var maxl = vcuts[0] - 0
+	for i, c := range vcuts[1:] {
+		if c-vcuts[i] > maxl {
+			maxl = c - vcuts[i]
+		}
+	}
+
+	if w-vcuts[len(vcuts)-1] > maxl {
+		maxl = w - vcuts[len(vcuts)-1]
+	}
+
+	return (maxw * maxl) % (1000000007)
+}
