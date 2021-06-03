c32b05fbd02276ddace85f7d311d2a92623e6abe
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-06-03 12:08:29 +0600

    isInterleave

--- leetcode/junechallenge/isInterleave.go
+++ leetcode/junechallenge/isInterleave.go
@@ -1,5 +1,70 @@
 package main
 
-func isInterleave(s1 string, s2 string, s3 string) bool {
+import "fmt"
+
+func main() {
+	// s1 := "aabcc"
+	// s2 := "dbbca"
+	// s3 := "aadbbcbcac"
+	s1 := "abx"
+	s2 := "mbn"
+	s3 := "ambbnx"
+	// matrix := make([][]bool, len(s1)+1)
+	// for i := 0; i <= len(s1); i++ {
+	// 	matrix[i] = make([]bool, len(s2)+1)
+	// }
+	// fmt.Println(matrix)
+	fmt.Println(isInterleave(s1, s2, s3))
 
 }
+
+func isInterleave(s1 string, s2 string, s3 string) bool {
+	matrix := make([][]bool, len(s1)+1)
+	for i := 0; i <= len(s1); i++ {
+		matrix[i] = make([]bool, len(s2)+1)
+	}
+	matrix[0][0] = true
+	for i := 0; i < len(s1); i++ {
+		for j := 0; j < len(s2); j++ {
+			Print(matrix)
+			fmt.Println("i:", i, "j:", j)
+			fmt.Println()
+			if s1[i] == s3[j+i] && matrix[i][j] == true {
+				matrix[i+1][j] = true
+			}
+			if s2[j] == s3[j+i] && matrix[i][j] == true {
+				matrix[i][j+1] = true
+			}
+		}
+	}
+	Print(matrix)
+	return matrix[len(s1)][len(s2)]
+}
+
+func Print(matrix [][]bool) {
+	for i := 0; i < len(matrix); i++ {
+		for j := 0; j < len(matrix); j++ {
+			fmt.Print(matrix[i][j])
+			fmt.Print(" ")
+		}
+		fmt.Println()
+	}
+}
+
+// func isInterleave(s1 string, s2 string, s3 string) bool {
+// 	for _, v := range s3 {
+// 		if len(s1) > 0 && byte(v) == s1[0] {
+// 			s1 = s1[1:]
+// 			fmt.Println(s1)
+// 			continue
+// 		}
+// 		if len(s2) > 0 && byte(v) == s2[0] {
+// 			s2 = s2[1:]
+// 			fmt.Println(s2)
+// 			continue
+// 		} else {
+// 			return false
+// 		}
+// 	}
+// 	return true
+// }
