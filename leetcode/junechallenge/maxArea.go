bf521810abd8e5c5563c34b15420ac138041fc0b
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-06-04 00:00:09 +0600

    maxArea

--- leetcode/junechallenge/maxArea.go
+++ leetcode/junechallenge/maxArea.go
@@ -1,17 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"sort"
 )
 
-func main() {
-	h := 5
-	w := 4
-	horizontalCuts := []int{1, 2, 4}
-	verticalCuts := []int{1, 3}
-	fmt.Println(maxArea(h, w, horizontalCuts, verticalCuts))
-}
+// func main() {
+// 	h := 5
+// 	w := 4
+// 	horizontalCuts := []int{1, 2, 4}
+// 	verticalCuts := []int{1, 3}
+// 	fmt.Println(maxArea(h, w, horizontalCuts, verticalCuts))
+// }
 
 func maxArea(h int, w int, horizontalCuts []int, verticalCuts []int) int {
 	maxHorizontalCut := maxDiff(horizontalCuts, h)
