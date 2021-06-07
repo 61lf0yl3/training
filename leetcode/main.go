b0bdd7e060f89c3e6c41a6c93f4cbdec80d8c3eb
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-06-07 10:54:33 +0600

    longestConsecutive

--- leetcode/main.go
+++ leetcode/main.go
@@ -2,10 +2,33 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
-	for i := 33; i < 127; i++ {
-		fmt.Println(string(i))
+	nums := []int{0, 3, 7, 2, 5, 8, 4, 6, 0, 1}
+	fmt.Println(longestConsecutive(nums))
+}
+func longestConsecutive(nums []int) int {
+	if len(nums) == 0 {
+		return 0
 	}
+	sort.Ints(nums)
+	fmt.Println(nums)
+	res := 1
+	curr := 1
+	for i := 1; i < len(nums); i++ {
+		fmt.Println("i-1:", nums[i-1]+1, "i:", nums[i])
+		if nums[i-1]+1 == nums[i] {
+			fmt.Println("pass")
+			curr++
+			if curr > res {
+				res = curr
+			}
+		} else {
+			fmt.Println("out")
+			curr = 1
+		}
+	}
+	return res
 }
