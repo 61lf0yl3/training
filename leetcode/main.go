8a29c8ff6008bd0826f3b4a4c658a1852b9ad5fb
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-06-07 11:02:27 +0600

    longestConsecutive

--- leetcode/main.go
+++ leetcode/main.go
@@ -35,3 +35,32 @@ func longestConsecutive(nums []int) int {
 	}
 	return res
 }
+
+func longestConsecutive2(nums []int) int {
+	sort.Ints(nums)
+	if len(nums) == 0 {
+		return 0
+	}
+	res := 1
+	seq := 1
+	for i := 1; i < len(nums); i++ {
+		if nums[i] == nums[i-1]+1 {
+			seq++
+			continue
+		}
+		if nums[i] == nums[i-1] {
+			continue
+		}
+		res = max(res, seq)
+		seq = 1
+	}
+	res = max(res, seq)
+	return res
+}
+
+func max(x int, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
