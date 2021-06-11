cf7554fda09bfb993d0a57cd038932d3397d2423
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-06-11 15:43:35 +0600

    MyCalendar

--- leetcode/junechallenge/mycalendar1.go
+++ leetcode/junechallenge/mycalendar1.go
@@ -23,3 +23,41 @@ func (this *MyCalendar) Book(start int, end int) bool {
  * obj := Constructor();
  * param_1 := obj.Book(start,end);
  */
+
+type MyCalendar2 struct {
+	root *Node
+}
+
+type Node2 struct {
+	start, end  int
+	left, right *Node
+}
+
+func Constructor2() MyCalendar {
+	return MyCalendar{root: nil}
+}
+
+func (this *MyCalendar2) Book2(start int, end int) bool {
+	if this.root == nil {
+		this.root = &Node2{start: start, end: end, left: nil, right: nil}
+		return true
+	}
+	return this.root.insert(&Node{start: start, end: end, left: nil, right: nil})
+}
+
+func (n *Node) insert(node *Node) bool {
+	if node.start >= n.end {
+		if n.right == nil {
+			n.right = node
+			return true
+		}
+		return n.right.insert(node)
+	} else if node.end <= n.start {
+		if n.left == nil {
+			n.left = node
+			return true
+		}
+		return n.left.insert(node)
+	}
+	return false
+}
