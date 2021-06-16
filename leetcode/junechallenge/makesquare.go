81c7ad8bc01a55328ad5250f4f1049e53f5513ba
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-06-16 12:42:56 +0600

    makesquare

--- leetcode/junechallenge/makesquare.go
+++ leetcode/junechallenge/makesquare.go
@@ -63,3 +63,53 @@ func analyze(nums []int) (ok bool, avg int) {
 	}
 	return true, sum / 4
 }
+
+func makesquare2(arr []int) bool {
+	if len(arr) < 4 {
+		return false
+	}
+	total := 0
+	for _, v := range arr {
+		total += v
+	}
+	if total%4 != 0 {
+		return false
+	}
+
+	sort.Slice(arr, func(i, j int) bool {
+		return arr[i] > arr[j]
+	})
+	visited := make([]bool, 16)
+
+	var dfs func([]int, int, int, int) bool
+	dfs = func(arr []int, cur int, grp int, sum int) bool {
+		if grp == 4 {
+			return true
+		}
+		if sum > total/4 {
+			return false
+		}
+		if sum == total/4 {
+			return dfs(arr, 0, grp+1, 0)
+		}
+
+		last := -1
+		for i := cur; i < len(arr); i++ {
+			if visited[i] {
+				continue
+			}
+			if last == arr[i] {
+				continue
+			}
+
+			visited[i] = true
+			last = arr[i]
+			if dfs(arr, i+1, grp, sum+arr[i]) {
+				return true
+			}
+			visited[i] = false
+		}
+		return false
+	}
+	return dfs(arr, 0, 0, 0)
+}
