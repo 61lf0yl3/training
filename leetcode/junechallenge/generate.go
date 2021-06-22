ae54b250e5e5600ba2e5b0cd89b63813ba68045b
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-06-22 12:45:08 +0600

    generate

--- leetcode/junechallenge/generate.go
+++ leetcode/junechallenge/generate.go
@@ -1,23 +1,19 @@
 package main
 
-import "fmt"
-
-func main() {
-	n := 5
-	fmt.Println(generate(n))
-}
+// func main() {
+// 	n := 5
+// 	fmt.Println(generate(n))
+// }
 
 func generate(numRows int) [][]int {
 	rows := [][]int{}
 	for i := 1; i <= numRows; i++ {
 		a := make([]int, i)
 		a[0] = 1
-		if len(a) > 1 {
-			a[len(a)-1] = 1
-		}
+		a[len(a)-1] = 1
 		if len(a) > 2 {
-			for j := 1; j < i; i++ {
-				a[j] = rows[i-1][j] + rows[i-1][j-1]
+			for j := 1; j < i-1; j++ {
+				a[j] = rows[i-2][j] + rows[i-2][j-1]
 			}
 		}
 		rows = append(rows, a)
