b7b503c0d9374f34ebcc8bb08119b06fe3b86729
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-08-16 17:01:22 -0400

    numpairDivisibley60

--- algo/PairsofSongsWithTotalDurationsDivisibleby60.java
+++ algo/PairsofSongsWithTotalDurationsDivisibleby60.java
@@ -44,4 +44,22 @@ class PairsofSongsWithTotalDurationsDivisibleby60 {
         }
         return res;
     }
+
+    // Approach 3: Use an Array to Store Frequencies
+    // Time complexity : O(N)
+    // Space complexity : O(N)
+    public int numPairsDivisibleBy603(int[] time) {
+        int[] remainder = new int[60];
+        int res = 0;
+        for (int t : time) {
+            if (t % 60 == 0) {
+                res += remainder[t % 60];
+            } else {
+                res += remainder[60 - t % 60];
+            }
+
+            remainder[t % 60]++;
+        }
+        return res;
+    }
 }
