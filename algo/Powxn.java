dae5c2a8d37df98ed2abef0588b1822c3b62bc54
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-08-28 14:55:23 -0400

    fastpow

--- algo/Powxn.java
+++ algo/Powxn.java
@@ -16,4 +16,28 @@ class Powxn {
         }
         return res;
     }
+
+    // Approach 1: Recursion
+    // Time complexity : O(N)
+    // Space complexity : O(1)
+    public double myPow2(double x, int n) {
+        if (n < 0) {
+            x = 1 / x;
+            n = -n;
+        }
+        return fastPow(x, n);
+    }
+
+    public double fastPow(double x, int n) {
+        if (n == 0) {
+            return 1.0;
+        }
+        double half = fastPow(x, n / 2);
+        if (n % 2 == 0) {
+            return half * half;
+        } else {
+            return half * half * x;
+        }
+    }
+
 }
