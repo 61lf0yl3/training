40fe000d22ba45b6726be96f38cef10a6e8790cb
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-09-27 17:44:26 -0700

    md1

--- leetcode/codeInterview/s17md1.java
+++ leetcode/codeInterview/s17md1.java
@@ -1,10 +1,32 @@
 
 class s17md1 {
     public static void main(String[] args) {
-        int[] vals = {1,2,1,2,1};
-        System.out.println(more1sThan2s(vals));
+        int[] vals = {};
+        //System.out.println(more1sThan2s(vals));
+        System.out.println(hasDouble5or8(vals));
     }
 
+    public static boolean hasDouble5or8(int[] nums) {
+        boolean hasDouble5 = false;
+        boolean hasDouble8 = false;
+        boolean res = false;
+
+        for (int i=0;i< nums.length -1; i++) {
+            if (nums[i] == 5 && nums[i]==nums[i+1]) {
+                hasDouble5 = true;
+                res = true;
+            } 
+            if (nums[i] == 8 && nums[i]==nums[i+1]) {
+                hasDouble8 = true;
+                res = true;
+            }
+        }
+        if (hasDouble5 == hasDouble8) {
+            res = false;
+        } 
+        return res;
+    }
+    
     public static boolean more1sThan2s(int[] vals) { int count1 = 0;
         int count2 = 0;
         for (int i = 0; i < vals.length; i++) {
@@ -17,4 +39,6 @@ class s17md1 {
             }
         return count1 > count2; 
     }
+
+
 }
