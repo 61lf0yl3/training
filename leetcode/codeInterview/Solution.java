cc4f7235c97b3f94b29502e1126ac8fcd568f014
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-09-29 18:58:28 -0700

    algo

--- leetcode/codeInterview/Solution.java
+++ leetcode/codeInterview/Solution.java
@@ -27,4 +27,23 @@ class Solution {
         }
         return low;
     }
+    public int searchInsert(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length-1;
+        while (low<=high) {
+            int mid = low + (high-low)/2;
+            if (target == nums[mid]) {
+                return mid;
+            }
+            if (target < nums[mid]) {
+                high = mid -1;
+            } else {
+                low = mid +1;
+            }
+        }
+        if (target > nums[nums.length-1]) {
+            return high+1;
+        }
+        return low;
+    }
 }
