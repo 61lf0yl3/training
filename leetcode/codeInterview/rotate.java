189e9b96765b98f963671e9cb061e7ad9054dbc8
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-10-02 21:25:42 -0700

    .

--- leetcode/codeInterview/rotate.java
+++ leetcode/codeInterview/rotate.java
@@ -1,5 +1,5 @@
 class Solution4 {
-    public void rotate(int[] nums, int k) {
+    public void rotate2(int[] nums, int k) {
         int[]  res = new int[nums.length];
         k = k % nums.length;
         for (int i = 0; i < nums.length; i++) {
@@ -10,4 +10,19 @@ class Solution4 {
             nums[i] = res[i];
         }
     }
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+      }
+      public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+          int temp = nums[start];
+          nums[start] = nums[end];
+          nums[end] = temp;
+          start++;
+          end--;
+        }
+      }
 }
