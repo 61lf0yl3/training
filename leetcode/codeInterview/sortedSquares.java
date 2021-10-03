08fe15283ffa5839ca149f87a53f9b2db61935c5
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-10-02 20:01:28 -0700

    codeinter

---
+++ leetcode/codeInterview/sortedSquares.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+class Solution2 {
+    public int[] sortedSquares(int[] A) {
+        int N = A.length;
+        int[] ans = new int[N];
+        for (int i = 0; i < N; ++i)
+            ans[i] = A[i] * A[i];
+
+        Arrays.sort(ans);
+        return ans;
+    }
+
+    //two pointer method O(n)
+    public int[] sortedSquares2(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        int left = 0;
+        int right = n - 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            int square;
+            if (Math.abs(nums[left]) < Math.abs(nums[right])) {
+                square = nums[right];
+                right--;
+            } else {
+                square = nums[left];
+                left++;
+            }
+            result[i] = square * square;
+        }
+        return result;
+    }
+}
