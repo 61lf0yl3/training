7abf59b32bf0a933ea3b3ccfe6a916d62197507f
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-10-16 16:18:14 -0700

    twosum

---
+++ algo/twoSum.java
@@ -0,0 +1,35 @@
+class Solution {
+    //O(n^2)
+    public int[] twoSum(int[] numbers, int target) {
+        int[] res = new int[2];
+        for (int i=0; i < numbers.length; i++) {
+            for (int j=i+1; j<numbers.length; j++) {
+                if (numbers[i]+numbers[j]==target) {
+                    res[0] = i+1;
+                    res[1] = j+1;
+                }
+            }
+        }
+        return res;
+    }
+    // O(n)
+    public int[] twoSum2(int[] numbers, int target) {
+        int[] res = new int[2];
+        int low = 0;
+        int high = numbers.length-1;
+        while (low < high) {
+            int sum = numbers[low]+numbers[high];
+            if (sum==target) {
+                res[0]=low+1;
+                res[1]=high+1;
+                return res;
+            } else if (sum > target) {
+                high--; 
+            } else {
+                low++;
+            }
+        }
+        return res;
+    }
+}
+}
