af3268efe8af158f25696d16b30e121f7ed60c4d
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-10-18 00:34:16 -0700

    lengthOfLongestSubstring

--- algo/lengthOfLongestSubstring.java
+++ algo/lengthOfLongestSubstring.java
@@ -1,4 +1,5 @@
 class Solution {
+    // O(n^2*logn) bruteforce
     public int lengthOfLongestSubstring(String s) {
         Map<Character, Boolean> map = new HashMap<>();
         int max = 0;
@@ -17,5 +18,29 @@ class Solution {
             }
         }
         return max;
+    }
+
+    public int lengthOfLongestSubstring2(String s) {
+        int[] chars = new int[128];
+
+        int left = 0;
+        int right = 0;
+
+        int res = 0;
+        while (right < s.length()) {
+            char r = s.charAt(right);
+            chars[r]++;
+
+            while (chars[r] > 1) {
+                char l = s.charAt(left);
+                chars[l]--;
+                left++;
+            }
+
+            res = Math.max(res, right - left + 1);
+
+            right++;
+        }
+        return res;
     }
 }
