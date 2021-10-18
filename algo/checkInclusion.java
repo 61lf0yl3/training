e216976355b68c10862ef06d49cc8807e645edae
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-10-18 14:23:18 -0700

    567

---
+++ algo/checkInclusion.java
@@ -0,0 +1,94 @@
+public class Solution {
+    //Using sorting
+    // Time complexity:  O(l1*log(l1)+(l2 −l1 )*l1*log(l1)).
+    public boolean checkInclusion(String s1, String s2) {
+        s1 = sort(s1);
+        for (int i = 0; i <= s2.length() - s1.length(); i++) {
+            if (s1.equals(sort(s2.substring(i, i + s1.length()))))
+                return true;
+        }
+        return false;
+    }
+    
+    // Using Array 
+    // Time complexity:  O(l1+26*l1 *(l2−l1)), where  l1,l2 is the length of string l1, l2.
+    public boolean checkInclusion2(String s1, String s2) {
+        if (s1.length() > s2.length())
+            return false;
+        int[] s1map = new int[26];
+        for (int i = 0; i < s1.length(); i++)
+            s1map[s1.charAt(i) - 'a']++;
+        for (int i = 0; i <= s2.length() - s1.length(); i++) {
+            int[] s2map = new int[26];
+            for (int j = 0; j < s1.length(); j++) {
+                s2map[s2.charAt(i + j) - 'a']++;
+            }
+            if (matches(s1map, s2map))
+                return true;
+        }
+        return false;
+    }
+    
+    public boolean matches(int[] s1map, int[] s2map) {
+        for (int i = 0; i < 26; i++) {
+            if (s1map[i] != s2map[i])
+                return false;
+        }
+        return true;
+    }
+
+    // Sliding Window
+    // Time complexity:  O(l1+26*l1 *(l2−l1)), where  l1,l2 is the length of string l1, l2.
+    public boolean checkInclusion3(String s1, String s2) {
+        if (s1.length() > s2.length())
+            return false;
+        int[] s1map = new int[26];
+        int[] s2map = new int[26];
+        for (int i = 0; i < s1.length(); i++) {
+            s1map[s1.charAt(i) - 'a']++;
+            s2map[s2.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < s2.length() - s1.length(); i++) {
+            if (matches(s1map, s2map))
+                return true;
+            s2map[s2.charAt(i + s1.length()) - 'a']++;
+            s2map[s2.charAt(i) - 'a']--;
+        }
+        return matches(s1map, s2map);
+    }
+ 
+    // Optimized Sliding Window
+    // Time complexity:  O ( l1 + ( l2 − l1 ) ) 
+    public boolean checkInclusion4(String s1, String s2) {
+        if (s1.length() > s2.length())
+            return false;
+        int[] s1map = new int[26];
+        int[] s2map = new int[26];
+        for (int i = 0; i < s1.length(); i++) {
+            s1map[s1.charAt(i) - 'a']++;
+            s2map[s2.charAt(i) - 'a']++;
+         }
+            
+        int count = 0;
+        for (int i = 0; i < 26; i++)
+            if (s1map[i] == s2map[i])
+                count++;
+                    
+        for (int i = 0; i < s2.length() - s1.length(); i++) {
+            int r = s2.charAt(i + s1.length()) - 'a', l = s2.charAt(i) - 'a';
+            if (count == 26)
+                    return true;
+            s2map[r]++;
+            if (s2map[r] == s1map[r])
+                count++;
+            else if (s2map[r] == s1map[r] + 1)
+                count--;
+            s2map[l]--;
+            if (s2map[l] == s1map[l])
+                count++;
+            else if (s2map[l] == s1map[l] - 1)
+                count--;
+        }
+           return count == 26;
+    }
+}
--- algo/lengthOfLongestSubstring.java
+++ algo/lengthOfLongestSubstring.java
@@ -19,7 +19,7 @@ class Solution {
         }
         return max;
     }
-
+    // O(n) Sliding Window
     public int lengthOfLongestSubstring2(String s) {
         int[] chars = new int[128];
 
@@ -42,5 +42,19 @@ class Solution {
             right++;
         }
         return res;
+    }
+
+    public int lengthOfLongestSubstring3(String s) {
+        int n = s.length(), ans = 0;
+        Map<Character, Integer> map = new HashMap<>(); // current index of character
+        // try to extend the range [i, j]
+        for (int j = 0, i = 0; j < n; j++) {
+            if (map.containsKey(s.charAt(j))) {
+                i = Math.max(map.get(s.charAt(j)), i);
+            }
+            ans = Math.max(ans, j - i + 1);
+            map.put(s.charAt(j), j + 1);
+        }
+        return ans;
     }
 }
