e12ee8b405bee63d157407547f876d445a74c9bd
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-10-22 23:40:27 -0700

    connect

--- algo/connect.java
+++ algo/connect.java
@@ -1,4 +1,6 @@
 class Solution {
+    // Time Complexity: O(n)
+    // Space Complexity: O(n) 
     public Node connect(Node root) {
         
         if (root == null) {
@@ -47,4 +49,46 @@ class Solution {
         // Since the tree has now been modified, return the root node
         return root;
     }
+
+
+    // Time Complexity: O(n)
+    // Space Complexity: O(1)
+        public Node connect2(Node root) {
+            
+            if (root == null) {
+                return root;
+            }
+            
+            // Start with the root node. There are no next pointers
+            // that need to be set up on the first level
+            Node leftmost = root;
+            
+            // Once we reach the final level, we are done
+            while (leftmost.left != null) {
+                
+                // Iterate the "linked list" starting from the head
+                // node and using the next pointers, establish the 
+                // corresponding links for the next level
+                Node head = leftmost;
+                
+                while (head != null) {
+                    
+                    // CONNECTION 1
+                    head.left.next = head.right;
+                    
+                    // CONNECTION 2
+                    if (head.next != null) {
+                        head.right.next = head.next.left;
+                    }
+                    
+                    // Progress along the list (nodes on the current level)
+                    head = head.next;
+                }
+                
+                // Move onto the next level
+                leftmost = leftmost.left;
+            }
+            
+            return root;
+        }
 }
