6df6d0e23a4a447264f1b04036cf97fca8da4b90
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-11-04 20:32:41 -0700

    houserobber2

---
+++ algo/HouseRobber2.java
@@ -0,0 +1,24 @@
+class HouseRobber2 {
+    //213. House Robber II
+
+    public int rob(int[] nums) {
+        int oldest = 0;
+        int old1 = nums[0];
+        for (int i = 2; i < nums.length; i++) {
+            int curr = Math.max(oldest + nums[i - 1], old1);
+            oldest = old1;
+            old1 = curr;
+        }
+        oldest = 0;
+        int old2 = 0;
+        if (nums.length>1) {
+            old2 = nums[1];
+            for (int i = 3; i <= nums.length; i++) {
+                int curr = Math.max(oldest + nums[i - 1], old2);
+                oldest = old2;
+                old2 = curr;
+            }
+        }
+        return Math.max(old1,old2);
+    }
+}
