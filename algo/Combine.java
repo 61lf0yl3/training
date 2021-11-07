89b30096264cb460aa200ef5efe7e754cf5d6d30
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-11-07 12:29:05 -0800

    combine

--- algo/Combine.java
+++ algo/Combine.java
@@ -13,8 +13,9 @@ class Combine {
         return res;
     }
    
-    //Time complexity : NCk = N!/(N−k)!k!
-    //Space complexity : NCk
+    // Backtracking
+    // Time complexity : NCk = N!/(N−k)!k!
+    // Space complexity : NCk
     public void backtrack(int nb, LinkedList<Integer> solution) {
         if (solution.size() == k) {
             res.addLast(new LinkedList(solution));
@@ -27,4 +28,27 @@ class Combine {
             }
         }
     }
+
+    //Lexicographic (binary sorted) combinations
+    public List<List<Integer>> combine2(int n, int k) {
+        // init first combination
+        LinkedList<Integer> nums = new LinkedList<Integer>();
+        for(int i = 1; i < k + 1; ++i)
+          nums.add(i);
+        nums.add(n + 1);
+    
+        List<List<Integer>> output = new ArrayList<List<Integer>>();
+        int j = 0;
+        while (j < k) {
+          // add current combination
+          output.add(new LinkedList(nums.subList(0, k)));
+          // increase first nums[j] by one
+          // if nums[j] + 1 != nums[j + 1]
+          j = 0;
+          while ((j < k) && (nums.get(j + 1) == nums.get(j) + 1))
+            nums.set(j, j++ + 1);
+          nums.set(j, nums.get(j) + 1);
+        }
+        return output;
+      }
 }
