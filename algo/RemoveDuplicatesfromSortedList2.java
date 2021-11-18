6f4d96f78705a96e9d4509a4f9df06fcdf9abeb9
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-11-18 02:21:24 -0800

    RemoveDuplicatesfromSortedList2

---
+++ algo/RemoveDuplicatesfromSortedList2.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class RemoveDuplicatesfromSortedList2 {
+    //82. Remove Duplicates from Sorted List II
+
+
+    //Approach 1: Sentinel Head + Predecessor
+    //Time Complexity:O(N)
+    //Space Complexity: O(1)
+    public ListNode deleteDuplicates(ListNode head) {
+        // sentinel
+        ListNode sentinel = new ListNode(0, head);
+
+        // predecessor = the last node 
+        // before the sublist of duplicates
+        ListNode pred = sentinel;
+        
+        while (head != null) {
+            // if it's a beginning of duplicates sublist 
+            // skip all duplicates
+            if (head.next != null && head.val == head.next.val) {
+                // move till the end of duplicates sublist
+                while (head.next != null && head.val == head.next.val) {
+                    head = head.next;    
+                }
+                // skip all duplicates
+                pred.next = head.next;     
+            // otherwise, move predecessor
+            } else {
+                pred = pred.next;    
+            }
+                
+            // move forward
+            head = head.next;    
+        }  
+        return sentinel.next;
+    }
+}
