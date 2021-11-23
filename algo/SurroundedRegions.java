5c964bae6953f09f9f43beec0e7af587a8d36b28
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-11-23 00:50:04 -0800

    SurroundedRegions

--- algo/SurroundedRegions.java
+++ algo/SurroundedRegions.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Queue;
+import java.util.LinkedList;
 
 class SurroundedRegions {
     //130. Surrounded Regions
@@ -44,6 +46,26 @@ class SurroundedRegions {
             dfs(board, seen, r-1,c);
             dfs(board, seen, r,c+1);
             dfs(board, seen, r+1,c);
+        }
+    }
+
+    int[][] direction = {{0,-1},{-1,0},{0,1},{1,0}};
+    public void bfs(char[][] board, boolean[][] seen, int r, int c) {
+        Queue<int[]>  q = new LinkedList<>();
+        q.add(new int[]{r,c});
+        seen[r][c]=true;
+        while (!q.isEmpty()) {
+            int[] cell = q.remove();
+            
+            for (int i=0; i<direction.length;i++ ) {
+                int newR = cell[0]+direction[i][0];
+                int newC = cell[1]+direction[i][1];
+                if (newR < board.length && newC < board[0].length && newR >= 0 && newC >= 0 && !seen[newR][newC] && board[newR][newC]=='O') {
+                    seen[newR][newC]=true;
+                    q.add(new int[]{newR,newC});
+            }
+           
+            }
         }
     }
 }
