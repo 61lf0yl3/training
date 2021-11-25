2ad59a8961b080665ded61dd2207082884269ca3
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-11-25 15:34:25 -0800

    permutation2

--- algo/Permutations2.java
+++ algo/Permutations2.java
@@ -1,11 +1,15 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.LinkedList;
 
 class Permutation2 {
     //Approach 1: Backtracking with Groups of Numbers and Set
+    //Time Complexity: O(N*N!)
+    //Space Complexity: O(N)
     public List<List<Integer>> permuteUnique(int[] nums) {
         Set<LinkedList<Integer>> m = new HashSet<>();
         List<List<Integer>> res = new ArrayList<>();
@@ -32,5 +36,55 @@ class Permutation2 {
            }
        }
         
+    }
+
+    //Approach 2: Backtracking with Groups of Numbers (leetcode version)
+    //Time Complexity: O(N*N!)
+    //Space Complexity: O(N)
+    public List<List<Integer>> permuteUnique2(int[] nums) {
+        List<List<Integer>> results = new ArrayList<>();
+
+        // count the occurrence of each number
+        HashMap<Integer, Integer> counter = new HashMap<>();
+        for (int num : nums) {
+            if (!counter.containsKey(num))
+                counter.put(num, 0);
+            counter.put(num, counter.get(num) + 1);
+        }
+
+        LinkedList<Integer> comb = new LinkedList<>();
+        this.backtrack(comb, nums.length, counter, results);
+        return results;
+    }
+
+    protected void backtrack(
+            LinkedList<Integer> comb,
+            Integer N,
+            HashMap<Integer, Integer> counter,
+            List<List<Integer>> results) {
+
+        if (comb.size() == N) {
+            // make a deep copy of the resulting permutation,
+            // since the permutation would be backtracked later.
+            results.add(new ArrayList<Integer>(comb));
+            return;
+        }
+
+        for (Map.Entry<Integer, Integer> entry : counter.entrySet()) {
+            Integer num = entry.getKey();
+            Integer count = entry.getValue();
+            if (count == 0)
+                continue;
+            // add this number into the current combination
+            comb.addLast(num);
+            counter.put(num, count - 1);
+
+            // continue the exploration
+            backtrack(comb, N, counter, results);
+
+            // revert the choice for the next exploration
+            comb.removeLast();
+            counter.put(num, count);
+        }
     }
 }
