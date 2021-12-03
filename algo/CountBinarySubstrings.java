66d7692a568297e1b331b100439332789dbad997
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-12-03 16:50:50 -0500

    CountBinarySubstrings

--- algo/CountBinarySubstrings.java
+++ algo/CountBinarySubstrings.java
@@ -59,4 +59,21 @@ class CountBinarySubstrings {
         }
         return ans;
     }
+
+    // Approach 1: Linear Scan
+    // Time Complexity: O(N)
+    // Space Complexity: O(1)
+    public int countBinarySubstrings3(String s) {
+        int ans = 0, prev = 0, cur = 1;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i - 1) != s.charAt(i)) {
+                ans += Math.min(prev, cur);
+                prev = cur;
+                cur = 1;
+            } else {
+                cur++;
+            }
+        }
+        return ans + Math.min(prev, cur);
+    }
 }
