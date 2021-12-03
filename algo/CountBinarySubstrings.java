351bb1b62363e62c029b631352726c086138c574
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-12-03 16:50:13 -0500

    CountBinarySubstrings

--- algo/CountBinarySubstrings.java
+++ algo/CountBinarySubstrings.java
@@ -1,4 +1,6 @@
 class CountBinarySubstrings {
+    // 696. Count Binary Substrings
+
     // Approach 1: My version
     // Time Complexity: O(N)
     // Space Complexity: O(1)
@@ -34,5 +36,27 @@ class CountBinarySubstrings {
             }
         }
         return res;
+    }
+
+    // Approach 2: Group By Character
+    // Time Complexity: O(N)
+    // Space Complexity: O(N)
+    public int countBinarySubstrings2(String s) {
+        int[] groups = new int[s.length()];
+        int t = 0;
+        groups[0] = 1;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i - 1) != s.charAt(i)) {
+                groups[++t] = 1;
+            } else {
+                groups[t]++;
+            }
+        }
+
+        int ans = 0;
+        for (int i = 1; i <= t; i++) {
+            ans += Math.min(groups[i - 1], groups[i]);
+        }
+        return ans;
     }
 }
