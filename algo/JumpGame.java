f5ef2c52e591e7b3fb7dd46e20b9d27d7a294e76
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-12-02 22:18:21 -0800

    jampgame

--- algo/JumpGame.java
+++ algo/JumpGame.java
@@ -21,3 +21,30 @@ class JumpGame {
         return false;
     }
 }
+
+class JumpGame2 {
+    int[] memo; 
+    final int UNKNOWN =0;
+    final int GOOD =1;
+    final int BAD =-1;
+    public boolean canJump(int[] nums) {
+        memo = new int[nums.length];
+        memo[memo.length-1]=GOOD;
+        return backtrack(nums, 0, memo);
+    }
+    
+    public boolean backtrack(int[] nums, int index, int[] memo) {
+        if (memo[index]!=UNKNOWN) {
+            return memo[index]==GOOD;
+        } else {
+            for (int i = index+1; i<= index+nums[index]; i++) {
+                if (backtrack(nums, i,memo)){
+                    memo[index]=GOOD;
+                    return true;
+                }
+            }
+        }
+        memo[index]=BAD;
+        return false;
+    }
+}
