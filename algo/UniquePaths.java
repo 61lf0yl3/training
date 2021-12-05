b83bc7874400936b64f5c911108d568bf20c91d4
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-12-04 18:28:25 -0800

    uniqurpaths

---
+++ algo/UniquePaths.java
@@ -0,0 +1,18 @@
+class UniquePaths {
+    //62. Unique Paths
+
+    public int uniquePaths(int m, int n) {
+        int[][] paths = new int[n+1][m+1];
+        paths[n][m]=1;
+        for (int r = 1; r < paths.length; r++) {
+            for (int c = 1; c < paths[0].length; c++) {
+               if (r==2 && c ==1 || r == 1 && c == 2 || r == 1 && c == 1) {
+                   paths[r][c] = 1;
+               } else {
+                   paths[r][c] = paths[r-1][c]+paths[r][c-1];
+               } 
+            }
+        }
+        return paths[n][m];
+    }
+}
