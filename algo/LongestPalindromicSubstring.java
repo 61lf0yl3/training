3aa8c42bb7b4778d896067eaa07fb6833c0550ea
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-12-06 12:04:57 -0800

    longestPalindrome3

--- algo/LongestPalindromicSubstring.java
+++ algo/LongestPalindromicSubstring.java
@@ -33,8 +33,8 @@ class LongestPalindromicSubstring {
     }
 
     // Approach 3: DP
-    // Time Complexity: O(N^3)
-    // Space Complexity: O(1)
+    // Time Complexity: O(N^2)
+    // Space Complexity: O(N^2)
     public String longestPalindrome2(String s) {
         String res = ""+s.charAt(0);
         boolean[][] isPalindrome = new boolean[s.length()][s.length()];
@@ -62,4 +62,32 @@ class LongestPalindromicSubstring {
         }
         return res;
     }
+
+    // Approach 4: Expand Around Center
+    // Time Complexity: O(N^2)
+    // Space Complexity: O(N^2)
+    public String longestPalindrome3(String s) {
+        if (s == null || s.length() < 1) return "";
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+    
+    private int expandAroundCenter(String s, int left, int right) {
+        int L = left, R = right;
+        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+            L--;
+            R++;
+        }
+        return R - L - 1;
+    }
+
 }
