1603f8ba542b6f4358a8c2925e547123fc56f44c
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-12-07 22:56:48 -0800

    .

--- algo/ArithmeticSlices.java
+++ algo/ArithmeticSlices.java
@@ -37,4 +37,25 @@ class ArithmeticSlices {
         }
         return count;
     }
+
+    // Approach 3: Recursion
+    // Time Complexity: O(N)
+    // Space Complexity: O(N)
+    int sum;
+    public int numberOfArithmeticSlices3(int[] A) {
+        sum = 0;
+        slices(A, A.length - 1);
+        return sum;
+    }
+    public int slices(int[] A, int i) {
+        if (i < 2)
+            return 0;
+        int ap = 0;
+        if (A[i] - A[i - 1] == A[i - 1] - A[i - 2]) {
+            ap = 1 + slices(A, i - 1);
+            sum += ap;
+        } else
+            slices(A, i - 1);
+        return ap;
+    }
 }
