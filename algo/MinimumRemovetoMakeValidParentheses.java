63493e55b22665fdfd719f1504193d11f4be5fee
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-12-09 00:12:08 -0500

    minRemoveToMakeValid

--- algo/MinimumRemovetoMakeValidParentheses.java
+++ algo/MinimumRemovetoMakeValidParentheses.java
@@ -37,4 +37,31 @@ class MinimumRemovetoMakeValidParentheses {
         }
         return res.toString();
     }
+
+    // Approach 2: Two Pass String Builder
+    // Time Complexity: O(N)
+    // Space Complexity: O(N)
+    public String minRemoveToMakeValid2(String s) {
+        StringBuilder result = removeInvalidClosing(s, '(', ')');
+        result = removeInvalidClosing(result.reverse(), ')', '(');
+        return result.reverse().toString();
+    }
+
+    private StringBuilder removeInvalidClosing(CharSequence string, char open, char close) {
+        StringBuilder sb = new StringBuilder();
+        int balance = 0;
+        for (int i = 0; i < string.length(); i++) {
+            char c = string.charAt(i);
+            if (c == open) {
+                balance++;
+            }
+            if (c == close) {
+                if (balance == 0)
+                    continue;
+                balance--;
+            }
+            sb.append(c);
+        }
+        return sb;
+    }
 }
