6aa840183ee4972517d3b15b84d349e32dec3a37
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-12-11 02:11:28 -0800

    numberOfArithmeticSlices5

--- algo/ArithmeticSlices.java
+++ algo/ArithmeticSlices.java
@@ -91,4 +91,21 @@ class ArithmeticSlices {
         }
         return sum;
     }
+
+    // Approach 4: Constant Space Dynamic Programming
+    // Time Complexity: O(N)
+    // Space Complexity: O(1)
+    public int numberOfArithmeticSlices6(int[] A) {
+        int count = 0;
+        int sum = 0;
+        for (int i = 2; i < A.length; i++) {
+            if (A[i] - A[i - 1] == A[i - 1] - A[i - 2]) {
+                count++;
+            } else {
+                sum += (count + 1) * (count) / 2;
+                count = 0;
+            }
+        }
+        return sum += count * (count + 1) / 2;
+    }
 }
