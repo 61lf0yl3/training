b40c2beb8e4ca1fc9141c3368d5ba5cc59daea6f
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-12-12 22:51:48 -0500

    BitwiseANDofNumbersRange

--- algo/BitwiseANDofNumbersRange.java
+++ algo/BitwiseANDofNumbersRange.java
@@ -1,11 +1,17 @@
 class BitwiseANDofNumbersRange {
     // 201. Bitwise AND of Numbers Range
 
+    // The idea is that we shift both numbers to the right, until the numbers become
+    // equal, i.e. the numbers are reduced into their common prefix. Then we append
+    // zeros to the common prefix in order to obtain the desired result, by shifting
+    // the common prefix to the left.
+
     // Approach 1: Bit Shift
     // Time Complexity: O(1)
     // Space Complexity: O(1)
     public int rangeBitwiseAnd(int left, int right) {
         int counter = 0;
+        // find the common 1-bits
         while (left != right) {
             left >>= 1;
             right >>= 1;
@@ -14,5 +20,16 @@ class BitwiseANDofNumbersRange {
         left = left << counter;
 
         return left;
+    }
+
+    // Approach 1: Brian Kernighan's Algorithm
+    // Time Complexity: O(1)
+    // Space Complexity: O(1)
+    public int rangeBitwiseAnd2(int left, int right) {
+        while (right > left) {
+            // turn off rightmost 1-bit
+            right &= right - 1;
+        }
+        return right & left;
     }
 }
