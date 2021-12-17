f4cd6041f867f34f5b95d45cf68b7cc3a873e8c2
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-12-17 16:04:56 -0500

    lengthOfLIS3

--- algo/LinkedList.java
+++ algo/LinkedList.java
@@ -1,143 +1,136 @@
 import java.util.List;
 import java.util.ListIterator;
 import java.util.NoSuchElementException;
+import java.util.Queue;
 
 /**
-   A linked list is a sequence of nodes with efficient
-   element insertion and removal. This class 
-   contains a subset of the methods of the standard
-   java.util.LinkedList class.
-*/
-public class LinkedList implements List<List<Integer>>
-{  
+ * A linked list is a sequence of nodes with efficient
+ * element insertion and removal. This class
+ * contains a subset of the methods of the standard
+ * java.util.LinkedList class.
+ */
+public class LinkedList implements List<List<Integer>>, Queue<Integer> {
    private Node first;
-   
-   /** 
-      Constructs an empty linked list.
-   */
-   public LinkedList()
-   {  
+
+   /**
+    * Constructs an empty linked list.
+    */
+   public LinkedList() {
       first = null;
    }
-   
+
    /**
-      Returns the first element in the linked list.
-      @return the first element in the linked list
-   */
-   public Object getFirst()
-   {  
-      if (first == null) { throw new NoSuchElementException(); }
+    * Returns the first element in the linked list.
+    * 
+    * @return the first element in the linked list
+    */
+   public Object getFirst() {
+      if (first == null) {
+         throw new NoSuchElementException();
+      }
       return first.data;
    }
 
    /**
-      Removes the first element in the linked list.
-      @return the removed element
-   */
-   public Object removeFirst()
-   {  
-      if (first == null) { throw new NoSuchElementException(); }
+    * Removes the first element in the linked list.
+    * 
+    * @return the removed element
+    */
+   public Object removeFirst() {
+      if (first == null) {
+         throw new NoSuchElementException();
+      }
       Object element = first.data;
       first = first.next;
       return element;
    }
 
    /**
-      Adds an element to the front of the linked list.
-      @param element the element to add
-   */
-   public void addFirst(Object element)
-   {  
+    * Adds an element to the front of the linked list.
+    * 
+    * @param element the element to add
+    */
+   public void addFirst(Object element) {
       Node newNode = new Node();
       newNode.data = element;
       newNode.next = first;
       first = newNode;
    }
-   
+
    /**
-      Returns an iterator for iterating through this list.
-      @return an iterator for iterating through this list
-   */
-   public ListIterator listIterator()
-   {  
+    * Returns an iterator for iterating through this list.
+    * 
+    * @return an iterator for iterating through this list
+    */
+   public ListIterator listIterator() {
       return new LinkedListIterator();
    }
-   
-   class Node
-   {  
+
+   class Node {
       public Object data;
       public Node next;
    }
 
-   class LinkedListIterator implements ListIterator
-   {  
+   class LinkedListIterator implements ListIterator {
       private Node position;
       private Node previous;
       private boolean isAfterNext;
 
       /**
-         Constructs an iterator that points to the front
-         of the linked list.
-      */
-      public LinkedListIterator()
-      {  
+       * Constructs an iterator that points to the front
+       * of the linked list.
+       */
+      public LinkedListIterator() {
          position = null;
          previous = null;
          isAfterNext = false;
       }
-      
+
       /**
-         Moves the iterator past the next element.
-         @return the traversed element
-      */
-      public Object next()
-      {  
-         if (!hasNext()) { throw new NoSuchElementException(); }
+       * Moves the iterator past the next element.
+       * 
+       * @return the traversed element
+       */
+      public Object next() {
+         if (!hasNext()) {
+            throw new NoSuchElementException();
+         }
          previous = position; // Remember for remove
          isAfterNext = true;
 
-         if (position == null)
-         {
+         if (position == null) {
             position = first;
-         }
-         else
-         {
+         } else {
             position = position.next;
          }
 
          return position.data;
       }
-      
+
       /**
-         Tests if there is an element after the iterator position.
-         @return true if there is an element after the iterator position
-      */
-      public boolean hasNext()
-      {  
-         if (position == null)
-         {
+       * Tests if there is an element after the iterator position.
+       * 
+       * @return true if there is an element after the iterator position
+       */
+      public boolean hasNext() {
+         if (position == null) {
             return first != null;
-         }
-         else
-         {
+         } else {
             return position.next != null;
          }
       }
-      
+
       /**
-         Adds an element before the iterator position
-         and moves the iterator past the inserted element.
-         @param element the element to add
-      */
-      public void add(Object element)
-      {  
-         if (position == null)
-         {
+       * Adds an element before the iterator position
+       * and moves the iterator past the inserted element.
+       * 
+       * @param element the element to add
+       */
+      public void add(Object element) {
+         if (position == null) {
             addFirst(element);
             position = first;
-         }
-         else
-         {  
+         } else {
             Node newNode = new Node();
             newNode.data = element;
             newNode.next = position.next;
@@ -147,21 +140,19 @@ public class LinkedList implements List<List<Integer>>
 
          isAfterNext = false;
       }
-      
-      /**
-         Removes the last traversed element. This method may
-         only be called after a call to the next() method.
-      */
-      public void remove()
-      {  
-         if (!isAfterNext) { throw new IllegalStateException(); }
 
-         if (position == first)
-         {
-            removeFirst();
+      /**
+       * Removes the last traversed element. This method may
+       * only be called after a call to the next() method.
+       */
+      public void remove() {
+         if (!isAfterNext) {
+            throw new IllegalStateException();
          }
-         else 
-         {  
+
+         if (position == first) {
+            removeFirst();
+         } else {
             previous.next = position.next;
          }
          position = previous;
@@ -169,14 +160,15 @@ public class LinkedList implements List<List<Integer>>
       }
 
       /**
-         Sets the last traversed element to a different value. 
-         @param element the element to set
-      */
-      public void set(Object element)
-      {
-         if (!isAfterNext) { throw new IllegalStateException(); }
+       * Sets the last traversed element to a different value.
+       * 
+       * @param element the element to set
+       */
+      public void set(Object element) {
+         if (!isAfterNext) {
+            throw new IllegalStateException();
+         }
          position.data = element;
       }
    }
 }
-
--- algo/LongestIncreasingSubsequence.java
+++ algo/LongestIncreasingSubsequence.java
@@ -2,6 +2,15 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 class LongestIncreasingSubsequence {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> nums = new ArrayList<>();
+        nums.add(0);
+        nums.add(2);
+        nums.add(3);
+        nums.add(4);
+        System.out.println(binarySearch(nums, 1));
+    }
     // 300. Longest Increasing Subsequence
 
     // Approach 1: DP
@@ -44,5 +53,39 @@ class LongestIncreasingSubsequence {
             }
         }
         return subset.size();
+    }
+
+    // Approach 2: Intelligently Build a Subsequence Improved With Binary Search
+    // Time Complexity: O(N^2)
+    // Space Complexity: O(N)
+    public int lengthOfLIS3(int[] nums) {
+        ArrayList<Integer> subset = new ArrayList<>();
+        subset.add(nums[0]);
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > subset.get(subset.size() - 1)) {
+                subset.add(nums[i]);
+            } else {
+                int j = binarySearch(subset, nums[i]);
+                subset.set(j, nums[i]);
+            }
+        }
+        return subset.size();
+    }
+
+    public int binarySearch(ArrayList<Integer> subset, int num) {
+        int low = 0;
+        int high = subset.size() - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (num > subset.get(mid)) {
+                low = mid + 1;
+            } else if (num < subset.get(mid)) {
+                high = mid;
+            } else {
+                return mid;
+            }
+        }
+        return low;
     }
 }
