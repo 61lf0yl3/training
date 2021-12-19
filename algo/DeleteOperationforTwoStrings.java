aedb0727ce529e73ff83a5d4685d5acc9be77f09
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-12-19 01:47:17 -0500

    .

--- algo/DeleteOperationforTwoStrings.java
+++ algo/DeleteOperationforTwoStrings.java
@@ -60,4 +60,22 @@ class DeleteOperationforTwoStrings {
             memo[m][n] = Math.max(lcs(s1, s2, m, n - 1, memo), lcs(s1, s2, m - 1, n, memo));
         return memo[m][n];
     }
+
+    // Approach 3: Longest Common Subsequence - DP [Accepted]
+    // Time Complexity: O(N*M)
+    // Space Complexity: O(N*M)
+    public int minDistance4(String s1, String s2) {
+        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+        for (int i = 0; i <= s1.length(); i++) {
+            for (int j = 0; j <= s2.length(); j++) {
+                if (i == 0 || j == 0)
+                    continue;
+                if (s1.charAt(i - 1) == s2.charAt(j - 1))
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return s1.length() + s2.length() - 2 * dp[s1.length()][s2.length()];
+    }
 }
