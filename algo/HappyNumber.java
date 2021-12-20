dd2decf05e26ed9ae9eff79656de9c73c74e7bca
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-12-20 15:48:20 -0500

    HappyNumber

--- algo/HappyNumber.java
+++ algo/HappyNumber.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -33,12 +34,25 @@ class HappyNumber {
     // Space Complexity: O(1)
     public boolean isHappy2(int n) {
         int slowRunner = n;
-        int fastRunner = getNext(n);
+        int fastRunner = getNextN(n);
         while (fastRunner != 1 && slowRunner != fastRunner) {
-            slowRunner = getNext(slowRunner);
-            fastRunner = getNext(getNext(fastRunner));
+            slowRunner = getNextN(slowRunner);
+            fastRunner = getNextN(getNextN(fastRunner));
         }
         return fastRunner == 1;
+    }
+
+    //// Approach 3: Hardcoding the Only Cycle
+    // Time Complexity: O(logN)
+    // Space Complexity: O(1)
+    //
+    private static Set<Integer> cycleMembers = new HashSet<>(Arrays.asList(4, 16, 37, 58, 89, 145, 42, 20));
+
+    public boolean isHappy3(int n) {
+        while (n != 1 && !cycleMembers.contains(n)) {
+            n = getNextN(n);
+        }
+        return n == 1;
     }
 
 }
