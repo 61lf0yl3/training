57c06fd3c5ae572fe3caac9a6e336b7e7ba67e17
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-12-22 16:35:30 -0500

    ValidParentheses

--- algo/ValidParentheses.java
+++ algo/ValidParentheses.java
@@ -1,7 +1,35 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Stack;
 
 class ValidParentheses {
+    // 20. Valid Parentheses
+
+    // Approach 1: Stacks
+    // Time Complexity: O(N)
+    // Space Complexity: O(N)
+    public boolean isValid(String s) {
+        Stack<Character> stck = new Stack<Character>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            char top = ' ';
+            if (!stck.isEmpty()) {
+                top = stck.peek();
+            }
+            if (c == '(' || c == '{' || c == '[') {
+                stck.add(c);
+            } else if (c == ')' && top == '(') {
+                stck.pop();
+            } else if (c == '}' && top == '{') {
+                stck.pop();
+            } else if (c == ']' && top == '[') {
+                stck.pop();
+            } else {
+                return false;
+            }
+        }
+        return stck.isEmpty();
+    }
 
     public boolean isValid2(String s) {
         Map<Character, Integer> m = new HashMap<>();
