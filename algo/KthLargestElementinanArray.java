074751324d57ca57b93f06dcaf106a0edb26fcc6
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-12-24 22:19:39 -0500

    findKthLargest

---
+++ algo/KthLargestElementinanArray.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+class KthLargestElementinanArray {
+    // 215. Kth Largest Element in an Array
+
+    // Approach 1: PriorityQueue
+    // Time Complexity: O(N)
+    // Space Complexity: O(k)
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> qp = new PriorityQueue<>();
+        for (int i = 0; i < nums.length; i++) {
+            qp.add(nums[i]);
+            if (qp.size() > k) {
+                qp.remove();
+            }
+        }
+        return qp.poll();
+    }
+
+}
