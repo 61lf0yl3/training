7ac26a6f83f946cfd759ab9cb50c227f2c37602e
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-12-25 02:02:12 -0500

    groupAnagrams2

--- algo/GroupAnagrams.java
+++ algo/GroupAnagrams.java
@@ -7,9 +7,9 @@ import java.util.Map;
 class GroupAnagrams {
     // 49. Group Anagrams
 
-    // Approach 1: = HashMap
+    // Approach 1: HashMap
     // Time Complexity: O(M*NlogN)
-    // Space Complexity: O(k)
+    // Space Complexity: O(M*N)
     public List<List<String>> groupAnagrams(String[] strs) {
         if (strs.length == 0) {
             return new ArrayList();
@@ -35,5 +35,34 @@ class GroupAnagrams {
             res.add(anagrams);
         }
         return res;
+    }
+
+    // Approach 2: Categorize by Count
+    // Time Complexity: O(M*NlogN)
+    // Space Complexity: O(M*N)
+    public List<List<String>> groupAnagrams2(String[] strs) {
+        Map<String, List<String>> m = new HashMap<>();
+        for (String s : strs) {
+            int[] chars = new int[26];
+            for (char c : s.toCharArray()) {
+                chars[c - 'a']++;
+            }
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 26; i++) {
+                while (chars[i] > 0) {
+                    sb.append((char) (i + 'a'));
+                    chars[i]--;
+                }
+            }
+            String key = sb.toString();
+            List<String> anagrams = new ArrayList<>();
+            if (m.containsKey(key)) {
+                anagrams = m.get(key);
+            } else {
+                m.put(key, anagrams);
+            }
+            anagrams.add(s);
+        }
+        return new ArrayList(m.values());
     }
 }
