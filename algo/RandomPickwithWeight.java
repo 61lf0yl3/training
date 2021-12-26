eb3019c6332158a613247fb788db3986672b12bf
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-12-26 02:42:39 -0500

    RandompickwithWeight

---
+++ algo/RandomPickwithWeight.java
@@ -0,0 +1,52 @@
+import java.util.Random;
+
+class RandomPickwithWeight {
+    int[] w;
+    int[] prefixSums;
+    Random r;
+    int prefixSum;
+
+    // Approach 1: Prefix Sums with Binary Search
+    // Time complexity : RandomPickwithWeight() O(N)
+    // pickIndex() O(logN)
+    // Space complexity : RandomPickwithWeight() O(N)
+    // pickIndex() O(1)
+    public RandomPickwithWeight(int[] w) {
+        this.w = w;
+        r = new Random();
+        prefixSums = new int[w.length];
+        prefixSum = 0;
+        for (int i = 0; i < w.length; i++) {
+            prefixSum += w[i];
+            prefixSums[i] = prefixSum;
+        }
+    }
+
+    public int pickIndex() {
+        int generated = r.nextInt(prefixSum);
+        return binarySearch(generated + 1);
+    }
+
+    private int binarySearch(int target) {
+        int low = 0;
+        int high = prefixSums.length - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (target == prefixSums[mid]) {
+                return mid;
+            } else if (target > prefixSums[mid]) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return low;
+    }
+
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(w);
+ * int param_1 = obj.pickIndex();
+ */
