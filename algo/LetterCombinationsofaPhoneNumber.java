20e332c175ee923ba85226e581205283a2f25cb2
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-12-25 21:02:46 -0500

    .

--- algo/LetterCombinationsofaPhoneNumber.java
+++ algo/LetterCombinationsofaPhoneNumber.java
@@ -2,16 +2,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-class LetterCombinationsofaPhoneNumber   {
-    
+class LetterCombinationsofaPhoneNumber {
 
-    //Approach #1: Backtrack with hashmap;
-    //Time Complexity: O(4^N)
-    //Space Complexity: O(N)
+    // Approach #1: Backtrack with hashmap;
+    // Time Complexity: O(4^N)
+    // Space Complexity: O(N)
     private Map<Character, String> letters = Map.of(
-        '2', "abc", '3', "def", '4', "ghi", '5', "jkl", 
-        '6', "mno", '7', "pqrs", '8', "tuv", '9', "wxyz");
-    
+            '2', "abc", '3', "def", '4', "ghi", '5', "jkl",
+            '6', "mno", '7', "pqrs", '8', "tuv", '9', "wxyz");
+
     public List<String> letterCombinations(String digits) {
         List<String> res = new ArrayList<>();
         if (digits.length() == 0) {
@@ -20,16 +19,16 @@ class LetterCombinationsofaPhoneNumber   {
         backtrack(digits, res, new StringBuilder(), 0);
         return res;
     }
-    
-    public void backtrack(String digits, List<String> res,StringBuilder comb, int start) {
-        if (comb.length()==digits.length()) {
+
+    public void backtrack(String digits, List<String> res, StringBuilder comb, int start) {
+        if (comb.length() == digits.length()) {
             res.add(comb.toString());
         } else {
             String c = letters.get(digits.charAt(start));
-            for (int i=0; i<c.length();i++) {
+            for (int i = 0; i < c.length(); i++) {
                 comb.append(c.charAt(i));
-                backtrack(digits, res, comb, start+1);
-                comb.deleteCharAt(comb.length()-1);
+                backtrack(digits, res, comb, start + 1);
+                comb.deleteCharAt(comb.length() - 1);
             }
         }
     }
--- algo/SearchinRotatedSortedArray.java
+++ algo/SearchinRotatedSortedArray.java
@@ -1,142 +1,147 @@
 class SearchinRotatedSortedArray {
-    //Approach 1: Binary search (myway)
-    //Time Complexity:O(logN)
-    //Space Complexity: O(1) 
-    public int search(int[] nums, int target) {
-        if (nums.length == 1) {
-            if (nums[0] == target) {
-                return 0;
-            } else {
-                return -1;
-            }
-        }
-        
-        int low = 0;
-        int high = nums.length-1;
-        int indexOfMax = -1;
-        while (low <= high) {
-            int mid = low + (high-low)/2;
-            if (mid == high || nums[mid] > nums[mid+1]) {
-                indexOfMax = mid;
-                break;
-            }
-            if (nums[mid] > nums[low]) {
-                low = mid;
-            } else {
-                high = mid;
-            }
-        }
-        if (indexOfMax == -1) {
-            return indexOfMax;
-        }
-        int res = binarySearch(nums, target, 0, indexOfMax);
-        if (res == -1) {
-            res = binarySearch(nums, target, indexOfMax+1, nums.length-1);
-        }
-        
-        return res;
-    }
-    
-    public int binarySearch(int[] nums, int target,int low,int high) {
-        while (low <= high) {
-            int mid = low + (high-low)/2;
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] > target) {
-                high = mid-1;
-            } else {
-                low = mid+1;
-            }
-        }
+  // Approach 1: Binary search (myway)
+  // Time Complexity:O(logN)
+  // Space Complexity: O(1)
+  public int search(int[] nums, int target) {
+    if (nums.length == 1) {
+      if (nums[0] == target) {
+        return 0;
+      } else {
         return -1;
+      }
     }
 
+    int low = 0;
+    int high = nums.length - 1;
+    int indexOfMax = -1;
+    while (low <= high) {
+      int mid = low + (high - low) / 2;
+      if (mid == high || nums[mid] > nums[mid + 1]) {
+        indexOfMax = mid;
+        break;
+      }
+      if (nums[mid] > nums[low]) {
+        low = mid;
+      } else {
+        high = mid;
+      }
+    }
+    if (indexOfMax == -1) {
+      return indexOfMax;
+    }
+    int res = binarySearch(nums, target, 0, indexOfMax);
+    if (res == -1) {
+      res = binarySearch(nums, target, indexOfMax + 1, nums.length - 1);
+    }
 
-class SearchinRotatedSortedArray2 {
-        //Approach 2: One-pass Binary Search
-        //Time Complexity:O(logN)
-        //Space Complexity: O(1) 
-        public int search3(int[] nums, int target) {
-          int start = 0, end = nums.length - 1;
-          while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (nums[mid] == target) return mid;
-            else if (nums[mid] >= nums[start]) {
-              if (target >= nums[start] && target < nums[mid]) end = mid - 1;
-              else start = mid + 1;
-            }
-            else {
-              if (target <= nums[end] && target > nums[mid]) start = mid + 1;
-              else end = mid - 1;
-            }
-          }
-          return -1;
-        }
+    return res;
+  }
 
-        int [] nums;
-        int target;
-      
-        //Approach 1: Binary search
-        //Time Complexity:O(logN)
-        //Space Complexity: O(1) 
-        public int find_rotate_index(int left, int right) {
-          if (nums[left] < nums[right])
-            return 0;
-      
-          while (left <= right) {
-            int pivot = (left + right) / 2;
-            if (nums[pivot] > nums[pivot + 1])
-              return pivot + 1;
-            else {
-              if (nums[pivot] < nums[left])
-                right = pivot - 1;
-              else
-                left = pivot + 1;
-            }
-          }
-          return 0;
+  public int binarySearch(int[] nums, int target, int low, int high) {
+    while (low <= high) {
+      int mid = low + (high - low) / 2;
+      if (nums[mid] == target) {
+        return mid;
+      } else if (nums[mid] > target) {
+        high = mid - 1;
+      } else {
+        low = mid + 1;
+      }
+    }
+    return -1;
+  }
+
+  // Approach 2: One-pass Binary Search
+  // Time Complexity:O(logN)
+  // Space Complexity: O(1)
+  public int search2(int[] nums, int target) {
+    int low = 0;
+    int high = nums.length - 1;
+    while (low <= high) {
+      int mid = low + (high - low) / 2;
+      int midNb = nums[mid];
+      if (nums[mid] == target) {
+        return mid;
+      } else if (nums[mid] >= nums[0]) {
+        if (target < nums[mid] && target >= nums[0]) {
+          high = mid - 1;
+        } else {
+          low = mid + 1;
         }
-      
-        public int search(int left, int right) {
-          /*
-          Binary search
-          */
-          while (left <= right) {
-            int pivot = (left + right) / 2;
-            if (nums[pivot] == target)
-              return pivot;
-            else {
-              if (target < nums[pivot])
-                right = pivot - 1;
-              else
-                left = pivot + 1;
-            }
-          }
-          return -1;
-        }
-      
-        public int search(int[] nums, int target) {
-          this.nums = nums;
-          this.target = target;
-      
-          int n = nums.length;
-      
-          if (n == 1)
-            return this.nums[0] == target ? 0 : -1;
-      
-          int rotate_index = find_rotate_index(0, n - 1);
-      
-          // if target is the smallest element
-          if (nums[rotate_index] == target)
-            return rotate_index;
-          // if array is not rotated, search in the entire array
-          if (rotate_index == 0)
-            return search(0, n - 1);
-          if (target < nums[0])
-            // search in the right side
-            return search(rotate_index, n - 1);
-          // search in the left side
-          return search(0, rotate_index);
+      } else {
+        if (target > nums[mid] && target < nums[0]) {
+          low = mid + 1;
+        } else {
+          high = mid - 1;
         }
       }
+    }
+    return -1;
+  }
+
+  int[] nums;
+  int target;
+
+  // Approach 1: Binary search
+  // Time Complexity:O(logN)
+  // Space Complexity: O(1)
+  public int find_rotate_index(int left, int right) {
+    if (nums[left] < nums[right])
+      return 0;
+
+    while (left <= right) {
+      int pivot = (left + right) / 2;
+      if (nums[pivot] > nums[pivot + 1])
+        return pivot + 1;
+      else {
+        if (nums[pivot] < nums[left])
+          right = pivot - 1;
+        else
+          left = pivot + 1;
+      }
+    }
+    return 0;
+  }
+
+  public int search(int left, int right) {
+    /*
+     * Binary search
+     */
+    while (left <= right) {
+      int pivot = (left + right) / 2;
+      if (nums[pivot] == target)
+        return pivot;
+      else {
+        if (target < nums[pivot])
+          right = pivot - 1;
+        else
+          left = pivot + 1;
+      }
+    }
+    return -1;
+  }
+
+  public int search3(int[] nums, int target) {
+    this.nums = nums;
+    this.target = target;
+
+    int n = nums.length;
+
+    if (n == 1)
+      return this.nums[0] == target ? 0 : -1;
+
+    int rotate_index = find_rotate_index(0, n - 1);
+
+    // if target is the smallest element
+    if (nums[rotate_index] == target)
+      return rotate_index;
+    // if array is not rotated, search in the entire array
+    if (rotate_index == 0)
+      return search(0, n - 1);
+    if (target < nums[0])
+      // search in the right side
+      return search(rotate_index, n - 1);
+    // search in the left side
+    return search(0, rotate_index);
+  }
 }
