620c89529103d1272a5b1623fa443925c325c039
61lf0yl3 <chingiz.samigullin97@gmail.com>
2021-12-27 01:51:35 -0500

    MinimumKnightMoves

--- algo/MinimumKnightMoves.java
+++ algo/MinimumKnightMoves.java
@@ -1,5 +1,7 @@
 import java.util.Queue;
 import java.util.ArrayDeque;
+import java.util.HashMap;
+import java.util.Map;
 
 class MinimumKnightMoves {
 
@@ -37,5 +39,32 @@ class MinimumKnightMoves {
             step++;
         }
         return step;
+    }
+
+    // Approach 1: DFS
+    // Time complexity : O(|x|*|y|)
+    // Space complexity : O(|x|*|y|)
+    private Map<String, Integer> memo = new HashMap<>();
+
+    private int dfs(int x, int y) {
+        String key = x + "," + y;
+        if (memo.containsKey(key)) {
+            return memo.get(key);
+        }
+
+        if (x + y == 0) {
+            return 0;
+        } else if (x + y == 2) {
+            return 2;
+        } else {
+            Integer ret = Math.min(dfs(Math.abs(x - 1), Math.abs(y - 2)),
+                    dfs(Math.abs(x - 2), Math.abs(y - 1))) + 1;
+            memo.put(key, ret);
+            return ret;
+        }
+    }
+
+    public int minKnightMoves2(int x, int y) {
+        return dfs(Math.abs(x), Math.abs(y));
     }
 }
