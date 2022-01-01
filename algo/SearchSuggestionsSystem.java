41ba34e4b1a9906ae272be03b3522ab91a6d7dad
61lf0yl3 <chingiz.samigullin97@gmail.com>
2022-01-01 13:49:30 -0500

    .

--- algo/SearchSuggestionsSystem.java
+++ algo/SearchSuggestionsSystem.java
@@ -5,7 +5,7 @@ import java.util.List;
 class SearchSuggestionsSystem {
 
     // Approach 1: Binary Search
-    // Time complexity : O(NlogM + MlogN)
+    // Time complexity : O(NlogM + MlogN)s
     // Space complexity : O(N) because we use merge sort algo;
     public List<List<String>> suggestedProducts(String[] products, String searchWord) {
         Arrays.sort(products);
@@ -14,9 +14,16 @@ class SearchSuggestionsSystem {
         for (int i = 0; i < searchWord.length(); i++) {
             char c = searchWord.charAt(i);
             search += c;
+            // Get the starting index of word starting with `search`
             int start = binary(products, search);
+
+            // Add empty vector to result.
             res.add(new ArrayList<>());
             List<String> suggested = res.get(i);
+
+            // Add the words with the same search to the result.
+            // Loop runs until `i` reaches the end of input or 3 times or till the
+            // prefix is same for `products[i]` Whichever comes first.
             for (int j = start; j < Math.min(products.length, start + 3); j++) {
                 if (products[j].length() < search.length()) {
                     break;
