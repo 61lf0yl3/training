162e3d0e6c8e7064cd6a0d21b6be1a8645220597
61lf0yl3 <chingiz.samigullin97@gmail.com>
2022-01-01 18:24:51 -0500

    WordDictionary

--- algo/DesignAddandSearchWordsDataStructure.java
+++ algo/DesignAddandSearchWordsDataStructure.java
@@ -49,6 +49,68 @@ class WordDictionary {
     }
 }
 
+// Approach 1: Using TrieNode
+// Time complexity : (M) for add where M is a length of the word; O(M) for
+// search where M is a length of the word
+// Space complexity : (M) for add where M is a length of the word; O(1) for
+// search;
+class TrieNode {
+    Map<Character, TrieNode> children = new HashMap<>();
+    boolean word = false; // ?
+
+    public TrieNode() {
+    }
+}
+
+class WordDictionary2 {
+    TrieNode trie;
+
+    public WordDictionary2() {
+        trie = new TrieNode();
+    }
+
+    public void addWord(String word) {
+        TrieNode node = trie;
+        for (char c : word.toCharArray()) {
+            if (!node.children.containsKey(c)) {
+                node.children.put(c, new TrieNode());
+            }
+            node = node.children.get(c);
+        }
+        node.word = true;
+    }
+
+    public boolean search(String word) {
+        return searchR(word, trie);
+    }
+
+    public boolean searchR(String word, TrieNode node) {
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (!node.children.containsKey(c)) {
+                if (c == '.') {
+                    for (char x : node.children.keySet()) {
+                        if (searchR(word.substring(i + 1), node.children.get(x))) {
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            } else {
+                node = node.children.get(c);
+            }
+        }
+        return node.word;
+    }
+}
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary obj = new WordDictionary();
+ * obj.addWord(word);
+ * boolean param_2 = obj.search(word);
+ */
+
 /**
  * Your WordDictionary object will be instantiated and called as such:
  * WordDictionary obj = new WordDictionary();
