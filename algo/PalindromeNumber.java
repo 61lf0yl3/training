cfce40881ceac62de87002c7020eb39f0e9828fd
61lf0yl3 <chingiz.samigullin97@gmail.com>
2022-01-02 00:36:50 -0500

    PalindromeNumber

---
+++ algo/PalindromeNumber.java
@@ -0,0 +1,17 @@
+class PalindromeNumber {
+    // Approach 1: Revert half of the number
+    // Time complexity : O(logN)
+    // Space complexity : O(1)
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+        int revertedNumber = 0;
+        while (x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+
+        return x == revertedNumber || x == revertedNumber / 10;
+    }
+}
