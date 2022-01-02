fe0e6844774cf0e0d941a257606ab51864671b5c
61lf0yl3 <chingiz.samigullin97@gmail.com>
2022-01-02 14:49:07 -0500

    MinimumMovestoEqualArrayElement

---
+++ algo/MinimumMovestoEqualArrayElements.java
@@ -0,0 +1,42 @@
+class MinimumMovestoEqualArrayElements {
+    // 453. Minimum Moves to Equal Array Elements
+
+    // Approach 1: Brute Force (TLE)
+    // Time complexity : O(N^2)
+    // Space complexity : O(1)
+    public int minMoves(int[] nums) {
+        int res = 0;
+        int max = findMax(nums);
+        int min = findMin(nums);
+        while (min < max) {
+            res++;
+            int counter = 1;
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] == max && counter == 1) {
+                    counter--;
+                    continue;
+                }
+                nums[i]++;
+            }
+            max = findMax(nums);
+            min = findMin(nums);
+        }
+        return res;
+    }
+
+    private int findMax(int[] nums) {
+        int max = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            max = Math.max(max, nums[i]);
+        }
+        return max;
+    }
+
+    private int findMin(int[] nums) {
+        int min = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            min = Math.min(min, nums[i]);
+        }
+        return min;
+    }
+}
