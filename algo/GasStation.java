3cd4098ebcd05b33c772c691335b712b1efbb83d
61lf0yl3 <chingiz.samigullin97@gmail.com>
2022-01-02 17:45:54 -0500

    GasStation

--- algo/GasStation.java
+++ algo/GasStation.java
@@ -1,9 +1,31 @@
 class GasStation {
 
+    // Approach 1: One pass
+    // Time complexity : O(N)
+    // Space complexity : O(1)
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int startingStaton = 0;
+        int totalTank = 0;
+        int currTank = 0;
+        for (int i = 0; i < gas.length; i++) {
+            currTank += gas[i] - cost[i];
+            totalTank += gas[i] - cost[i];
+
+            // If one couldn't get here,
+            if (currTank < 0) {
+                // Pick up the next station as the starting one.
+                startingStaton = i + 1;
+                // Start with an empty tank.
+                currTank = 0;
+            }
+        }
+        return totalTank >= 0 ? startingStaton : -1;
+    }
+
     // Approach 2: Brute Force
     // Time complexity : O(N^2)
     // Space complexity : O(1)
-    public int canCompleteCircuit(int[] gas, int[] cost) {
+    public int canCompleteCircuit2(int[] gas, int[] cost) {
         int n = gas.length;
         for (int i = 0; i < n; i++) {
             int tank = 0;
