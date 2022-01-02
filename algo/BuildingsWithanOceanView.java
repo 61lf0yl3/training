c95f25a9e9b8b8b5bd598fbca45f52e143af1f35
61lf0yl3 <chingiz.samigullin97@gmail.com>
2022-01-02 00:04:41 -0500

    BuildingsWithanOceanView

--- algo/BuildingsWithanOceanView.java
+++ algo/BuildingsWithanOceanView.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 class BuildingsWithanOceanView {
     // Approach 1: Monotonic Stack Space Optimization
@@ -21,7 +22,7 @@ class BuildingsWithanOceanView {
         return res;
     }
 
-    // Approach 1: Linear Iteration
+    // Approach 2: Linear Iteration
     // Time complexity : O(N)
     // Space complexity : O(N)
     public int[] findBuildings2(int[] heights) {
@@ -42,6 +43,40 @@ class BuildingsWithanOceanView {
         int[] answer = new int[list.size()];
         for (int i = 0; i < list.size(); ++i) {
             answer[i] = list.get(i);
+        }
+
+        return answer;
+    }
+
+    // Approach 3: Monotonic Stack
+    // Time complexity : O(N)
+    // Space complexity : O(N)
+    public int[] findBuildings3(int[] heights) {
+        int n = heights.length;
+        List<Integer> list = new ArrayList<>();
+
+        // Monotonically decreasing stack.
+        Stack<Integer> stack = new Stack<>();
+        for (int current = n - 1; current >= 0; --current) {
+            // If the building to the right is smaller, we can pop it.
+            while (!stack.isEmpty() && heights[stack.peek()] < heights[current]) {
+                stack.pop();
+            }
+
+            // If the stack is empty, it means there is no building to the right
+            // that can block the view of the current building.
+            if (stack.isEmpty()) {
+                list.add(current);
+            }
+
+            // Push the current building in the stack.
+            stack.push(current);
+        }
+
+        // Push elements from list to answer array in reverse order.
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); ++i) {
+            answer[i] = list.get(list.size() - 1 - i);
         }
 
         return answer;
