f3b7517fcb0cc7da3f61649a3e5b8993f875f0a3
61lf0yl3 <chingiz.samigullin97@gmail.com>
2022-01-04 14:09:50 -0500

    AllNodesDistanceKinBinaryTree

--- algo/AllNodesDistanceKinBinaryTree.java
+++ algo/AllNodesDistanceKinBinaryTree.java
@@ -17,11 +17,10 @@ import java.util.Set;
  */
 class AllNodesDistanceKinBinaryTree {
     // 863. All Nodes Distance K in Binary Tree
-ы
     // Approach 1: Annotate Parent
     // Time complexity : O(logN)
     // Space complexity : O(1)
-    Map<TreeNode,TreeNode>m;
+    Map<TreeNode, TreeNode> m;
 
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
         m = new HashMap<>();
@@ -69,5 +68,57 @@ class AllNodesDistanceKinBinaryTree {
         m.put(child, parent);
         dfs(child.left, child);
         dfs(child.right, child);
+    }
+}
+
+class AllNodesDistanceKinBinaryTree2 {
+    List<Integer> ans;
+    TreeNode target;
+    int K;
+
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int K) {
+        ans = new LinkedList();
+        this.target = target;
+        this.K = K;
+        dfs(root);
+        return ans;
+    }
+
+    // Return vertex distance from node to target if exists, else -1
+    // Vertex distance: the number of vertices on the path from node to target
+    public int dfs(TreeNode node) {
+        if (node == null)
+            return -1;
+        else if (node == target) {
+            subtree_add(node, 0);
+            return 1;
+        } else {
+            int L = dfs(node.left), R = dfs(node.right);
+            if (L != -1) {
+                if (L == K)
+                    ans.add(node.val);
+                subtree_add(node.right, L + 1);
+                return L + 1;
+            } else if (R != -1) {
+                if (R == K)
+                    ans.add(node.val);
+                subtree_add(node.left, R + 1);
+                return R + 1;
+            } else {
+                return -1;
+            }
+        }
+    }
+
+    // Add all nodes 'K - dist' from the node to answer.
+    public void subtree_add(TreeNode node, int dist) {
+        if (node == null)
+            return;
+        if (dist == K)
+            ans.add(node.val);
+        else {
+            subtree_add(node.left, dist + 1);
+            subtree_add(node.right, dist + 1);
+        }
     }
 }
