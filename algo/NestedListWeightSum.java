891b01d2efe48740305684192ece999c749eb365
61lf0yl3 <chingiz.samigullin97@gmail.com>
2022-01-04 22:01:52 -0500

    NestedListWeightSum

--- algo/NestedListWeightSum.java
+++ algo/NestedListWeightSum.java
@@ -1,4 +1,6 @@
+import java.util.ArrayDeque;
 import java.util.List;
+import java.util.Queue;
 
 // This is the interface that allows for creating nested lists.
 // You should not implement it, or speculate about its implementation
@@ -47,7 +49,7 @@ class NestedListWeightSum {
 
     // Approach 1: Recursive
     // Time complexity : O(N)
-    // Space complexity : O(1)
+    // Space complexity : O(N)
     int res;
 
     public int depthSum(List<NestedInteger> nestedList) {
@@ -66,6 +68,40 @@ class NestedListWeightSum {
             } else {
                 depthSumR(nestedList.get(j).getList(), 0, depth + 1);
             }
+        }
+    }
+
+    // Approach 2: DFS
+    // Time complexity : O(N)
+    // Space complexity : O(N)
+    private void dfs(List<NestedInteger> nestedList, int depth) {
+        for (NestedInteger n : nestedList) {
+            if (n.isInteger()) {
+                res += n.getInteger() * depth;
+            } else {
+                dfs(n.getList(), depth + 1);
+            }
+        }
+    }
+
+    // Approach 3: BFS
+    // Time complexity : O(N)
+    // Space complexity : O(N)
+    private void bfs(List<NestedInteger> nestedList) {
+        int depth = 1;
+        Queue<NestedInteger> q = new ArrayDeque();
+        q.addAll(nestedList);
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                NestedInteger n = q.poll();
+                if (n.isInteger()) {
+                    res += n.getInteger() * depth;
+                } else {
+                    q.addAll(n.getList());
+                }
+            }
+            depth++;
         }
     }
 }
