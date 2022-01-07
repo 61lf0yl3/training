64c7b0ca080587cb0b185b6f6897a2069b4cb7a1
61lf0yl3 <chingiz.samigullin97@gmail.com>
2022-01-07 16:46:54 -0500

    HouseRobber

--- algo/HouseRobber.java
+++ algo/HouseRobber.java
@@ -21,9 +21,9 @@ class HouseRobber {
         return res;
     }
 
-    // Approach 1: Recursion
+    // Approach 2: Recursion
     // Time Complexity: O(N)
-    // Space Complexity: O(N)
+    // Space Complexity: O(1)
     int res;
 
     public int rob2(int[] nums) {
@@ -41,6 +41,41 @@ class HouseRobber {
             robR(nums, i + 2, sum + nums[i]);
             robR(nums, i + 3, sum + nums[i]);
         }
+    }
+
+    // Approach 2: Recursion
+    // Time Complexity: O(N)
+    // Space Complexity: O(N)
+    private int[] memo;
+
+    public int rob3(int[] nums) {
+
+        this.memo = new int[100];
+
+        // Fill with sentinel value representing not-calculated recursions.
+        Arrays.fill(this.memo, -1);
+
+        return this.robFrom(0, nums);
+    }
+
+    private int robFrom(int i, int[] nums) {
+
+        // No more houses left to examine.
+        if (i >= nums.length) {
+            return 0;
+        }
+
+        // Return cached value.
+        if (this.memo[i] > -1) {
+            return this.memo[i];
+        }
+
+        // Recursive relation evaluation to get the optimal answer.
+        int ans = Math.max(this.robFrom(i + 1, nums), this.robFrom(i + 2, nums) + nums[i]);
+
+        // Cache for future use.
+        this.memo[i] = ans;
+        return ans;
     }
 
 }
