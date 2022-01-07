8045422d842513adb395118de09ccac4d3ec75c9
61lf0yl3 <chingiz.samigullin97@gmail.com>
2022-01-07 16:36:57 -0500

    HouseRobber

--- algo/HouseRobber.java
+++ algo/HouseRobber.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 class HouseRobber {
     // 198. House Robber
 
@@ -17,6 +19,28 @@ class HouseRobber {
             old = prevRes;
         }
         return res;
+    }
+
+    // Approach 1: Recursion
+    // Time Complexity: O(N)
+    // Space Complexity: O(N)
+    int res;
+
+    public int rob2(int[] nums) {
+        res = 0;
+        robR(nums, 0, 0);
+        return res;
+    }
+
+    public void robR(int[] nums, int index, int sum) {
+        if (index >= nums.length) {
+            res = Math.max(res, sum);
+            return;
+        }
+        for (int i = index; i < nums.length; i++) {
+            robR(nums, i + 2, sum + nums[i]);
+            robR(nums, i + 3, sum + nums[i]);
+        }
     }
 
 }
