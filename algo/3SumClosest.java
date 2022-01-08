4a934ad6fd89286bdb97d4c428e18926c34f552a
61lf0yl3 <chingiz.samigullin97@gmail.com>
2022-01-07 23:07:08 -0500

    3SumClosest

--- algo/3SumClosest.java
+++ algo/3SumClosest.java
@@ -20,4 +20,31 @@ class 3SumClosest {
         }
         return res;
     }
+
+    // Approach 2: Brute Force
+    // Time Complexity: O(N^3)
+    // Space Complexity: O(1)
+    public int threeSumClosest(int[] nums, int target) {
+        int res = 0;
+        int diff = Integer.MAX_VALUE;
+        
+        Arrays.sort(nums);
+        for (int mid = 0; mid < nums.length; mid++) {
+            int low = mid+1;
+            int high = nums.length-1;
+            while (low < high) {
+                int sum = nums[low] + nums[mid] + nums[high];
+                if (diff > Math.abs(target-sum)){
+                    diff = Math.abs(target-sum);
+                    res = sum;
+                }
+                if (sum > target) {
+                    high--;
+                } else {
+                    low++;
+                }
+            }
+        }
+        return res;
+    }
 }
