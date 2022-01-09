2cbedfa4f5942076cacd6e44e4b9553e7488923a
61lf0yl3 <chingiz.samigullin97@gmail.com>
2022-01-09 15:35:18 -0500

    MaxStack

---
+++ algo/MaxStack.java
@@ -0,0 +1,58 @@
+import java.util.Stack;
+
+class MaxStack {
+    // 716. Max Stack
+
+    // Approach 1: Using Two Stack
+    // Time Complexity: O(N)
+    // Space Complexity: O(N)
+    Stack<Integer> stack;
+    Stack<Integer> maxStack;
+
+    public MaxStack() {
+        stack = new Stack<>();
+        maxStack = new Stack<>();
+    }
+
+    public void push(int x) {
+        int max = maxStack.isEmpty() ? x : maxStack.peek();
+        maxStack.push(max > x ? max : x);
+        stack.push(x);
+    }
+
+    public int pop() {
+        maxStack.pop();
+        return stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int peekMax() {
+        return maxStack.peek();
+    }
+
+    public int popMax() {
+        int max = peekMax();
+        Stack<Integer> buffer = new Stack<>();
+        while (top() != max) {
+            buffer.push(pop());
+        }
+        pop();
+        while (!buffer.isEmpty()) {
+            push(buffer.pop());
+        }
+        return max;
+    }
+}
+
+/**
+ * Your MaxStack object will be instantiated and called as such:
+ * MaxStack obj = new MaxStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.peekMax();
+ * int param_5 = obj.popMax();
+ */
