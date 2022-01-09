2acb41773c4adb492c7e4400840561db55b3f860
61lf0yl3 <chingiz.samigullin97@gmail.com>
2022-01-09 14:34:39 -0500

    MergeSortedArray

--- algo/MergeSortedArray.java
+++ algo/MergeSortedArray.java
@@ -7,20 +7,24 @@ class MergeSortedArray {
     // Time Complexity: O(m+n)
     // Space Complexity: O(1)
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        // Set p1 and p2 to point to the end of their respective arrays.
-        int p1 = m - 1;
-        int p2 = n - 1;
+        if (nums1.length == m) {
+            return;
+        }
 
-        // And move p backwards through the array, each time writing
-        // the smallest value pointed at by p1 or p2.
-        for (int p = m + n - 1; p >= 0; p--) {
-            if (p2 < 0) {
-                break;
-            }
-            if (p1 >= 0 && nums1[p1] > nums2[p2]) {
-                nums1[p] = nums1[p1--];
-            } else {
-                nums1[p] = nums2[p2--];
+        int j1 = m - 1;
+        int j2 = n - 1;
+
+        for (int i = m + n - 1; i >= 0; i--) {
+            if (j1 >= 0 && j2 >= 0) {
+                if (nums1[j1] > nums2[j2]) {
+                    nums1[i] = nums1[j1--];
+                } else {
+                    nums1[i] = nums2[j2--];
+                }
+            } else if (j1 >= 0) {
+                nums1[i] = nums1[j1--];
+            } else if (j2 >= 0) {
+                nums1[i] = nums2[j2--];
             }
         }
     }
