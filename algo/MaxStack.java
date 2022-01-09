5a649d037240667a207cd651b3f4341521ff8bc2
61lf0yl3 <chingiz.samigullin97@gmail.com>
2022-01-09 15:48:57 -0500

    MaxStack

--- algo/MaxStack.java
+++ algo/MaxStack.java
@@ -1,11 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
+import java.util.TreeMap;
 
 class MaxStack {
     // 716. Max Stack
 
     // Approach 1: Using Two Stack
-    // Time Complexity: O(N)
-    // Space Complexity: O(N)
+    // Time Complexity: O(N) for the popMax operation, and O(1) for the other
+    // operations, where N is the number of operations performed.
+    // Space Complexity: O(N) the maximum size of the stack.
     Stack<Integer> stack;
     Stack<Integer> maxStack;
 
@@ -44,6 +48,92 @@ class MaxStack {
             push(buffer.pop());
         }
         return max;
+    }
+}
+
+class MaxStack2 {
+    TreeMap<Integer, List<Node>> map;
+    DoubleLinkedList dll;
+
+    public MaxStack2() {
+        map = new TreeMap();
+        dll = new DoubleLinkedList();
+    }
+
+    public void push(int x) {
+        Node node = dll.add(x);
+        if (!map.containsKey(x))
+            map.put(x, new ArrayList<Node>());
+        map.get(x).add(node);
+    }
+
+    public int pop() {
+        int val = dll.pop();
+        List<Node> L = map.get(val);
+        L.remove(L.size() - 1);
+        if (L.isEmpty())
+            map.remove(val);
+        return val;
+    }
+
+    public int top() {
+        return dll.peek();
+    }
+
+    public int peekMax() {
+        return map.lastKey();
+    }
+
+    public int popMax() {
+        int max = peekMax();
+        List<Node> L = map.get(max);
+        Node node = L.remove(L.size() - 1);
+        dll.unlink(node);
+        if (L.isEmpty())
+            map.remove(max);
+        return max;
+    }
+}
+
+class DoubleLinkedList {
+    Node head, tail;
+
+    public DoubleLinkedList() {
+        head = new Node(0);
+        tail = new Node(0);
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public Node add(int val) {
+        Node x = new Node(val);
+        x.next = tail;
+        x.prev = tail.prev;
+        tail.prev = tail.prev.next = x;
+        return x;
+    }
+
+    public int pop() {
+        return unlink(tail.prev).val;
+    }
+
+    public int peek() {
+        return tail.prev.val;
+    }
+
+    public Node unlink(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+        return node;
+    }
+}
+
+class Node {
+    int val;
+    Node prev, next;
+
+    public Node(int v) {
+        val = v;
     }
 }
 
