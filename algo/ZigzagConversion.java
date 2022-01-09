30b1bcdafdabc3519d38ba29830f9805bba3ff8d
61lf0yl3 <chingiz.samigullin97@gmail.com>
2022-01-09 01:14:42 -0500

    ZigzagConversion

--- algo/ZigzagConversion.java
+++ algo/ZigzagConversion.java
@@ -60,4 +60,26 @@ class ZigzagConversion {
             ret.append(row);
         return ret.toString();
     }
+
+    // Approach 1: Visit by Row
+    // Time Complexity: O(N)
+    // Space Complexity: O(N)
+    public String convert3(String s, int numRows) {
+
+        if (numRows == 1)
+            return s;
+
+        StringBuilder ret = new StringBuilder();
+        int n = s.length();
+        int cycleLen = 2 * numRows - 2;
+
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j + i < n; j += cycleLen) {
+                ret.append(s.charAt(j + i));
+                if (i != 0 && i != numRows - 1 && j + cycleLen - i < n)
+                    ret.append(s.charAt(j + cycleLen - i));
+            }
+        }
+        return ret.toString();
+    }
 }
