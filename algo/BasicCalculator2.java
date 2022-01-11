797ebbc90edbc811491e6f3b485e92aa23a49538
61lf0yl3 <chingiz.samigullin97@gmail.com>
2022-01-11 15:07:25 -0500

    BasicCalculator2

--- algo/BasicCalculator2.java
+++ algo/BasicCalculator2.java
@@ -1,6 +1,7 @@
 import java.util.Stack;
 
-class Solution {
+class BasicCalculator2 {
+
     // Approach 1: Using Stack
     // Time Complexity: O(N)
     // Space Complexity: O(N)
@@ -51,4 +52,42 @@ class Solution {
         }
         return nums.pop();
     }
+
+    // Approach 1: Using Stack
+    // Time Complexity: O(N)
+    // Space Complexity: O(N)
+    public int calculate2(String s) {
+
+        if (s == null || s.isEmpty())
+            return 0;
+        int len = s.length();
+        Stack<Integer> stack = new Stack<Integer>();
+        int currentNumber = 0;
+        char operation = '+';
+        for (int i = 0; i < len; i++) {
+            char currentChar = s.charAt(i);
+            if (Character.isDigit(currentChar)) {
+                currentNumber = (currentNumber * 10) + (currentChar - '0');
+            }
+            if (!Character.isDigit(currentChar) && !Character.isWhitespace(currentChar) || i == len - 1) {
+                if (operation == '-') {
+                    stack.push(-currentNumber);
+                } else if (operation == '+') {
+                    stack.push(currentNumber);
+                } else if (operation == '*') {
+                    stack.push(stack.pop() * currentNumber);
+                } else if (operation == '/') {
+                    stack.push(stack.pop() / currentNumber);
+                }
+                operation = currentChar;
+                currentNumber = 0;
+            }
+        }
+        int result = 0;
+        while (!stack.isEmpty()) {
+            result += stack.pop();
+        }
+        return result;
+    }
+
 }
