b7f1a0bb3322024569baf122f2e9d359aa142d18
61lf0yl3 <chingiz.samigullin97@gmail.com>
2022-01-14 18:33:39 -0800

    MinCostClimbingStairs.java

---
+++ algo/MinCostClimbingStairs.java
@@ -0,0 +1,22 @@
+class MinCostClimbingStairs {
+
+    // Approach 1: DP
+    // Time Complexity: O(N)
+    // Space Complexity: O(N)
+    public int minCostClimbingStairs(int[] cost) {
+        if (cost.length == 2) {
+            return Math.min(cost[0], cost[1]);
+        }
+        int[] dp = new int[cost.length];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for (int i = 2; i < cost.length; i++) {
+            if (i == cost.length - 1) {
+                dp[i] = Math.min(dp[i - 1], dp[i - 2] + cost[i]);
+            } else {
+                dp[i] = Math.min(dp[i - 1], dp[i - 2]) + cost[i];
+            }
+        }
+        return dp[cost.length - 1];
+    }
+}
