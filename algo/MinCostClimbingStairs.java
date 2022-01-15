7609550ac9b2cc34564f9199b4176091299b10a1
61lf0yl3 <chingiz.samigullin97@gmail.com>
2022-01-14 19:11:25 -0800

    MinCostClimbingStairs.java

--- algo/MinCostClimbingStairs.java
+++ algo/MinCostClimbingStairs.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class MinCostClimbingStairs {
 
     // Approach 1: DP
@@ -18,5 +21,34 @@ class MinCostClimbingStairs {
             }
         }
         return dp[cost.length - 1];
+    }
+
+    // Approach 1: DP
+    // Time Complexity: O(N)
+    // Space Complexity: O(N)
+    int[] cost;
+    Map<Integer, Integer> m;
+
+    public int minCostClimbingStairs2(int[] cost) {
+        if (cost.length == 2) {
+            return Math.min(cost[0], cost[1]);
+        }
+        this.cost = cost;
+        m = new HashMap<>();
+        return DP(cost.length);
+    }
+
+    private int DP(int i) {
+        Map<Integer, Integer> temp = m;
+        if (i <= 1) {
+            return 0;
+        }
+        if (m.containsKey(i)) {
+            return m.get(i);
+        }
+        int downOne = DP(i - 1) + cost[i - 1];
+        int downTwo = DP(i - 2) + cost[i - 2];
+        m.put(i, Math.min(downOne, downTwo));
+        return m.get(i);
     }
 }
