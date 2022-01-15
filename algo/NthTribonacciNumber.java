30851d08dcbafebab67057f4ec1b38662e0440f9
61lf0yl3 <chingiz.samigullin97@gmail.com>
2022-01-15 00:10:41 -0800

    NthTribonacciNumber

--- algo/NthTribonacciNumber.java
+++ algo/NthTribonacciNumber.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class NthTribonacciNumber {
     // 1137. N-th Tribonacci Number
 
@@ -21,5 +24,31 @@ class NthTribonacciNumber {
             dad = temp;
         }
         return son;
+    }
+
+    // Approach 1: DP (Recursion+Memoization)
+    // Time Complexity: O(N)
+    // Space Complexity: O(1)
+    public int tribonacci2(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n <= 2) {
+            return 1;
+        }
+        return tribonacciDP(n);
+    }
+
+    Map<Integer, Integer> m = new HashMap<>();
+
+    private int tribonacciDP(int n) {
+        if (n <= 0) {
+            return 0;
+        } else if (n <= 2) {
+            return 1;
+        }
+        if (!m.containsKey(n)) {
+            m.put(n, tribonacciDP(n - 1) + tribonacciDP(n - 2) + tribonacciDP(n - 3));
+        }
+        return m.get(n);
     }
 }
