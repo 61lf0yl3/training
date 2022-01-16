7c9ed9e6bfeaaaddacb7815ba99383b645f7646f
61lf0yl3 <chingiz.samigullin97@gmail.com>
2022-01-15 22:09:13 -0800

    longestCommonSubsequence

--- algo/MaximumScorefromPerformingMultiplicationOperations.java
+++ algo/MaximumScorefromPerformingMultiplicationOperations.java
@@ -8,18 +8,17 @@ class MaximumScorefromPerformingMultiplicationOperations {
     // Approach 2: DP Bottom-Up
     // Time Complexity: O(M^2)
     // Space Complexity: O(M^2)
-    public int longestCommonSubsequence(String text1, String text2) {
-        int n = text1.length();
-        int m = text2.length();
-        int[][] memo = new int[n + 1][m + 1];
+    public int maximumScore(int[] nums, int[] multipliers) {
+        int n = nums.length;
+        int m = multipliers.length;
+        int[][] memo = new int[m + 1][m + 1];
 
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = m - 1; j >= 0; j--) {
-                if (text1.charAt(i) == text2.charAt(j)) {
-                    memo[i][j] = 1 + memo[i + 1][j + 1];
-                } else {
-                    memo[i][j] = Math.max(memo[i + 1][j], memo[i][j + 1]);
-                }
+        for (int i = m - 1; i >= 0; i--) {
+            for (int left = i; left >= 0; left--) {
+                int mult = multipliers[i];
+                int right = n - 1 - (i - left);
+                memo[i][left] = Math.max(nums[left] * mult + memo[i + 1][left + 1],
+                        nums[right] * mult + memo[i + 1][left]);
             }
         }
 
