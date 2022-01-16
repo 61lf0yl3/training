679c88a8770039f3ed9e1e0440b0d831baac0fdb
61lf0yl3 <chingiz.samigullin97@gmail.com>
2022-01-15 16:43:04 -0800

    maximumScore

--- algo/MaximumScorefromPerformingMultiplicationOperations.java
+++ algo/MaximumScorefromPerformingMultiplicationOperations.java
@@ -5,10 +5,30 @@ class MaximumScorefromPerformingMultiplicationOperations {
     int n;
     int m;
 
-    // Approach 1: DP Top-Down
+    // Approach 2: DP Bottom-Up
     // Time Complexity: O(M^2)
     // Space Complexity: O(M^2)
     public int maximumScore(int[] nums, int[] multipliers) {
+        int n = nums.length;
+        int m = multipliers.length;
+        int[][] memo = new int[m + 1][m + 1];
+
+        for (int i = m - 1; i >= 0; i--) {
+            for (int left = i; left >= 0; left--) {
+                int mult = multipliers[i];
+                int right = n - 1 - (i - left);
+                memo[i][left] = Math.max(nums[left] * mult + memo[i + 1][left + 1],
+                        nums[right] * mult + memo[i + 1][left]);
+            }
+        }
+
+        return memo[0][0];
+    }
+
+    // Approach 2: DP Top-Down
+    // Time Complexity: O(M^2)
+    // Space Complexity: O(M^2)
+    public int maximumScore2(int[] nums, int[] multipliers) {
         n = nums.length;
         m = multipliers.length;
         this.nums = nums;
