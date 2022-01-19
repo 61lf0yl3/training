67472d59fcf23b3a41157916095a37e5de17e335
61lf0yl3 <chingiz.samigullin97@gmail.com>
2022-01-18 21:34:29 -0800

    numDecodings4

--- algo/DecodeWays.java
+++ algo/DecodeWays.java
@@ -120,4 +120,21 @@ class DecodeWays {
         }
         return memo1[i];
     }
+
+    public int numDecodings5(String s) {
+        if (s.charAt(0) == '0') {
+            return 0;
+        }
+        int[] dp = new int[s.length() + 1];
+        dp[s.length()] = 1;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != '0') {
+                dp[i] = dp[i + 1];
+                if (i < s.length() - 1 && Integer.parseInt(s.substring(i, i + 2)) <= 26) {
+                    dp[i] += dp[i + 2];
+                }
+            }
+        }
+        return dp[0];
+    }
 }
