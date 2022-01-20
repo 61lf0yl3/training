3a5d4140adbead5abc1b1fad9e785052d2643b21
61lf0yl3 <chingiz.samigullin97@gmail.com>
2022-01-19 23:34:55 -0800

    maxSubarraySumCircular

--- algo/MaximumSumCircularSubarray.java
+++ algo/MaximumSumCircularSubarray.java
@@ -1,3 +1,6 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 class MaximumSumCircularSubarray {
     // Approach 1: Kadane's (Sign Variant)
     // Time Complexity: O(N)
@@ -32,4 +35,40 @@ class MaximumSumCircularSubarray {
         return max;
     }
 
+    // Approach 2: Prefix Sums + Monoqueue
+    // Time Complexity: O(N)
+    // Space Complexity: O(N)
+    public int maxSubarraySumCircular2(int[] A) {
+        int N = A.length;
+
+        // Compute P[j] = B[0] + B[1] + ... + B[j-1]
+        // for fixed array B = A+A
+        int[] P = new int[2 * N + 1];
+        for (int i = 0; i < 2 * N; ++i)
+            P[i + 1] = P[i] + A[i % N];
+
+        // Want largest P[j] - P[i] with 1 <= j-i <= N
+        // For each j, want smallest P[i] with i >= j-N
+        int ans = A[0];
+        // deque: i's, increasing by P[i]
+        Deque<Integer> deque = new ArrayDeque();
+        deque.offer(0);
+
+        for (int j = 1; j <= 2 * N; ++j) {
+            // If the smallest i is too small, remove it.
+            if (deque.peekFirst() < j - N)
+                deque.pollFirst();
+
+            // The optimal i is deque[0], for cand. answer P[j] - P[i].
+            ans = Math.max(ans, P[j] - P[deque.peekFirst()]);
+
+            // Remove any i1's with P[i2] <= P[i1].
+            while (!deque.isEmpty() && P[j] <= P[deque.peekLast()])
+                deque.pollLast();
+
+            deque.offerLast(j);
+        }
+
+        return ans;
+    }
 }
