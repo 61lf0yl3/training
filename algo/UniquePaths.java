a38d469e6b2095227295763b399586de9c351a97
61lf0yl3 <chingiz.samigullin97@gmail.com>
2022-01-20 21:22:18 -0800

    uniquePath

--- algo/UniquePaths.java
+++ algo/UniquePaths.java
@@ -21,12 +21,58 @@ class UniquePaths {
         return paths[n][m];
     }
 
+    // Approach 1: DP
+    // Time Complexity: O(M*N)
+    // Space Complexity: O(M*N)
+    public int uniquePaths1(int m, int n) {
+        int[][] dp = new int[m][n];
+        dp[0][0] = 1;
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                if (r > 0) {
+                    dp[r][c] += dp[r - 1][c];
+                }
+                if (c > 0) {
+                    dp[r][c] += dp[r][c - 1];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
     // Approach 2: Recursion (Time limit exceeded)
     public int uniquePaths2(int m, int n) {
         if (m == 1 || n == 1) {
             return 1;
         }
         return uniquePaths(m - 1, n) + uniquePaths(m, n - 1);
+    }
+
+    int[][] memo;
+
+    // Approach 2: DP Top-Down (Recursion with Memoization)
+    // Time Complexity: O(M*N)
+    // Space Complexity: O(M*N)
+    public int uniquePaths4(int m, int n) {
+        memo = new int[m][n];
+        return uniquePathsDP(m - 1, n - 1);
+    }
+
+    private int uniquePathsDP(int r, int c) {
+        if (r + c == 0) {
+            return 1;
+        }
+        if (memo[r][c] == 0) {
+            int ways = 0;
+            if (r > 0) {
+                ways = uniquePathsDP(r - 1, c);
+            }
+            if (c > 0) {
+                ways += uniquePathsDP(r, c - 1);
+            }
+            memo[r][c] = ways;
+        }
+        return memo[r][c];
     }
 
     // Approach 1: DP (leetcode version)
