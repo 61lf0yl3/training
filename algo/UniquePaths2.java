8b2dfebb84fde2fa4f7562c3210389501e356441
61lf0yl3 <chingiz.samigullin97@gmail.com>
2022-01-20 22:04:40 -0800

    uniquePathsWithObstacles

---
+++ algo/UniquePaths2.java
@@ -0,0 +1,29 @@
+class UniquePaths2 {
+
+    // Approach 1: DP Bottom-Up
+    // Time Complexity: O(M*N)
+    // Space Complexity: O(M*N)
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[m - 1][n - 1] == 1) {
+            return 0;
+        }
+        int[][] dp = new int[m][n];
+        dp[0][0] = 1;
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                if (obstacleGrid[r][c] == 0) {
+                    if (r > 0 && obstacleGrid[r - 1][c] == 0) {
+                        dp[r][c] += dp[r - 1][c];
+                    }
+                    if (c > 0 && obstacleGrid[r][c - 1] == 0) {
+                        dp[r][c] += dp[r][c - 1];
+                    }
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
+}
