cbd9b5ee1c088f94d9563f414e51940d691a9ffd
61lf0yl3 <samigull@usc.edu>
2022-01-22 00:15:56 -0800

    PaintHouse

--- algo/PaintHouse.java
+++ algo/PaintHouse.java
@@ -1,11 +1,24 @@
 class PaintHouse {
-    // Approach 1: DP Top-Down (Recursion with memorazation);
+
+    // Approach 1: DP Bottom-Up ;
+    // Time Complexity: O(N)
+    // Space Complexity: O(1)
+    public int minCost(int[][] costs) {
+        for (int i = costs.length - 2; i >= 0; i--) {
+            costs[i][0] += Math.min(costs[i + 1][1], costs[i + 1][2]);
+            costs[i][1] += Math.min(costs[i + 1][0], costs[i + 1][2]);
+            costs[i][2] += Math.min(costs[i + 1][0], costs[i + 1][1]);
+        }
+        return Math.min(costs[0][0], Math.min(costs[0][1], costs[0][2]));
+    }
+
+    // Approach 2: DP Top-Down (Recursion with memorazation);
     // Time Complexity: O(N)
     // Space Complexity: O(N)
     int[][] costs;
     Integer[][] memo;
 
-    public int minCost(int[][] costs) {
+    public int minCost2(int[][] costs) {
         this.costs = costs;
         memo = new Integer[costs.length][4];
         return minCostDP(0, 3);
