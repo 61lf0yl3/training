616fb2c3cce89d6b7f0501fe79a2fc19ba5d7da2
61lf0yl3 <samigull@usc.edu>
2022-01-21 23:07:48 -0800

    BestTimetoBuyandSellStockwithTransactionFee

--- algo/BestTimetoBuyandSellStockwithTransactionFee.java
+++ algo/BestTimetoBuyandSellStockwithTransactionFee.java
@@ -22,4 +22,35 @@ class BestTimetoBuyandSellStockwithTransactionFee {
         return dp[0][0];
     }
 
+    // Approach 2: DP Top-Down
+    // Time Complexity: O(N)
+    // Space Complexity: O(N)
+    Integer[][] memo;
+    int[] prices;
+    int fee;
+
+    public int maxProfit2(int[] prices, int fee) {
+        this.prices = prices;
+        this.fee = fee;
+        memo = new Integer[prices.length][2];
+        return maxProfitDP(0, 0);
+    }
+
+    private int maxProfitDP(int i, int holding) {
+        if (i == prices.length) {
+            return 0;
+        }
+        if (memo[i][holding] == null) {
+            int doNothing = maxProfitDP(i + 1, holding);
+            int sellOrBuy;
+            if (holding == 1) {
+                sellOrBuy = prices[i] - fee + maxProfitDP(i + 1, 0);
+            } else {
+                sellOrBuy = -prices[i] + maxProfitDP(i + 1, 1);
+            }
+            memo[i][holding] = Math.max(sellOrBuy, doNothing);
+        }
+        return memo[i][holding];
+    }
+
 }
