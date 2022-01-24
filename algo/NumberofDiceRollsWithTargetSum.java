f5280344c02d3159c66dde07cf8e1d44701edcd1
61lf0yl3 <samigull@usc.edu>
2022-01-24 15:17:49 -0800

    NumberofDiceRollsWithTargetSum

--- algo/NumberofDiceRollsWithTargetSum.java
+++ algo/NumberofDiceRollsWithTargetSum.java
@@ -31,9 +31,31 @@ public class NumberofDiceRollsWithTargetSum {
     }
 
     // Approach 2: DP Bottom-up
+    // Time Complexity: O(N*(K+T))
+    // Space Complexity: O(taget)
+    public int numRollsToTarget2(int n, int k, int target) {
+        long[] dp = new long[target + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            long prev = 0;
+            for (int l = 0; l < k && l <= target; l++) {
+                prev = (prev + dp[target - l]) % MOD;
+            }
+            for (int j = target; j >= 1; j--) {
+                prev += -dp[j] + MOD;
+                if (k <= j)
+                    prev += dp[j - k];
+                dp[j] = prev % MOD;
+            }
+            dp[0] = 0;
+        }
+        return (int) dp[target];
+    }
+
+    // Approach 2: DP Bottom-up
     // Time Complexity: O(N*K*T)
     // Space Complexity: O(N*K)
-    public int numRollsToTarget2(int n, int k, int target) {
+    public int numRollsToTarget3(int n, int k, int target) {
         int MOD = (int) 1e9 + 7;
         int[][] dp = new int[n + 1][target + 1];
         dp[0][0] = 1;
