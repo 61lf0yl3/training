b6a611fa01b577f17e52b31b4d3ef7b2d2721614
61lf0yl3 <samigull@usc.edu>
2022-01-26 23:53:00 -0800

    DominoandTrominoTiling

--- algo/DominoandTrominoTiling.java
+++ algo/DominoandTrominoTiling.java
@@ -87,4 +87,24 @@ class DominoandTrominoTiling {
         }
         return (int) (fCurrent);
     }
+
+    // Approach 2: Math optimization (Fibonacci sequence like)
+    // Time Complexity: O(N)
+    // Space Complexity: O(1)
+    public int numTilings4(int n) {
+        int MOD = 1_000_000_007;
+        if (n <= 2) {
+            return n;
+        }
+        long fCurrent = 5L;
+        long fPrevious = 2;
+        long fBeforePrevious = 1;
+        for (int k = 4; k < n + 1; ++k) {
+            long tmp = fPrevious;
+            fPrevious = fCurrent;
+            fCurrent = (2 * fCurrent + fBeforePrevious) % MOD;
+            fBeforePrevious = tmp;
+        }
+        return (int) (fCurrent % MOD);
+    }
 }
