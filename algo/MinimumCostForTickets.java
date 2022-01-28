168a535c551be8a1e2df127e779473808b175db8
61lf0yl3 <samigull@usc.edu>
2022-01-27 16:01:25 -0800

    MinimumCostForTickets

--- algo/MinimumCostForTickets.java
+++ algo/MinimumCostForTickets.java
@@ -39,4 +39,42 @@ class MinimumCostForTickets {
         }
         return memo[i];
     }
+
+    // Approach 1: DP Top-Down (Recursion with memorazation);
+    // Time Complexity: O(N) where N is the number of unique days in your travel
+    // plan.
+    // Space Complexity: O(N)
+    public int mincostTickets2(int[] days, int[] costs) {
+        this.days = days;
+        this.costs = costs;
+        memo = new int[days.length];
+        return mincostTicketsDP2(0);
+    }
+
+    private int mincostTicketsDP2(int i) {
+        int[] memo1 = memo;
+        if (i >= days.length) {
+            return 0;
+        }
+        if (memo[i] == 0) {
+
+            // 1-day pass
+            int ret = costs[0] + mincostTicketsDP(i + 1);
+            int j = i;
+
+            // 7-day pass
+            while (j < days.length && days[j] < days[i] + 7) {
+                j++;
+            }
+            ret = Math.min(ret, costs[1] + mincostTicketsDP(j));
+
+            // 30-day pass
+            while (j < days.length && days[j] < days[i] + 30) {
+                j++;
+            }
+            ret = Math.min(ret, costs[2] + mincostTicketsDP(j));
+            memo[i] = ret;
+        }
+        return memo[i];
+    }
 }
