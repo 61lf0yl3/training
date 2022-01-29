0494b639d4a094d9632a2900c40ce3340d6bc0db
61lf0yl3 <samigull@usc.edu>
2022-01-29 02:01:04 -0800

    NestedListWeightSum2

---
+++ algo/NestedListWeightSum2.java
@@ -0,0 +1,67 @@
+import java.util.List;
+
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ * // Constructor initializes an empty nested list.
+ * public NestedInteger();
+ *
+ * // Constructor initializes a single integer.
+ * public NestedInteger(int value);
+ *
+ * // @return true if this NestedInteger holds a single integer, rather than a
+ * nested list.
+ * public boolean isInteger();
+ *
+ * // @return the single integer that this NestedInteger holds, if it holds a
+ * single integer
+ * // Return null if this NestedInteger holds a nested list
+ * public Integer getInteger();
+ *
+ * // Set this NestedInteger to hold a single integer.
+ * public void setInteger(int value);
+ *
+ * // Set this NestedInteger to hold a nested list and adds a nested integer to
+ * it.
+ * public void add(NestedInteger ni);
+ *
+ * // @return the nested list that this NestedInteger holds, if it holds a
+ * nested list
+ * // Return empty list if this NestedInteger holds a single integer
+ * public List<NestedInteger> getList();
+ * }
+ */
+class NestedListWeightSum2 {
+    // Approach 1: Depth-first Search
+    // Time Complexity: O(N)
+    // Space Complexity: O(N)
+    int maxDepth = 0;
+    int res = 0;
+
+    public int depthSumInverse(List<NestedInteger> nestedList) {
+        depthR(nestedList, 0);
+        depthSumR(nestedList, 0);
+        return res;
+    }
+
+    private void depthR(List<NestedInteger> nestedList, int depth) {
+        for (NestedInteger n : nestedList) {
+            if (n.isInteger()) {
+                maxDepth = Math.max(maxDepth, depth);
+            } else {
+                depthR(n.getList(), depth + 1);
+            }
+        }
+    }
+
+    private void depthSumR(List<NestedInteger> nestedList, int depth) {
+        for (NestedInteger n : nestedList) {
+            if (n.isInteger()) {
+                res += (maxDepth - depth + 1) * n.getInteger();
+            } else {
+                depthSumR(n.getList(), depth + 1);
+            }
+        }
+    }
+}
