e7f2f9b2e74369b1129dfb04d7bd2b316eb90310
61lf0yl3 <samigull@usc.edu>
2022-01-31 23:23:36 -0800

    MergekSortedLists

--- algo/MergekSortedLists.java
+++ algo/MergekSortedLists.java
@@ -11,7 +11,7 @@ import java.util.PriorityQueue;
  * }
  */
 class MergekSortedLists {
-    // Approach 3: Optimize Approach 2 by Priority Queue
+    // Approach 1: Optimize Approach 2 by Priority Queue
     // Time Complexity: O(NlogK)
     // Space Complexity: O(N)
     public ListNode mergeKLists(ListNode[] lists) {
@@ -32,5 +32,47 @@ class MergekSortedLists {
         }
 
         return res.next;
+    }
+
+    // Approach 2: Optimize Approach 2 by Priority Queue
+    // Time Complexity: O(NlogK)
+    // Space Complexity: O(1)
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode h = new ListNode(0);
+        ListNode ans = h;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                h.next = l1;
+                h = h.next;
+                l1 = l1.next;
+            } else {
+                h.next = l2;
+                h = h.next;
+                l2 = l2.next;
+            }
+        }
+        if (l1 == null) {
+            h.next = l2;
+        }
+        if (l2 == null) {
+            h.next = l1;
+        }
+        return ans.next;
+    }
+
+    public ListNode mergeKLists2(ListNode[] lists) {
+        if (lists.length == 0) {
+            return null;
+        }
+        int interval = 1;
+        while (interval < lists.length) {
+            System.out.println(lists.length);
+            for (int i = 0; i + interval < lists.length; i = i + interval * 2) {
+                lists[i] = mergeTwoLists(lists[i], lists[i + interval]);
+            }
+            interval *= 2;
+        }
+
+        return lists[0];
     }
 }
