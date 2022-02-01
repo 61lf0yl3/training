780471611825a84c6af47b54b6bfc122de2ca764
61lf0yl3 <samigull@usc.edu>
2022-01-31 17:42:57 -0800

    CombinationSum4

--- algo/CombinationSum4.java
+++ algo/CombinationSum4.java
@@ -29,4 +29,22 @@ class CombinationSum4 {
         }
         return memo[remain];
     }
+
+    // Approach 2: DP Borrom-Up
+    // Time Complexity: O(N*T)
+    // Space Complexity: O(N*T)
+    public int combinationSum42(int[] nums, int target) {
+        Integer[] dp = new Integer[target + 1];
+        dp[0] = 1;
+        for (int remain = 1; remain <= target; remain++) {
+            int best = 0;
+            for (int nb : nums) {
+                if (nb <= remain) {
+                    best += dp[remain - nb];
+                }
+            }
+            dp[remain] = best;
+        }
+        return dp[target];
+    }
 }
