806cf256d449fae5f2bd5dc7d0f5cf9b10388e67
61lf0yl3 <samigull@usc.edu>
2022-02-01 00:30:27 -0800

    ReorderList

---
+++ algo/ReorderList.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    // Approach 1: Reverse the Second Part of the List and Merge Two Sorted Lists
+    // Time Complexity: O(N)
+    // Space Complexity: O(1)
+    public void reorderList(ListNode head) {
+        // find the middle of linked list [Problem 876]
+        // in 1->2->3->4->5->6 find 4
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // reverse the second part of the list [Problem 206]
+        // convert 1->2->3->4->5->6 into 1->2->3->4 and 6->5->4
+        // reverse the second half in-place
+        ListNode prev = null;
+        ListNode curr = slow;
+        while (curr != null) {
+            ListNode tmp = curr.next;
+
+            curr.next = prev;
+            prev = curr;
+            curr = tmp;
+        }
+
+        // merge two sorted linked lists [Problem 21]
+        // merge 1->2->3->4 and 6->5->4 into 1->6->2->5->3->4
+        ListNode first = head;
+        ListNode second = prev;
+        while (second.next != null) {
+            ListNode tmp = first.next;
+            first.next = second;
+            first = tmp;
+
+            tmp = second.next;
+            second.next = first;
+            second = tmp;
+        }
+    }
+}
