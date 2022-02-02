3faa84575531ccfe7986ba073dfadef47306d459
61lf0yl3 <samigull@usc.edu>
2022-02-02 01:48:48 -0800

    BinaryTreeLevelOrderTraversal

--- algo/BinaryTreeLevelOrderTraversal.java
+++ algo/BinaryTreeLevelOrderTraversal.java
@@ -31,4 +31,29 @@ public class BinaryTreeLevelOrderTraversal {
         }
         return res;
     }
+
+    // Approach 1: DFS
+    // Time Complexity: O(N)
+    // Space Complexity: O(N)
+    List<List<Integer>> res = new ArrayList<>();
+
+    public List<List<Integer>> levelOrder2(TreeNode root) {
+        if (root == null) {
+            return res;
+        }
+        dfs(root, 0);
+        return res;
+    }
+
+    private void dfs(TreeNode node, int depth) {
+        if (node == null) {
+            return;
+        }
+        if (res.size() <= depth) {
+            res.add(new ArrayList<>());
+        }
+        res.get(depth).add(node.val);
+        dfs(node.left, depth + 1);
+        dfs(node.right, depth + 1);
+    }
 }
