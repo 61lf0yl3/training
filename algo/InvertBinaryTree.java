e0c3e9d1d1c504d08ce1243f34d901b392fd09ab
61lf0yl3 <samigull@usc.edu>
2022-02-04 01:20:09 -0800

    InvertBinaryTree

--- algo/InvertBinaryTree.java
+++ algo/InvertBinaryTree.java
@@ -1,3 +1,7 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.concurrent.DelayQueue;
+
 public class InvertBinaryTree {
     // Approach 1: DFS
     // Time Complexity: O(N)
@@ -16,5 +20,29 @@ public class InvertBinaryTree {
         node.right = temp;
         dfs(node.left);
         dfs(node.right);
+    }
+
+    // Approach 1: BFS with Queue
+    // Time Complexity: O(N)
+    // Space Complexity: O(N)
+    public TreeNode invertTree2(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        Queue<TreeNode> q = new ArrayDeque<>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            TreeNode node = q.poll();
+            TreeNode temp = node.left;
+            node.left = node.right;
+            node.right = temp;
+            if (node.left != null) {
+                q.add(node.left);
+            }
+            if (node.right != null) {
+                q.add(node.right);
+            }
+        }
+        return root;
     }
 }
