928c48e35f244be0561465f963b9376935f9e93c
61lf0yl3 <samigull@usc.edu>
2022-02-05 00:56:41 -0800

    BinaryTreeMaximumPathSum

---
+++ algo/BinaryTreeMaximumPathSum.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+
+// Approach 1: DFS
+// Time Complexity: O(N)
+// Space Complexity: O(1)
+class BinaryTreeMaximumPathSum {
+    int maxSum = Integer.MIN_VALUE;
+
+    public int maxPathSum(TreeNode root) {
+        dfs(root);
+        return maxSum;
+    }
+
+    private int dfs(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int left = Math.max(dfs(node.left), 0);
+        int right = Math.max(dfs(node.right), 0);
+
+        int price_newpath = node.val + left + right;
+
+        maxSum = Math.max(maxSum, price_newpath);
+        return node.val + Math.max(left, right);
+    }
+}
