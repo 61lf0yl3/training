6568e89cee75faf25ea8f597ec1a0af4d474e37c
61lf0yl3 <samigull@usc.edu>
2022-02-06 14:43:52 -0800

    kthSmallest

--- algo/KthSmallestElementinaBST.java
+++ algo/KthSmallestElementinaBST.java
@@ -1,9 +1,29 @@
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class KthSmallestElementinaBST {
+    // Approach 1: Recursive Inorder Traversal
+    // Time Complexity: O(N)
+    // Space Complexity: O(N)
+    ArrayList<Integer> al = new ArrayList<>();
+
     public int kthSmallest(TreeNode root, int k) {
+        dfs(root);
+        return al.get(k - 1);
+    }
+
+    private void dfs(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        dfs(root.left);
+        al.add(root.val);
+        dfs(root.right);
+    }
+
+    public int kthSmallest3(TreeNode root, int k) {
         PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
         Queue<TreeNode> q = new ArrayDeque<>();
         q.add(root);
