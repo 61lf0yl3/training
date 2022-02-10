a8e5345f28023318a7ce660eefddfc2aaf756a47
61lf0yl3 <samigull@usc.edu>
2022-02-10 01:18:53 -0800

    MissingNumber.java

--- algo/MissingNumber.java
+++ algo/MissingNumber.java
@@ -13,4 +13,16 @@ public class MissingNumber {
         }
         return sum;
     }
+
+    // Approach 1: Gauss' Formula [Accepted]
+    // Time Complexity: O(NlogN)
+    // Space Complexity: O(N)
+    public int missingNumber2(int[] nums) {
+        int n = nums.length;
+        int sum = n * (n + 1) / 2;
+        for (int i = 0; i < n; i++) {
+            sum -= nums[i];
+        }
+        return sum;
+    }
 }
