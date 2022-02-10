bbf3334134603b9118a0d909bc2239f2d5218bb7
61lf0yl3 <samigull@usc.edu>
2022-02-10 01:52:22 -0800

    SymmetricTree

---
+++ algo/SymmetricTree.java
@@ -0,0 +1,47 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class SymmetricTree {
+    // Approach 1: Iterative using Queue
+    // Time Complexity: O(N)
+    // Space Complexity: O(1)
+    public boolean isSymmetric(TreeNode root) {
+        Queue<TreeNode> q = new ArrayDeque<>();
+        q.add(root);
+        q.add(root);
+        while (!q.isEmpty()) {
+            TreeNode t1 = q.poll();
+            TreeNode t2 = q.poll();
+
+            if (t1 == null && t2 == null) {
+                continue;
+            }
+            if (t1 == null || t2 == null) {
+                return false;
+            }
+            if (t1.val != t2.val) {
+                return false;
+            }
+            q.add(t1.left);
+            q.add(t2.right);
+            q.add(t1.right);
+            q.add(t2.left);
+        }
+        return true;
+    }
+}
