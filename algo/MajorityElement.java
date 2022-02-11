11213c3207aa3b703108dd44558e60036e32a96f
61lf0yl3 <samigull@usc.edu>
2022-02-10 17:37:03 -0800

    MajorityElement

--- algo/MajorityElement.java
+++ algo/MajorityElement.java
@@ -30,4 +30,33 @@ class MajorityElement {
         return nums[n / 2];
     }
 
+    // Approach 3: Using Randomization
+    // Time Complexity: O(N)
+    // Space Complexity: O(N)
+    private int randRange(Random rand, int min, int max) {
+        return rand.nextInt(max - min) + min;
+    }
+
+    private int countOccurences(int[] nums, int num) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == num) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int majorityElement3(int[] nums) {
+        Random rand = new Random();
+
+        int majorityCount = nums.length / 2;
+
+        while (true) {
+            int candidate = nums[randRange(rand, 0, nums.length)];
+            if (countOccurences(nums, candidate) > majorityCount) {
+                return candidate;
+            }
+        }
+    }
 }
