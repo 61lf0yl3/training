c834f6ceaa3cbb1181c3f3422ee9bf6effb82cfc
61lf0yl3 <samigull@usc.edu>
2022-02-11 20:20:06 -0800

    kthFactor2

--- algo/ThekthFactorofn.java
+++ algo/ThekthFactorofn.java
@@ -1,3 +1,6 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
+
 class ThekthFactorofn {
     // Approach 1: Brute Force
     // Time Complexity: O(N)
@@ -12,5 +15,30 @@ class ThekthFactorofn {
             }
         }
         return -1;
+    }
+
+    // Approach 2: Iterative Using Heap
+    // Time Complexity: O(sqrt(N)*logK)
+    // Space Complexity: O(min(N, K))
+    public int kthFactor2(int n, int k) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>(Collections.reverseOrder());
+        for (int i = 1; i * i <= n; i++) {
+            if (n % i == 0) {
+                heap.add(i);
+                if (heap.size() > k) {
+                    heap.poll();
+                }
+                if (i * i != n) {
+                    heap.add(n / i);
+                }
+                if (heap.size() > k) {
+                    heap.poll();
+                }
+            }
+        }
+        if (heap.size() < k) {
+            return -1;
+        }
+        return heap.peek();
     }
 }
