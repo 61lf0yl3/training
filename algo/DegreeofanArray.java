4bc4a185937c4c2e779ff86f352ca36af982ec39
61lf0yl3 <samigull@usc.edu>
2022-02-12 18:00:35 -0800

    DegreeofanArray

--- algo/DegreeofanArray.java
+++ algo/DegreeofanArray.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -31,5 +32,30 @@ class DegreeofanArray {
             res = Math.min(res, list.get(size - 1) - list.get(0) + 1);
         }
         return res;
+    }
+
+    // Approach #1: Left and Right Index [Accepted]
+    // Time Complexity: O(N)
+    // Space Complexity: O(N)
+    public int findShortestSubArray2(int[] nums) {
+        Map<Integer, Integer> left = new HashMap(),
+                right = new HashMap(), count = new HashMap();
+
+        for (int i = 0; i < nums.length; i++) {
+            int x = nums[i];
+            if (left.get(x) == null)
+                left.put(x, i);
+            right.put(x, i);
+            count.put(x, count.getOrDefault(x, 0) + 1);
+        }
+
+        int ans = nums.length;
+        int degree = Collections.max(count.values());
+        for (int x : count.keySet()) {
+            if (count.get(x) == degree) {
+                ans = Math.min(ans, right.get(x) - left.get(x) + 1);
+            }
+        }
+        return ans;
     }
 }
