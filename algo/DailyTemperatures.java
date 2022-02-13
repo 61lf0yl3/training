1dd7d12c895c67f76acc6f6ce8798b0d07524ce8
61lf0yl3 <samigull@usc.edu>
2022-02-12 16:03:33 -0800

    dailyTemperatures2

--- algo/DailyTemperatures.java
+++ algo/DailyTemperatures.java
@@ -20,4 +20,26 @@ public class DailyTemperatures {
         }
         return res;
     }
+
+    // Approach 1: Array, Optimized Space
+    // Time Complexity: O(N)
+    // Space Complexity: O(1)
+    public int[] dailyTemperatures2(int[] temperatures) {
+        int n = temperatures.length;
+        int[] res = new int[n];
+        int hotest = temperatures[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            int curr = temperatures[i];
+            if (curr >= hotest) {
+                hotest = curr;
+            } else {
+                int days = 1;
+                while (curr >= temperatures[i + days]) {
+                    days += res[i + days];
+                }
+                res[i] = days;
+            }
+        }
+        return res;
+    }
 }
