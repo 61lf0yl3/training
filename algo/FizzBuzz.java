252593a01c0691b227e831be66b14b7293037721
61lf0yl3 <samigull@usc.edu>
2022-02-17 03:19:36 -0800

    FizzBuzz

---
+++ algo/FizzBuzz.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBuzz {
+    // Approach 1: Iterative
+    // Time Complexity: O(N)
+    // Space Complexity: O(1)
+    public List<String> fizzBuzz(int n) {
+        List<String> res = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                res.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                res.add("Fizz");
+            } else if (i % 5 == 0) {
+                res.add("Buzz");
+            } else {
+                res.add(String.valueOf(i));
+            }
+        }
+        return res;
+    }
+}
