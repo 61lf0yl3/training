0a691de9052723e4a406cf2d4f23a40fee1ee619
61lf0yl3 <samigull@usc.edu>
2022-02-20 17:57:30 -0800

    fizzBuzz

--- algo/FizzBuzz.java
+++ algo/FizzBuzz.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class FizzBuzz {
     // Approach 1: Iterative
@@ -21,7 +23,7 @@ public class FizzBuzz {
         return res;
     }
 
-    // Approach 1: String Concatenation
+    // Approach 2: String Concatenation
     // Time Complexity: O(N)
     // Space Complexity: O(1)
     public List<String> fizzBuzz2(int n) {
@@ -37,6 +39,30 @@ public class FizzBuzz {
                 if (i % 5 == 0) {
                     temp.append("Buzz");
                 }
+            }
+            res.add(temp.toString());
+        }
+        return res;
+    }
+
+    // Approach 3: String Concatenation
+    // Time Complexity: O(N)
+    // Space Complexity: O(1)
+    public List<String> fizzBuzz3(int n) {
+        Map<Integer, String> m = new HashMap<>();
+        m.put(3, "Fizz");
+        m.put(5, "Buzz");
+
+        List<String> res = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            StringBuilder temp = new StringBuilder();
+            for (int key : m.keySet()) {
+                if (i % key == 0) {
+                    temp.append(m.get(key));
+                }
+            }
+            if (temp.length() == 0) {
+                temp.append(String.valueOf(i));
             }
             res.add(temp.toString());
         }
