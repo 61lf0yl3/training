75898522b6facc54dc7deddf0da7fefff4b89a8a
61lf0yl3 <samigull@usc.edu>
2022-02-23 01:08:21 -0800

    MaximumSwap

---
+++ algo/MaximumSwap.java
@@ -0,0 +1,29 @@
+public class MaximumSwap {
+    // Approach 1: Iterative
+    // Time Complexity: O(N)
+    // Space Complexity: O(N)
+    public int maximumSwap(int num) {
+        char[] digits = String.valueOf(num).toCharArray();
+        int n = digits.length;
+        for (int i = 0; i < n - 1; i++) {
+            if (digits[i] < digits[i + 1]) {
+                char max = digits[i + 1];
+                int maxIndex = i + 1;
+                for (int j = i + 1; j < n; j++) {
+                    if (digits[j] >= max) {
+                        max = digits[j];
+                        maxIndex = j;
+                    }
+                }
+                for (int j = 0; j <= i; j++) {
+                    if (max > digits[j]) {
+                        digits[maxIndex] = digits[j];
+                        digits[j] = max;
+                        return Integer.parseInt(new String(digits));
+                    }
+                }
+            }
+        }
+        return num;
+    }
+}
