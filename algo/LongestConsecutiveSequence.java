067a6f897d0f8a2dfba253d8c057b4b37e4a093a
61lf0yl3 <samigull@usc.edu>
2022-02-28 22:40:50 -0800

    longestConsecutive

--- algo/LongestConsecutiveSequence.java
+++ algo/LongestConsecutiveSequence.java
@@ -1,8 +1,35 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class LongestConsecutiveSequence {
-    // Approach 1: Brute Force
+    // Approach 1: HashSet and Intelligent Sequence Building
     // Time Complexity: O(N^3)
     // Space Complexity: O(1)
     public int longestConsecutive(int[] nums) {
+        Set<Integer> s = new HashSet<>();
+        for (int num : nums) {
+            s.add(num);
+        }
+
+        int res = 0;
+        for (int num : s) {
+            if (!s.contains(num - 1)) {
+                int curr = num + 1;
+                int substring = 1;
+                while (s.contains(curr)) {
+                    curr++;
+                    substring++;
+                }
+                res = Math.max(res, substring);
+            }
+        }
+        return res;
+    }
+
+    // Approach 1: Brute Force
+    // Time Complexity: O(N^3)
+    // Space Complexity: O(1)
+    public int longestConsecutive3(int[] nums) {
         int res = 0;
         for (int num : nums) {
             int curr = num;
