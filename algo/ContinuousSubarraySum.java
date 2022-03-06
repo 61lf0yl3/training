a9ebd886b3914674eda3d15ec0ebf4dd4da03c11
61lf0yl3 <samigull@usc.edu>
2022-03-06 00:11:09 -0800

    ContinuousSubarraySum

--- algo/ContinuousSubarraySum.java
+++ algo/ContinuousSubarraySum.java
@@ -1,8 +1,32 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class ContinuousSubarraySum {
-    // Approach 1: Brute Force
-    // Time Complexity: O(N^2)
+
+    // Approach 1: Using HashMap
+    // Time Complexity: O(N)
     // Space Complexity: O(1)
     public boolean checkSubarraySum(int[] nums, int k) {
+        Map<Integer, Integer> m = new HashMap<>();
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            sum %= k;
+            if (sum == 0 && i > 0) {
+                return true;
+            }
+            if (m.containsKey(sum) && i - m.get(sum) > 1) {
+                return true;
+            }
+            m.putIfAbsent(sum, i);
+        }
+        return false;
+    }
+
+    // Approach 2: Brute Force
+    // Time Complexity: O(N^2)
+    // Space Complexity: O(1)
+    public boolean checkSubarraySum2(int[] nums, int k) {
         int n = nums.length;
         for (int i = 0; i < n; i++) {
             int sum = nums[i];
