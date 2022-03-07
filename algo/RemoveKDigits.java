d306da8972dedefa760fe265e2970d2bd8caedd2
61lf0yl3 <samigull@usc.edu>
2022-03-06 21:25:30 -0800

    RemoveKDigits

--- algo/RemoveKDigits.java
+++ algo/RemoveKDigits.java
@@ -1,4 +1,5 @@
 import java.util.Stack;
+import java.util.LinkedList;
 
 public class RemoveKDigits {
     // Approach 1: Using Greedy with Stack
@@ -37,6 +38,38 @@ public class RemoveKDigits {
         boolean leadingZero = true;
         for (i = 0; i < res.length(); i++) {
             char digit = res.charAt(i);
+            if (leadingZero && digit == '0')
+                continue;
+            leadingZero = false;
+            ret.append(digit);
+        }
+
+        /* return the final string */
+        if (ret.length() == 0)
+            return "0";
+        return ret.toString();
+    }
+
+    public String removeKdigits2(String num, int k) {
+        LinkedList<Character> stack = new LinkedList<Character>();
+
+        for (char digit : num.toCharArray()) {
+            while (stack.size() > 0 && k > 0 && stack.peekLast() > digit) {
+                stack.removeLast();
+                k -= 1;
+            }
+            stack.addLast(digit);
+        }
+
+        /* remove the remaining digits from the tail. */
+        for (int i = 0; i < k; ++i) {
+            stack.removeLast();
+        }
+
+        // build the final string, while removing the leading zeros.
+        StringBuilder ret = new StringBuilder();
+        boolean leadingZero = true;
+        for (char digit : stack) {
             if (leadingZero && digit == '0')
                 continue;
             leadingZero = false;
