fe2d61931b06d95aa06735653fbd942d30b9adb3
61lf0yl3 <samigull@usc.edu>
2022-03-11 19:44:43 -0800

    TrappingRainWater

--- algo/TrappingRainWater.java
+++ algo/TrappingRainWater.java
@@ -1,8 +1,31 @@
 public class TrappingRainWater {
-    // Approach 1: Brute Force
+    // Approach 1: DP
+    // Time Complexity: O(N)
+    // Space Complexity: O(N)
+    public int trap(int[] height) {
+        int n = height.length;
+        int res = 0;
+        int[] leftMax = new int[n];
+        leftMax[0] = height[0];
+        int[] rightMax = new int[n];
+        rightMax[n - 1] = height[n - 1];
+
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(height[i], leftMax[i - 1]);
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(height[i], rightMax[i + 1]);
+        }
+        for (int i = 0; i < n; i++) {
+            res += Math.min(leftMax[i], rightMax[i]) - height[i];
+        }
+        return res;
+    }
+
+    // Approach 3: Brute Force
     // Time Complexity: O(N^2)
     // Space Complexity: O(1)
-    public int trap(int[] height) {
+    public int trap3(int[] height) {
         int res = 0;
         for (int i = 0; i < height.length; i++) {
             int leftMax = 0;
@@ -18,4 +41,5 @@ public class TrappingRainWater {
         }
         return res;
     }
+
 }
