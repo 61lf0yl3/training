263ad12e027f292f516212c79990e83c076cf12c
61lf0yl3 <samigull@usc.edu>
2022-03-13 03:17:34 -0700

    nextPermutation

---
+++ algo/NextPermutation.java
@@ -0,0 +1,35 @@
+public class NextPermutation {
+    // Approach 1: Single Pass Approach
+    // Time Complexity: O(N)
+    // Space Complexity: O(1)
+    public void nextPermutation(int[] nums) {
+
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+    }
+
+    private void reverse(int[] nums, int start) {
+        int i = start, j = nums.length - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
