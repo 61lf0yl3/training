ccd9eed9f2872dd7a09530914426c7d15f32d2d0
61lf0yl3 <samigull@usc.edu>
2022-03-14 16:46:47 -0700

    SlidingWindowMaximum

--- algo/SlidingWindowMaximum.java
+++ algo/SlidingWindowMaximum.java
@@ -1,4 +1,58 @@
+import java.util.ArrayDeque;
+
 class SlidingWindowMaximum {
+
+    // Approach 3: Using Deque
+    // Time Complexity: O(N)
+    // Space Complexity: O(N)
+    int[] nums;
+    ArrayDeque<Integer> deq = new ArrayDeque<Integer>();
+
+    public int[] maxSlidingWindow(int[] nums, int k) {
+
+        int n = nums.length;
+        if (n * k == 0) {
+            return new int[0];
+        }
+        if (k == 1)
+            return nums;
+
+        // init deque and output
+        this.nums = nums;
+        int max_idx = 0;
+        for (int i = 0; i < k; i++) {
+            cleanDeque(i, k);
+            deq.addLast(i);
+            // compute max in nums[:k]
+            if (nums[i] > nums[max_idx])
+                max_idx = i;
+        }
+        int[] output = new int[n - k + 1];
+        output[0] = nums[max_idx];
+
+        // build output
+        for (int i = k; i < n; i++) {
+            cleanDeque(i, k);
+            deq.addLast(i);
+            output[i - k + 1] = nums[deq.getFirst()];
+        }
+        return output;
+    }
+
+    private void cleanDeque(int i, int k) {
+
+        // remove indexes of elements not from sliding window
+        if (!deq.isEmpty() && deq.getFirst() == i - k) {
+            deq.removeFirst();
+        }
+
+        // remove from deq indexes of all elements
+        // which are smaller than current element nums[i]
+        while (!deq.isEmpty() && nums[i] > nums[deq.getLast()]) {
+            deq.removeLast();
+        }
+    }
+
     // Approach 3: Brute Force (Hammer)
     // Time Complexity: O(N^2)
     // Space Complexity: O(N)
@@ -14,4 +68,5 @@ class SlidingWindowMaximum {
         }
         return res;
     }
+
 }
