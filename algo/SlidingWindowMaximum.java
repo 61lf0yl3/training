de0cde08096cd242cbba6e0ecd9b7cb2700f01c5
61lf0yl3 <samigull@usc.edu>
2022-03-14 17:16:08 -0700

    SlidingWindowMaximum

--- algo/SlidingWindowMaximum.java
+++ algo/SlidingWindowMaximum.java
@@ -2,13 +2,47 @@ import java.util.ArrayDeque;
 
 class SlidingWindowMaximum {
 
-    // Approach 3: Using Deque
+    // Approach 1: DP
+    // Time Complexity: O(N)
+    // Space Complexity: O(N)
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+
+        left[0] = nums[0];
+        right[n - 1] = nums[n - 1];
+        for (int i = 1; i < nums.length; i++) {
+            // from left to right
+            if (i % k == 0) { // block_start
+                left[i] = nums[i];
+            } else {
+                left[i] = Math.max(left[i - 1], nums[i]);
+            }
+
+            // from right to left
+            int j = n - i - 1;
+            if (j % k == 0) { // block_start
+                right[j] = nums[j];
+            } else {
+                right[j] = Math.max(right[j + 1], nums[j]);
+            }
+        }
+
+        int[] res = new int[n - k + 1];
+        for (int i = 0; i < n - k + 1; i++) {
+            res[i] = Math.max(right[i], left[i + k - 1]);
+        }
+        return res;
+    }
+
+    // Approach 1: Using Deque
     // Time Complexity: O(N)
     // Space Complexity: O(N)
     int[] nums;
     ArrayDeque<Integer> deq = new ArrayDeque<Integer>();
 
-    public int[] maxSlidingWindow(int[] nums, int k) {
+    public int[] maxSlidingWindow2(int[] nums, int k) {
 
         int n = nums.length;
         if (n * k == 0) {
