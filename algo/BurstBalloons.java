e76a47161552aa4612d75970f2ba400971c31f44
61lf0yl3 <samigull@usc.edu>
2022-03-16 15:43:54 -0700

    BurstBalloons

--- algo/BurstBalloons.java
+++ algo/BurstBalloons.java
@@ -32,4 +32,38 @@ class BurstBalloons {
         }
         return memo[left][right];
     }
+
+    // Approach 2: Dynamic Programming (Bottom-Up)
+    // Time Complexity: O(N^3)
+    // Space Complexity: O(N^2)
+    public int maxCoins2(int[] nums) {
+        // add 1 before and after nums
+        int n = nums.length + 2;
+        int[] newNums = new int[n];
+        System.arraycopy(nums, 0, newNums, 1, n - 2);
+        newNums[0] = 1;
+        newNums[n - 1] = 1;
+        // dp[i][j] represents
+        // maximum if we burst all nums[left]...nums[right], inclusive
+        int[][] dp = new int[n][n];
+
+        // do not include the first one and the last one
+        // since they are both fake balloons added by ourselves and we can not burst
+        // them
+        for (int left = n - 2; left >= 1; left--) {
+            for (int right = left; right <= n - 2; right++) {
+                // find the last burst one in newNums[left]...newNums[right]
+                for (int i = left; i <= right; i++) {
+                    // newNums[i] is the last burst one
+                    int gain = newNums[left - 1] * newNums[i] * newNums[right + 1];
+                    // recursively call left side and right side
+                    int remaining = dp[left][i - 1] + dp[i + 1][right];
+                    // update
+                    dp[left][right] = Math.max(dp[left][right], remaining + gain);
+                }
+            }
+        }
+        // burst newNums[1]...newNums[n-2], excluding the first one and the last one
+        return dp[1][n - 2];
+    }
 }
