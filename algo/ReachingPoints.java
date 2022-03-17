02480060a108706056766b657e1bd39a32969c6e
61lf0yl3 <samigull@usc.edu>
2022-03-16 23:00:42 -0700

    ReachingPoints

--- algo/ReachingPoints.java
+++ algo/ReachingPoints.java
@@ -3,6 +3,31 @@ import java.util.Set;
 import java.awt.Point;
 
 class ReachingPoints {
+    // Approach 1: Work Backwards (Modulo Variant) [Accepted]
+    // Time Complexity: O(log(max(tx+ty))
+    // Space Complexity: O(1)
+    public boolean reachingPoints3(int sx, int sy, int tx, int ty) {
+        while (tx >= sx && ty >= sy) {
+            if (tx == ty) {
+                break;
+            }
+            if (tx > ty) {
+                if (ty > sy) {
+                    tx %= ty;
+                } else {
+                    return (tx - sx) % ty == 0;
+                }
+            } else {
+                if (tx > sx) {
+                    ty %= tx;
+                } else {
+                    return (ty - sy) % tx == 0;
+                }
+            }
+        }
+        return (tx == sx && ty == sy);
+    }
+
     // Approach 1: Exhaustive Search (TLE)
     // Time Complexity: O(2^tx+ty)
     // Space Complexity: O(tx*ty)
@@ -16,7 +41,7 @@ class ReachingPoints {
         return reachingPoints(sx, sx + sy, tx, ty) || reachingPoints(sx + sy, sx, tx, ty);
     }
 
-    // Approach 2: DP (TLE)
+    // Approach 3: DP (TLE)
     // Time Complexity: O(tx*ty)
     // Space Complexity: O(tx*ty)
     Set<Point> seen;
