6872aa8155d9fac8f261eafe87c9fc703a91507b
61lf0yl3 <samigull@usc.edu>
2022-03-17 00:59:48 -0700

    KokoEatingBananas

--- algo/KokoEatingBananas.java
+++ algo/KokoEatingBananas.java
@@ -1,4 +1,33 @@
 class KokoEatingBananas {
+
+    // Approach 2: Binary Search
+    // Time Complexity: O(N*logM)
+    // Space Complexity: O(1)
+    public int minEatingSpeed(int[] piles, int h) {
+        int max = 0;
+        int min = 1;
+        for (int num : piles) {
+            max = Math.max(num, max);
+        }
+
+        while (min < max) {
+            int mid = min + (max - min) / 2;
+            int currH = 0;
+            for (int pile : piles) {
+                currH += Math.ceil((double) pile / mid);
+                if (currH > h) {
+                    break;
+                }
+            }
+            if (currH <= h) {
+                max = mid;
+            } else if (currH > h) {
+                min = mid + 1;
+            }
+        }
+        return min;
+    }
+
     // Approach 2: Brute Force
     // Time Complexity: O(N*M)
     // Space Complexity: O(1)
