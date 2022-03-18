ff9e04d612c32cb28887f2af45425798535f675e
61lf0yl3 <samigull@usc.edu>
2022-03-18 00:54:18 -0700

    KthMissingPositiveNumber

--- algo/KthMissingPositiveNumber.java
+++ algo/KthMissingPositiveNumber.java
@@ -1,8 +1,29 @@
 class KthMissingPositiveNumber {
+
     // Approach 2: Iterative
     // Time Complexity: O(N)
-    // Space Complexity: O(N)
+    // Space Complexity: O(1)
     public int findKthPositive(int[] arr, int k) {
+        if (k < arr[0]) {
+            return k;
+        }
+
+        k -= arr[0] - 1;
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int missing = arr[i] - arr[i - 1] - 1;
+            if (missing >= k) {
+                return arr[i - 1] + k;
+            }
+            k -= missing;
+        }
+        return arr[n - 1] + k;
+    }
+
+    // Approach 3: Iterative
+    // Time Complexity: O(N)
+    // Space Complexity: O(N)
+    public int findKthPositive3(int[] arr, int k) {
         int[] nums = new int[1001];
         for (int i : arr) {
             nums[i] = 1;
