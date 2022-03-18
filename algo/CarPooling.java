6a64c2ca9e5cd1912e9a5cc46de4f44a4b20d8fd
61lf0yl3 <samigull@usc.edu>
2022-03-17 18:06:59 -0700

    CarPooling

--- algo/CarPooling.java
+++ algo/CarPooling.java
@@ -2,10 +2,29 @@ import java.util.Map;
 import java.util.TreeMap;
 
 class CarPooling {
-    // Approach 1: Time Stamp
-    // Time Complexity: O(NlogN)
+    // Approach 1: Bucket Sort
+    // Time Complexity: O(N)
     // Space Complexity: O(N)
     public boolean carPooling(int[][] trips, int capacity) {
+        int[] timestamp = new int[1001];
+        for (int[] trip : trips) {
+            timestamp[trip[1]] += trip[0];
+            timestamp[trip[2]] -= trip[0];
+        }
+
+        for (int currCapacity : timestamp) {
+            capacity -= currCapacity;
+            if (capacity < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Approach 2: Time Stamp
+    // Time Complexity: O(NlogN)
+    // Space Complexity: O(N)
+    public boolean carPooling2(int[][] trips, int capacity) {
         Map<Integer, Integer> m = new TreeMap<>();
 
         for (int[] trip : trips) {
