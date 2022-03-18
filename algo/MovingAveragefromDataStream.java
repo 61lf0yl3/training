d40fe9d26a46e24f41be378848ef407f6c00b7a4
61lf0yl3 <samigull@usc.edu>
2022-03-18 01:22:30 -0700

    MovingAverage

---
+++ algo/MovingAveragefromDataStream.java
@@ -0,0 +1,34 @@
+import java.util.Queue;
+import java.util.LinkedList;
+
+class MovingAverage {
+    private int size;
+    private int sum;
+    private Queue<Integer> last;
+
+    public MovingAverage(int size) {
+        this.size = size;
+        sum = 0;
+        size = 0;
+        last = new LinkedList<>();
+    }
+
+    public double next(int val) {
+        if (size > last.size()) {
+            sum += val;
+            last.add(val);
+            return sum * 1.0 / last.size();
+        } else {
+            sum -= last.poll();
+            last.add(val);
+            sum += val;
+            return sum * 1.0 / last.size();
+        }
+    }
+}
+
+/**
+ * Your MovingAverage object will be instantiated and called as such:
+ * MovingAverage obj = new MovingAverage(size);
+ * double param_1 = obj.next(val);
+ */
