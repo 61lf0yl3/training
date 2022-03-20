6032c6eef87bfb84d8421653da9e2a5b85e0a3b1
61lf0yl3 <samigull@usc.edu>
2022-03-19 18:10:07 -0700

    SubarraySumEqualsK

--- algo/SubarraySumEqualsK.java
+++ algo/SubarraySumEqualsK.java
@@ -44,24 +44,23 @@ public class SubarraySumEqualsK {
     // Space Complexity: O(N)
     public int subarraySum3(int[] nums, int k) {
         int res = 0;
-        int[] sum = new int[nums.length + 1];
-        for (int i = 1; i < sum.length; i++) {
-            sum[i] = nums[i - 1] + sum[i - 1];
-        }
-        for (int start = 0; start < sum.length - 1; start++) {
-            for (int end = start + 1; end < sum.length; end++) {
-                if (sum[end] - sum[start] == k) {
-                    res++;
-                }
+        int sum = 0;
+        Map<Integer, Integer> m = new HashMap<>();
+        m.put(0, 1);
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (m.containsKey(sum - k)) {
+                res += m.get(sum - k);
             }
+            m.put(sum, m.getOrDefault(sum, 0) + 1);
         }
         return res;
     }
 
-    // Approach #3: Brute Force
+    // Approach #4: Brute Force
     // Time Complexity: O(N^3)
     // Space Complexity: O(1)
-    public int subarraySum3(int[] nums, int k) {
+    public int subarraySum4(int[] nums, int k) {
         int count = 0;
         for (int start = 0; start < nums.length; start++) {
             for (int end = start + 1; end <= nums.length; end++) {
