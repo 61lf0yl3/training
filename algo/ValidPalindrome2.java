5e1c0f2e50cee52a3c546432e1846d228b559e8f
61lf0yl3 <samigull@usc.edu>
2022-03-20 17:18:42 -0700

    ValidPalindrome2

--- algo/ValidPalindrome2.java
+++ algo/ValidPalindrome2.java
@@ -22,4 +22,31 @@ class ValidPalindrome2 {
         return true;
     }
 
+    // Approach 1: Recursion with Two Pointers
+    // Time Complexity: O(N)
+    // Space Complexity: O(1)
+    public boolean validPalindrome2(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return checkPalindrome(s, i + 1, j) || checkPalindrome(s, i, j - 1);
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    private boolean checkPalindrome(String s, int i, int j) {
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
 }
