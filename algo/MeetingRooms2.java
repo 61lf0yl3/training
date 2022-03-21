f0fb358225c0df71fa017787c4d038e6534a5771
61lf0yl3 <samigull@usc.edu>
2022-03-20 22:15:37 -0700

    minMeetingRooms

--- algo/MeetingRooms2.java
+++ algo/MeetingRooms2.java
@@ -41,6 +41,21 @@ class MeetingRooms2 {
         return q.size();
     }
 
+    // Approach 1: Priority Queues
+    // Time Complexity: O(NlogN)
+    // Space Complexity: O(N)
+    public int minMeetingRooms1(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int[] interval : intervals) {
+            if (!pq.isEmpty() && pq.peek() <= interval[0]) {
+                pq.poll();
+            }
+            pq.add(interval[1]);
+        }
+        return pq.size();
+    }
+
     // Approach 2: Chronological Ordering
     // Time Complexity: O(NlogN)
     // Space Complexity: O(N)
