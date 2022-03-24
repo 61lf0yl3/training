1c4dbbbd268355e77495e25ac28e9c6b1d5b1df1
61lf0yl3 <samigull@usc.edu>
2022-03-24 00:23:10 -0700

    KClosestPointstoOrigin

--- algo/KClosestPointstoOrigin.java
+++ algo/KClosestPointstoOrigin.java
@@ -80,6 +80,23 @@ class KClosestPointstoOrigin {
 
         return res;
     }
+
+    public int[][] kClosest4(int[][] points, int k) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+                (a, b) -> (b[0] * b[0] + b[1] * b[1]) - (a[0] * a[0] + a[1] * a[1]));
+        for (int[] point : points) {
+            pq.add(point);
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+        int[][] res = new int[pq.size()][2];
+        int i = 0;
+        while (!pq.isEmpty()) {
+            res[i++] = pq.poll();
+        }
+        return res;
+    }
 }
 
 class SortDistances implements Comparator<Pair<Integer, Double>> {
