910db4ed54bb45d54f0f037f462567239bc6c5af
61lf0yl3 <samigull@usc.edu>
2022-03-25 16:13:13 -0700

    search4

--- algo/SearchinRotatedSortedArray.java
+++ algo/SearchinRotatedSortedArray.java
@@ -144,4 +144,32 @@ class SearchinRotatedSortedArray {
     // search in the left side
     return search(0, rotate_index);
   }
+
+  public int search4(int[] nums, int target) {
+    int low = 0;
+    int high = nums.length - 1;
+    while (low <= high) {
+      int mid = low + (high - low) / 2;
+
+      if (nums[mid] == target) {
+        return mid;
+      }
+
+      if (nums[low] <= nums[mid]) {
+        if (nums[low] <= target && target < nums[mid]) {
+          high = mid - 1;
+        } else {
+          low = mid + 1;
+        }
+      } else {
+        if (nums[mid] < target && target <= nums[high]) {
+          low = mid + 1;
+        } else {
+          high = mid - 1;
+        }
+      }
+
+    }
+    return -1;
+  }
 }
