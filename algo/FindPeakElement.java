fc072c3e1eed89f0b980f43a29ddfffe726bcd82
61lf0yl3 <samigull@usc.edu>
2022-03-30 16:40:45 -0700

    findPeakElement

--- algo/FindPeakElement.java
+++ algo/FindPeakElement.java
@@ -1,18 +1,20 @@
 class FindPeakElement {
-    //162. Find Peak Element
+    // 162. Find Peak Element
 
-    //Approach 1: Binary Search
-    //Time Complexity:O(logN)
-    //Space Complexity: O(1)
+    // Approach 1: Binary Search
+    // Time Complexity:O(logN)
+    // Space Complexity: O(1)
     public int findPeakElement(int[] nums) {
         int low = 0;
-        int high = nums.length-1;
-        while(low<high) {
-            int mid = low + (high-low)/2;
-            if (nums[mid]>nums[mid+1]) {
-                high = mid;
+        int high = nums.length - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] < nums[mid + 1]) {
+                low = mid + 1;
             } else {
-                low = mid+1;
+                high = mid;
             }
         }
         return low;
