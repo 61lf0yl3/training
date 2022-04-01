3787b6ebd5f015e95c991df3151dda50309dc38b
61lf0yl3 <samigull@usc.edu>
2022-04-01 03:24:41 -0700

    ReverseInteger

--- algo/ReverseInteger.java
+++ algo/ReverseInteger.java
@@ -19,4 +19,22 @@ class ReverseInteger {
         }
         return res;
     }
+
+    // Approach 1: Pop and Push Digits & Check before Overflow
+    // Time Complexity: O(logN)
+    // Space Complexity: O(1)
+    public int reverse2(int x) {
+        int sign = x < 0 ? -1 : 1;
+        x = Math.abs(x);
+        long res = 0;
+        while (x > 0) {
+            res = res * 10 + x % 10;
+            x /= 10;
+        }
+        res *= sign;
+        if (res > Integer.MAX_VALUE || res < Integer.MIN_VALUE) {
+            return 0;
+        }
+        return (int) res;
+    }
 }
