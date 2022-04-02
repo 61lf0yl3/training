d0c5b855834a224a7c592222aa78eb6646929d67
61lf0yl3 <samigull@usc.edu>
2022-04-02 02:12:38 -0700

    MinimumKnightMoves

--- algo/MinimumKnightMoves.java
+++ algo/MinimumKnightMoves.java
@@ -44,27 +44,25 @@ class MinimumKnightMoves {
     // Approach 2: DFS
     // Time complexity : O(|x|*|y|)
     // Space complexity : O(|x|*|y|)
-    private Map<String, Integer> memo = new HashMap<>();
+    private HashMap<String, Integer> memo;
 
-    private int dfs(int x, int y) {
+    private int dp(int x, int y) {
         String key = x + "," + y;
-        if (memo.containsKey(key)) {
-            return memo.get(key);
-        }
-
-        if (x + y == 0) {
+        if (x == 0 && y == 0) {
             return 0;
         } else if (x + y == 2) {
             return 2;
-        } else {
-            Integer ret = Math.min(dfs(Math.abs(x - 1), Math.abs(y - 2)),
-                    dfs(Math.abs(x - 2), Math.abs(y - 1))) + 1;
-            memo.put(key, ret);
-            return ret;
+        } else if (!memo.containsKey(key)) {
+            int res = Math.min(dp(Math.abs(x - 1), Math.abs(y - 2)),
+                    dp(Math.abs(x - 2), Math.abs(y - 1))) + 1;
+            memo.put(key, res);
+
         }
+        return memo.get(key);
     }
 
     public int minKnightMoves2(int x, int y) {
-        return dfs(Math.abs(x), Math.abs(y));
+        memo = new HashMap<>();
+        return dp(x, y);
     }
 }
