6fbb771f4980a5d650b5d40a35cbc431b043fdcc
61lf0yl3 <samigull@usc.edu>
2022-04-04 15:26:13 -0700

    ProductofArrayExceptSelf

--- algo/ProductofArrayExceptSelf.java
+++ algo/ProductofArrayExceptSelf.java
@@ -5,19 +5,23 @@ class ProductofArrayExceptSelf {
     // Time complexity : O(N)
     // Space complexity : O(N)
     public int[] productExceptSelf(int[] nums) {
-        int[] left = new int[nums.length];
+        int n = nums.length;
+        int[] left = new int[n];
         left[0] = 1;
-        for (int i = 1; i < nums.length; i++) {
+
+        for (int i = 1; i < n; i++) {
             left[i] = left[i - 1] * nums[i - 1];
         }
 
-        int[] right = new int[nums.length];
-        right[nums.length - 1] = 1;
-        for (int i = nums.length - 2; i >= 0; i--) {
+        int[] right = new int[n];
+        right[n - 1] = 1;
+
+        for (int i = n - 2; i >= 0; i--) {
             right[i] = right[i + 1] * nums[i + 1];
         }
-        int[] res = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
+
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++) {
             res[i] = left[i] * right[i];
         }
         return res;
