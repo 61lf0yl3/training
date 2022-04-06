7ec70bf46b236bcdd3336fd6ca43d271cd8e3852
61lf0yl3 <samigull@usc.edu>
2022-04-05 17:27:49 -0700

    ContainerWithMostWater

--- algo/ContainerWithMostWater.java
+++ algo/ContainerWithMostWater.java
@@ -1,24 +1,23 @@
 class ContainerWithMostWater {
-    //11. Container With Most Water
+    // 11. Container With Most Water
 
-    //Approach #1: Two pointer
-    //Time Complexity: O(N)
-    //Space Complexity: O(1)
+    // Approach #1: Two pointer
+    // Time Complexity: O(N)
+    // Space Complexity: O(1)
     public int maxArea(int[] height) {
+        int n = height.length;
         int low = 0;
-        int high = height.length-1;
-        int max = 0;
-        while (low <= high) {
-            int temp = (high - low)*Math.min(height[low],height[high]);
+        int high = n - 1;
+        int res = 0;
+        while (low < high) {
+            int temp = (high - low) * Math.min(height[low], height[high]);
+            res = Math.max(res, temp);
             if (height[low] < height[high]) {
                 low++;
             } else {
                 high--;
             }
-            if (temp > max) {
-                max = temp;
-            }
         }
-        return max;
+        return res;
     }
 }
