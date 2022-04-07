f05c2249ae72716d154308af978be41c1ac79ba8
61lf0yl3 <samigull@usc.edu>
2022-04-07 03:01:49 -0700

    AsteroidCollision

--- algo/AsteroidCollision.java
+++ algo/AsteroidCollision.java
@@ -7,33 +7,33 @@ class AsteroidCollision {
     // Time complexity : O(N)
     // Space complexity : O(N)
     public int[] asteroidCollision(int[] asteroids) {
-        Stack<Integer> s = new Stack<>();
-        for (int a : asteroids) {
-            if (s.isEmpty() || a > 0) {
-                s.add(a);
+        Stack<Integer> stack = new Stack<>();
+        for (int asteroid : asteroids) {
+            if (stack.isEmpty() || asteroid > 0) {
+                stack.add(asteroid);
             } else {
-                boolean insertA = true;
-                while (!s.isEmpty() && s.peek() > 0) {
-                    if (s.peek() == -a) {
-                        insertA = false;
-                        s.pop();
+                boolean PUSH = true;
+                while (!stack.isEmpty() && stack.peek() > 0) {
+                    if (stack.peek() == -asteroid) {
+                        PUSH = false;
+                        stack.pop();
                         break;
-                    } else if (s.peek() < -a) {
-                        s.pop();
+                    } else if (stack.peek() < -asteroid) {
+                        stack.pop();
                     } else {
-                        insertA = false;
+                        PUSH = false;
                         break;
                     }
                 }
-                if (insertA) {
-                    s.add(a);
+                if (PUSH) {
+                    stack.add(asteroid);
                 }
             }
         }
-
-        int[] res = new int[s.size()];
-        for (int i = res.length - 1; i >= 0; i--) {
-            res[i] = s.pop();
+        int[] res = new int[stack.size()];
+        int i = stack.size() - 1;
+        while (!stack.isEmpty()) {
+            res[i--] = stack.pop();
         }
         return res;
     }
