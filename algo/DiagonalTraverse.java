abd743547675db21d0db2ad6014e15afc97d3788
61lf0yl3 <samigull@usc.edu>
2022-04-09 00:38:56 -0400

    findDiagonalOrder

--- algo/DiagonalTraverse.java
+++ algo/DiagonalTraverse.java
@@ -8,10 +8,9 @@ public class DiagonalTraverse {
     public int[] findDiagonalOrder(int[][] mat) {
         int n = mat.length;
         int m = mat[0].length;
-        int x = 0;
         int[] res = new int[n * m];
+        int i = 0;
         for (int d = 0; d < n + m - 1; d++) {
-
             int r = d < m ? 0 : d - m + 1;
             int c = d < m ? d : m - 1;
             ArrayList<Integer> temp = new ArrayList<>();
@@ -19,10 +18,13 @@ public class DiagonalTraverse {
                 temp.add(mat[r++][c--]);
             }
             if (d % 2 == 0) {
-                Collections.reverse(temp);
-            }
-            for (int num : temp) {
-                res[x++] = num;
+                for (int j = temp.size() - 1; j >= 0; j--) {
+                    res[i++] = temp.get(j);
+                }
+            } else {
+                for (int num : temp) {
+                    res[i++] = num;
+                }
             }
         }
         return res;
