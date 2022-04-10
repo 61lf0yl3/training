19815aade69a82eddbfaf63712404cfbe636b9a9
61lf0yl3 <samigull@usc.edu>
2022-04-09 22:33:20 -0700

    KdiffPairsinanArray

--- algo/K-diffPairsinanArray.java
+++ algo/K-diffPairsinanArray.java
@@ -2,6 +2,11 @@ import java.util.HashSet;
 
 class KdiffPairsinanArray {
 
+    // 532. K-diff Pairs in an Array
+
+    // Approach 1: HashSet
+    // Time complexity : O(N)
+    // Space complexity : O(N)
     public int findPairs(int[] nums, int k) {
         HashSet<Integer> s1 = new HashSet<>();
         HashSet<String> s2 = new HashSet<>();
@@ -21,6 +26,26 @@ class KdiffPairsinanArray {
             }
 
             s1.add(nums[i]);
+        }
+        return res;
+    }
+
+    // Approach 1: Hashmap
+    // Time complexity : O(N)
+    // Space complexity : O(N)
+    public int findPairs2(int[] nums, int k) {
+        HashMap<Integer, Integer> m = new HashMap<>();
+        for (int num : nums) {
+            m.put(num, m.getOrDefault(num, 0) + 1);
+        }
+
+        int res = 0;
+        for (int key : m.keySet()) {
+            if (k > 0 && m.containsKey(key + k)) {
+                res++;
+            } else if (k == 0 && m.get(key) > 1) {
+                res++;
+            }
         }
         return res;
     }
