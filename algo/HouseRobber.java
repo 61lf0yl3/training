8035eeb0e83c0992dbe5f2d5063aac8477acdf57
61lf0yl3 <samigull@usc.edu>
2022-04-13 02:06:56 -0700

    HouseRobber

--- algo/HouseRobber.java
+++ algo/HouseRobber.java
@@ -27,18 +27,17 @@ class HouseRobber {
     // Time Complexity: O(N)
     // Space Complexity: O(1)
     public int rob5(int[] nums) {
-        if (nums.length == 1) {
-            return nums[0];
-        }
         int[] dp = new int[nums.length];
         dp[0] = nums[0];
+        if (nums.length == 1) {
+            return dp[0];
+        }
         dp[1] = Math.max(nums[0], nums[1]);
         for (int i = 2; i < nums.length; i++) {
-            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+            dp[i] = Math.max(dp[i-1], dp[i-2]+nums[i]);
         }
-
-        return dp[nums.length - 1];
-    }
+        return dp[nums.length-1];
+    }return dp[nums.length-1];}
 
     // Approach 2: Recursion
     // Time Complexity: O(N)
