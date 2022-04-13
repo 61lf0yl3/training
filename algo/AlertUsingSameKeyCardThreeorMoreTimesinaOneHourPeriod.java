dfa116d1a27678268b6033658b3e53fc5e22e4b2
61lf0yl3 <samigull@usc.edu>
2022-04-13 03:19:12 -0700

    AlertUsingSameKeyCardThreeorMoreTimesinaOneHourPeriod

--- algo/AlertUsingSameKeyCardThreeorMoreTimesinaOneHourPeriod.java
+++ algo/AlertUsingSameKeyCardThreeorMoreTimesinaOneHourPeriod.java
@@ -1,8 +1,10 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeSet;
 
 class AlertUsingSameKeyCardThreeorMoreTimesinaOneHourPeriod {
@@ -44,5 +46,40 @@ class AlertUsingSameKeyCardThreeorMoreTimesinaOneHourPeriod {
     private Integer parse(String time) {
         return 600 * (time.charAt(0) - '0') + 60 * (time.charAt(1) - '0') + 10 * (time.charAt(3) - '0')
                 + (time.charAt(4) - '0');
+    }
+
+    public List<String> alertNames2(String[] keyName, String[] keyTime) {
+        Map<String, ArrayList<Integer>> m = new HashMap<>();
+
+        Set<String> alertNames = new HashSet<>();
+
+        for (int i = 0; i < keyName.length; i++) {
+            if (alertNames.contains(keyName[i])) {
+                continue;
+            }
+            if (!m.containsKey(keyName[i])) {
+                m.put(keyName[i], new ArrayList<>());
+            }
+            String timeString = keyTime[i];
+            int time = parse(keyTime[i]);
+
+            m.get(keyName[i]).add(time);
+
+            if (m.get(keyName[i]).size() >= 3) {
+                int size = m.get(keyName[i]).size();
+                if (m.get(keyName[i]).get(size - 1) - m.get(keyName[i]).get(size - 3) <= 60) {
+                    alertNames.add(keyName[i]);
+                    // break;
+                }
+            }
+        }
+
+        List<String> res = new ArrayList<>();
+        for (String name : alertNames) {
+            res.add(name);
+        }
+
+        Collections.sort(res);
+        return res;
     }
 }
