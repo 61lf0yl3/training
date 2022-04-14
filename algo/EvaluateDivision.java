28626b6d59f8bacd27a7ef3474be14e75cd10772
61lf0yl3 <samigull@usc.edu>
2022-04-14 02:47:35 -0700

    EvaluateDivision

---
+++ algo/EvaluateDivision.java
@@ -0,0 +1,72 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
+class EvaluateDivision {
+    HashMap<String, HashMap<String, Double>> graph;
+
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        graph = new HashMap<>();
+
+        // Step 1). build the graph from the equations
+        for (int i = 0; i < equations.size(); i++) {
+            List<String> equation = equations.get(i);
+            String numerator = equation.get(0);
+            String denominator = equation.get(1);
+            double value = values[i];
+            if (!graph.containsKey(numerator)) {
+                graph.put(numerator, new HashMap<String, Double>());
+            }
+            graph.get(numerator).put(denominator, value);
+
+            if (!graph.containsKey(denominator)) {
+                graph.put(denominator, new HashMap<String, Double>());
+            }
+            graph.get(denominator).put(numerator, 1 / value);
+        }
+
+        // Step 2). Evaluate each query via bactracking (DFS)
+        // by verifying if there exists a path from dividend to divisor
+        double[] res = new double[queries.size()];
+        for (int i = 0; i < queries.size(); i++) {
+            List<String> query = queries.get(i);
+            String numerator = query.get(0);
+            String denominator = query.get(1);
+
+            if (!graph.containsKey(numerator) || !graph.containsKey(denominator)) {
+                res[i] = -1.0;
+            } else if (numerator.equals(denominator)) {
+                res[i] = 1.0;
+            } else {
+                res[i] = dfs(new HashSet<String>(), numerator, denominator, 1);
+            }
+
+        }
+        return res;
+    }
+
+    private double dfs(HashSet<String> seen, String numerator, String denominator, double value) {
+        // mark the visit
+        seen.add(numerator);
+        double ret = -1.0;
+
+        HashMap<String, Double> neighbors = graph.get(numerator);
+        if (neighbors.containsKey(denominator)) {
+            ret = value * neighbors.get(denominator);
+        } else {
+            for (String neighbor : neighbors.keySet()) {
+                if (seen.contains(neighbor)) {
+                    continue;
+                }
+                ret = dfs(seen, neighbor, denominator, value * neighbors.get(neighbor));
+                if (ret != -1.0) {
+                    break;
+                }
+            }
+        }
+
+        // unmark the visit, for the next backtracking
+        seen.remove(numerator);
+        return ret;
+    }
+}
