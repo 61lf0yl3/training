9aaf1385714ab194720c43584e5ab67ee1b1e6c5
61lf0yl3 <samigull@usc.edu>
2022-04-15 03:52:44 -0700

    CarPooling

--- algo/CarPooling.java
+++ algo/CarPooling.java
@@ -26,23 +26,18 @@ class CarPooling {
     // Space Complexity: O(N)
     public boolean carPooling2(int[][] trips, int capacity) {
         Map<Integer, Integer> m = new TreeMap<>();
-
         for (int[] trip : trips) {
-            int startPassenger = m.getOrDefault(trip[1], 0) + trip[0];
-            m.put(trip[1], startPassenger);
-
-            int endPassenger = m.getOrDefault(trip[2], 0) - trip[0];
-            m.put(trip[2], endPassenger);
-
+            m.put(trip[1], m.getOrDefault(trip[1], 0)+trip[0]);
+            m.put(trip[2], m.getOrDefault(trip[2], 0)-trip[0]);
         }
-        int currCapacity = 0;
-        for (int val : m.values()) {
-            currCapacity += val;
-            if (currCapacity > capacity) {
+        
+        for (int key : m.values()) {
+            capacity -= key;
+            if (capacity < 0) {
                 return false;
             }
         }
-
         return true;
+    }
     }
 }
