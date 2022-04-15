3d6ea5c6ca89951fa6dde13aa7fb8bc7c691a745
61lf0yl3 <samigull@usc.edu>
2022-04-15 02:23:05 -0700

    TopKFrequentWords

--- algo/TopKFrequentWords.java
+++ algo/TopKFrequentWords.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
@@ -34,5 +35,21 @@ class TopKFrequentWords {
             }
             return a.getKey().compareTo(b.getKey());
         }
+    }
+
+    public List<String> topKFrequent2(String[] words, int k) {
+        HashMap<String, Integer> freq = new HashMap<>();
+        for (int i = 0; i < words.length; i++) {
+            freq.put(words[i], freq.getOrDefault(words[i], 0) + 1);
+        }
+        List<String> res = new ArrayList(freq.keySet());
+        // sorting
+        // if two words have the same frequency, then the word with the lower
+        // alphabetical order comes first.
+        // else most frequent words will come first
+        Collections.sort(res,
+                (w1, w2) -> freq.get(w1).equals(freq.get(w2)) ? w1.compareTo(w2) : freq.get(w2) - freq.get(w1));
+
+        return res.subList(0, k);
     }
 }
