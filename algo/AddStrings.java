3f3e33ba909a51a61cde97f600de6926d86eb622
61lf0yl3 <samigull@usc.edu>
2022-04-18 15:20:19 -0700

    AddStrings

--- algo/AddStrings.java
+++ algo/AddStrings.java
@@ -5,26 +5,24 @@ class AddStrings {
     // Time Complexity: O(N)
     // Space Complexity: O(1)
     public String addStrings(String num1, String num2) {
+
         StringBuilder res = new StringBuilder();
         int remainder = 0;
         int i1 = num1.length() - 1;
         int i2 = num2.length() - 1;
         while (i1 >= 0 || i2 >= 0) {
-            int nb1 = 0;
-            int nb2 = 0;
+            int d1 = 0;
+            int d2 = 0;
             if (i1 >= 0) {
-                nb1 = num1.charAt(i1) - '0';
+                d1 = num1.charAt(i1--) - '0';
             }
             if (i2 >= 0) {
-                nb2 = num2.charAt(i2) - '0';
+                d2 = num2.charAt(i2--) - '0';
             }
-            int value = (nb1 + nb2 + remainder) % 10;
-            remainder = (nb1 + nb2 + remainder) / 10;
-            res.append(value);
-            i1--;
-            i2--;
+            res.append((d1 + d2 + remainder) % 10);
+            remainder = (d1 + d2 + remainder) / 10;
         }
-        if (remainder != 0) {
+        if (remainder > 0) {
             res.append(remainder);
         }
         return res.reverse().toString();
