de66dd668429f403173c0cb2b030c23ab7fbd576
61lf0yl3 <samigull@usc.edu>
2022-04-18 13:57:06 -0700

    SingleElementinaSortedArray

--- algo/SingleElementinaSortedArray.java
+++ algo/SingleElementinaSortedArray.java
@@ -5,13 +5,11 @@ class SingleElementinaSortedArray {
     // Time complexity : O(logN)
     // Space complexity : O(1)
     public int singleNonDuplicate(int[] nums) {
-        if (nums.length == 1) {
-            return nums[0];
-        }
         int low = 0;
         int high = nums.length - 1;
         while (low < high) {
             int mid = low + (high - low) / 2;
+
             if (nums[mid] == nums[mid - 1]) {
                 if (mid % 2 == 0) {
                     high = mid;
