b3516b742ad9ffba2e3fb0017cb07b3fd2cfc264
61lf0yl3 <samigull@usc.edu>
2022-04-19 16:08:47 -0700

    shortestPathBinaryMatrix

--- algo/ShortestPathinBinaryMatrix.java
+++ algo/ShortestPathinBinaryMatrix.java
@@ -1,43 +1,38 @@
 import java.util.Queue;
 import java.util.LinkedList;
 
-
 class ShortestPathinBinaryMatrix {
 
-    //Approach 1: Breadth-first Search (BFS), Overwriting Input
-    //Time Complexity: O(N)
-    //Space Complexity: O(N)
+    // Approach 1: Breadth-first Search (BFS), Overwriting Input
+    // Time Complexity: O(N)
+    // Space Complexity: O(N)
     public int shortestPathBinaryMatrix(int[][] grid) {
-        if (grid[0][0] != 0 ||  grid[grid.length-1][grid.length-1]!= 0) {
-             return -1;
+        int n = grid.length;
+        if (grid[0][0] != 0 || grid[n - 1][n - 1] != 0) {
+            return -1;
         }
-        
+
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{0,0});
+        q.add(new int[] { 0, 0 });
         grid[0][0] = 1;
-        if (grid.length>1) {
-            grid[grid.length-1][grid.length-1] =-1;
+        if (n > 1) {
+            grid[n - 1][n - 1] = -1;
         }
-        int[][] directions = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
-    
+        int[][] directions = { { -1, -1 }, { -1, 0 }, { -1, 1 }, { 0, -1 }, { 0, 1 }, { 1, -1 }, { 1, 0 }, { 1, 1 } };
 
         while (!q.isEmpty()) {
-            int[] cell = q.remove();
-            int currR = cell[0];
-            int currC = cell[1];
-            int distance = grid[currR][currC];
-            
-            for (int i = 0; i<directions.length; i++) {
-                int newR = currR+directions[i][0];
-                int newC = currC+directions[i][1];
-                
-                if (newR>=0 && newC>=0 && newR < grid.length && newC < grid[0].length && grid[newR][newC]<=0) {
-                    q.add(new int[]{newR, newC});
-                    grid[newR][newC] = distance+1;
+            int[] point = q.poll();
+            int r = point[0];
+            int c = point[1];
+            for (int[] direction : directions) {
+                int newR = r + direction[0];
+                int newC = c + direction[1];
+                if (newR >= 0 && newC >= 0 && newR < n && newC < n && grid[newR][newC] <= 0) {
+                    q.add(new int[] { newR, newC });
+                    grid[newR][newC] = grid[r][c] + 1;
                 }
             }
         }
-        return grid[grid.length-1][grid.length-1];
-        
+        return grid[n - 1][n - 1];
     }
 }
