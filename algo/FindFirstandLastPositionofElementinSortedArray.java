4e710607bf6cf49d9bfa136529d7e6ef8ba5ff56
61lf0yl3 <samigull@usc.edu>
2022-04-19 14:53:03 -0700

    FindFirstandLastPositionofElementinSortedArray

---
+++ algo/FindFirstandLastPositionofElementinSortedArray.java
@@ -0,0 +1,43 @@
+class FindFirstandLastPositionofElementinSortedArray {
+
+    // Approach 1: Binary Search
+    // Time Complexity: O(logN)
+    // Space Complexity: O(1)
+    public int[] searchRange(int[] nums, int target) {
+        int[] res = new int[] { -1, -1 };
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                if (mid == low || nums[mid - 1] < target) {
+                    res[0] = mid;
+                    break;
+                }
+                high = mid - 1;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        low = 0;
+        high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                if (mid == high || target < nums[mid + 1]) {
+                    res[1] = mid;
+                    break;
+                }
+                low = mid + 1;
+            } else if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return res;
+    }
+}
