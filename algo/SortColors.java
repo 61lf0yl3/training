05eb703413324f93d8e986110b061383545bcd68
61lf0yl3 <samigull@usc.edu>
2022-04-25 22:10:12 -0400

    SortColors

--- algo/SortColors.java
+++ algo/SortColors.java
@@ -1,10 +1,28 @@
 import java.util.Arrays;
 
 class SortColors {
+    public void sortColors(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+        int curr = 0;
+
+        while (curr <= high) {
+            if (nums[curr] == 0) {
+                nums[curr++] = nums[low];
+                nums[low++] = 0;
+            } else if (nums[curr] == 2) {
+                nums[curr] = nums[high];
+                nums[high--] = 2;
+            } else {
+                curr++;
+            }
+        }
+    }
+
     // Approach 3: Buble Sort
     // Time Complexity: O(N^2)
     // Space Complexity: O(N)
-    public void sortColors(int[] nums) {
+    public void sortColors4(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = 0; j < nums.length; j++) {
                 if (nums[i] < nums[j]) {
