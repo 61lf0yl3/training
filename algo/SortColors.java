de65cafc64fad3a995f2e4470935062b88fbe587
61lf0yl3 <samigull@usc.edu>
2022-04-25 22:11:12 -0400

    SortColors

--- algo/SortColors.java
+++ algo/SortColors.java
@@ -1,6 +1,9 @@
 import java.util.Arrays;
 
 class SortColors {
+    // Approach 1: One Pass
+    // Time Complexity: O(N)
+    // Space Complexity: O(1)
     public void sortColors(int[] nums) {
         int low = 0;
         int high = nums.length - 1;
@@ -22,7 +25,7 @@ class SortColors {
     // Approach 3: Buble Sort
     // Time Complexity: O(N^2)
     // Space Complexity: O(N)
-    public void sortColors4(int[] nums) {
+    public void sortColors3(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = 0; j < nums.length; j++) {
                 if (nums[i] < nums[j]) {
@@ -39,25 +42,5 @@ class SortColors {
     // Space Complexity: O(N)
     public void sortColors2(int[] nums) {
         Arrays.sort(nums);
-    }
-
-    // Approach 1: One Pass
-    // Time Complexity: O(NlogN)
-    // Space Complexity: O(N)
-    public void sortColors3(int[] nums) {
-        int p0 = 0;
-        int curr = 0;
-        int p2 = nums.length - 1;
-        while (curr <= p2) {
-            if (nums[curr] == 0) {
-                nums[curr++] = nums[p0];
-                nums[p0++] = 0;
-            } else if (nums[curr] == 2) {
-                nums[curr] = nums[p2];
-                nums[p2--] = 2;
-            } else {
-                curr++;
-            }
-        }
     }
 }
