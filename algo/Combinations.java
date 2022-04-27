c72bef98e135078ce451272c6e670722ca880617
61lf0yl3 <samigull@usc.edu>
2022-04-26 23:36:37 -0400

    Combinations

---
+++ algo/Combinations.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+class Solution {
+    List<List<Integer>> res;
+    int n;
+    int k;
+
+    public List<List<Integer>> combine(int n, int k) {
+        res = new ArrayList<>();
+        this.n = n;
+        this.k = k;
+        backtrak(1, new LinkedList<>());
+        return res;
+    }
+
+    private void backtrak(int first, LinkedList<Integer> comb) {
+        if (comb.size() == k) {
+            res.add(new ArrayList<>(comb));
+            return;
+        } else {
+            for (int i = first; i <= n; i++) {
+                comb.add(i);
+                backtrak(i + 1, comb);
+                comb.removeLast();
+            }
+        }
+    }
+}
