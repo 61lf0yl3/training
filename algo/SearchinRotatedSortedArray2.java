132fe0e673459095b69682eef1a4d61d9242a9b0
61lf0yl3 <samigull@usc.edu>
2022-04-29 02:27:33 -0400

    SearchinRotatedSortedArray2

---
+++ algo/SearchinRotatedSortedArray2.java
@@ -0,0 +1,32 @@
+class SearchinRotatedSortedArray2 {
+    // 81. Search in Rotated Sorted Array II
+
+    // Approach 1: Binary Search
+    // Time Complexity: O(logN)
+    // Space Complexity: O(1)
+    public boolean search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                return true;
+            } else if (nums[low] < nums[mid]) {
+                if (nums[low] <= target && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else if (nums[low] == nums[mid]) {
+                low++;
+            } else {
+                if (nums[mid] < target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+}
