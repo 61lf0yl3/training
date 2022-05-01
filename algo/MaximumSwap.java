2af82069802e45a3494eb445188711aba538ca81
61lf0yl3 <samigull@usc.edu>
2022-05-01 03:04:19 -0400

    maximumSwap2

--- algo/MaximumSwap.java
+++ algo/MaximumSwap.java
@@ -26,4 +26,27 @@ public class MaximumSwap {
         }
         return num;
     }
+
+    public int maximumSwap2(int num) {
+        char[] digits = String.valueOf(num).toCharArray();
+        int n = digits.length;
+        for (int i = 0; i < n; i++) {
+            char max = digits[i];
+            int maxIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (digits[j] >= max) {
+                    max = digits[j];
+                    maxIndex = j;
+                }
+            }
+            for (int j = 0; j < maxIndex; j++) {
+                if (max > digits[j]) {
+                    digits[maxIndex] = digits[j];
+                    digits[j] = max;
+                    return Integer.parseInt(new String(digits));
+                }
+            }
+        }
+        return num;
+    }
 }
