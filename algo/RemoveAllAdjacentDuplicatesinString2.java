b77ca799a42b24f6c5344227a8d92494ee3e3572
61lf0yl3 <samigull@usc.edu>
2022-05-04 13:24:20 -0400

    removeDuplicates5

--- algo/RemoveAllAdjacentDuplicatesinString2.java
+++ algo/RemoveAllAdjacentDuplicatesinString2.java
@@ -96,4 +96,30 @@ class RemoveAllAdjacentDuplicatesinString2 {
         }
         return res.toString();
     }
+
+    public String removeDuplicates5(String s, int k) {
+        Stack<Pair<Character, Integer>> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+
+            char curr = s.charAt(i);
+
+            if (!stack.isEmpty() && curr == stack.peek().getKey()) {
+                Pair<Character, Integer> newPair = new Pair<>(curr, stack.peek().getValue() + 1);
+                stack.pop();
+                stack.add(newPair);
+            } else {
+                stack.add(new Pair<>(curr, 1));
+            }
+            if (stack.peek().getValue() == k) {
+                stack.pop();
+            }
+        }
+        StringBuilder res = new StringBuilder();
+        for (Pair<Character, Integer> p : stack) {
+            for (int i = 0; i < p.getValue(); i++) {
+                res.append(p.getKey());
+            }
+        }
+        return res.toString();
+    }
 }
