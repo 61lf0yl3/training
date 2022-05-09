2a10456c44bfbd78a8aeb42731405c72ab85067a
61lf0yl3 <samigull@usc.edu>
2022-05-09 01:12:03 -0400

    findDiagonalOrder

--- algo/DiagonalTraverse.java
+++ algo/DiagonalTraverse.java
@@ -7,30 +7,6 @@ public class DiagonalTraverse {
     // Approach 1: Diagonal Iteration and Reversal
     // Time Complexity: O(N*M)
     // Space Complexity: O(N*M)
-    public int[] findDiagonalOrder(int[][] mat) {
-        int n = mat.length;
-        int m = mat[0].length;
-        int[] res = new int[n * m];
-        int i = 0;
-        for (int d = 0; d < n + m - 1; d++) {
-            int r = d < m ? 0 : d - m + 1;
-            int c = d < m ? d : m - 1;
-            ArrayList<Integer> temp = new ArrayList<>();
-            while (r < n && c >= 0) {
-                temp.add(mat[r++][c--]);
-            }
-            if (d % 2 == 0) {
-                for (int j = temp.size() - 1; j >= 0; j--) {
-                    res[i++] = temp.get(j);
-                }
-            } else {
-                for (int num : temp) {
-                    res[i++] = num;
-                }
-            }
-        }
-        return res;
-    }
 
     // Approach 2: Simulation
     // Time Complexity: O(N*M)
