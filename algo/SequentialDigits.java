c7b2454cbbe12d73dd9506bc7d8d1b72a50b98e2
61lf0yl3 <samigull@usc.edu>
2022-05-10 01:29:46 -0400

    SequentialDigits

--- algo/SequentialDigits.java
+++ algo/SequentialDigits.java
@@ -24,4 +24,24 @@ class SequentialDigits {
         }
         return res;
     }
+
+    // Approach 1: Sliding Window
+    // Time Complexity: O(1)
+    // Space Complexity: O(1)
+    public List<Integer> sequentialDigits2(int low, int high) {
+        String sample = "123456789";
+        int n = 10;
+        List<Integer> nums = new ArrayList();
+
+        int lowLen = String.valueOf(low).length();
+        int highLen = String.valueOf(high).length();
+        for (int length = lowLen; length < highLen + 1; ++length) {
+            for (int start = 0; start < n - length; ++start) {
+                int num = Integer.parseInt(sample.substring(start, start + length));
+                if (num >= low && num <= high)
+                    nums.add(num);
+            }
+        }
+        return nums;
+    }
 }
