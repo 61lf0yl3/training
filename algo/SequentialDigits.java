8b379504781624ecda133933977516cd5c7e2150
61lf0yl3 <samigull@usc.edu>
2022-05-10 01:30:14 -0400

    SequentialDigits

--- algo/SequentialDigits.java
+++ algo/SequentialDigits.java
@@ -44,4 +44,31 @@ class SequentialDigits {
         }
         return nums;
     }
+
+    public static Seq s = new Seq();
+
+    public List<Integer> sequentialDigits3(int low, int high) {
+        List<Integer> output = new ArrayList();
+        for (int num : s.nums) {
+            if (num >= low && num <= high)
+                output.add(num);
+        }
+        return output;
+    }
+}
+
+class Seq {
+    public List<Integer> nums = new ArrayList();
+
+    Seq() {
+        String sample = "123456789";
+        int n = 10;
+
+        for (int length = 2; length < n; ++length) {
+            for (int start = 0; start < n - length; ++start) {
+                int num = Integer.parseInt(sample.substring(start, start + length));
+                nums.add(num);
+            }
+        }
+    }
 }
