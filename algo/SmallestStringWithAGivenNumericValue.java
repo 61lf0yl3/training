0480d46dd907abe913780371314eb11e4e4d7b7a
61lf0yl3 <samigull@usc.edu>
2022-05-11 20:38:16 -0400

    SmallestStringWithAGivenNumericValue

--- algo/SmallestStringWithAGivenNumericValue.java
+++ algo/SmallestStringWithAGivenNumericValue.java
@@ -1,4 +1,9 @@
 class SmallestStringWithAGivenNumericValue {
+    // 1663. Smallest String With A Given Numeric Value
+
+    // Approach 1: Build number from left to right
+    // Time Complexity: O(N)
+    // Space Complexity: O(1)
     public String getSmallestString(int n, int k) {
         char[] chars = new char[n];
         for (int i = n - 1; i >= 0; i--) {
