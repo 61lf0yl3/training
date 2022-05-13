4a68d0a462ad2e922dc2d23b9c36bf0c71f3385b
61lf0yl3 <samigull@usc.edu>
2022-05-13 00:47:50 -0400

    FindPeakElement

--- algo/FindPeakElement.java
+++ algo/FindPeakElement.java
@@ -35,4 +35,29 @@ class FindPeakElement {
             return search(nums, l, mid);
         return search(nums, mid + 1, r);
     }
+
+    public int findPeakElement3(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            long left = (long) nums[mid] - 1;
+            if (mid > 0) {
+                left = nums[mid - 1];
+            }
+            long right = (long) nums[mid] + 1;
+            if (mid < nums.length - 1) {
+                right = nums[mid + 1];
+            }
+            if (left < nums[mid] && right < nums[mid]) {
+                return mid;
+            } else if (left < right) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
 }
