24b12c545c9abd2cab010931a0fcc86a4632cc10
61lf0yl3 <samigull@usc.edu>
2022-05-15 00:46:37 -0400

    isPalindrome2

--- algo/ValidPalindrome.java
+++ algo/ValidPalindrome.java
@@ -21,4 +21,31 @@ class ValidPalindrome {
         }
         return true;
     }
+
+    public boolean isPalindrome2(String s) {
+        StringBuilder builder = new StringBuilder();
+
+        for (char ch : s.toCharArray()) {
+            if (Character.isLetterOrDigit(ch)) {
+                builder.append(Character.toLowerCase(ch));
+            }
+        }
+
+        String filteredString = builder.toString();
+        String reversedString = builder.reverse().toString();
+
+        return filteredString.equals(reversedString);
+    }
+
+    /** An alternate solution using Java 8 Streams */
+    public boolean isPalindromeUsingStreams(String s) {
+        StringBuilder builder = new StringBuilder();
+
+        s.chars()
+                .filter(c -> Character.isLetterOrDigit(c))
+                .mapToObj(c -> Character.toLowerCase((char) c))
+                .forEach(builder::append);
+
+        return builder.toString().equals(builder.reverse().toString());
+    }
 }
