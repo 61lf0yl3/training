264a987d7a8281cd02b12f8a382afb7d9d3b20db
61lf0yl3 <samigull@usc.edu>
2022-05-14 22:56:07 -0400

    maxDistToClosest

--- algo/MaximizeDistancetoClosestPerson.java
+++ algo/MaximizeDistancetoClosestPerson.java
@@ -1,35 +1,34 @@
 public class MaximizeDistancetoClosestPerson {
+    // 849. Maximize Distance to Closest Person
+
     // Approach 1: Next Array
     // Time Complexity: O(N)
     // Space Complexity: O(N)
     public int maxDistToClosest(int[] seats) {
         int n = seats.length;
         int[] left = new int[n];
-        int[] right = new int[n];
-        for (int i = 0; i < n; i++) {
+        if (seats[0] == 0) {
+            left[0] = n;
+        }
+        for (int i = 1; i < n; i++) {
             if (seats[i] == 1) {
                 left[i] = 0;
             } else {
-                if (i > 0) {
-                    left[i] = left[i - 1] + 1;
-                } else {
-                    left[i] = n;
-                }
+                left[i] = left[i - 1] + 1;
             }
         }
 
-        for (int i = n - 1; i >= 0; i--) {
+        int[] right = new int[n];
+        if (seats[n - 1] == 0) {
+            right[n - 1] = n;
+        }
+        for (int i = n - 2; i >= 0; i--) {
             if (seats[i] == 1) {
                 right[i] = 0;
             } else {
-                if (i < n - 1) {
-                    right[i] = right[i + 1] + 1;
-                } else {
-                    right[i] = n;
-                }
+                right[i] = right[i + 1] + 1;
             }
         }
-
         int res = 0;
         for (int i = 0; i < n; i++) {
             res = Math.max(res, Math.min(left[i], right[i]));
