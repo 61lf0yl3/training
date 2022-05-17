21975e56a0b9dd18c252e9f07ee2ed24c615739c
61lf0yl3 <samigull@usc.edu>
2022-05-16 23:41:20 -0400

    ThreeSumWithMultiplicity

--- algo/3SumWithMultiplicity.java
+++ algo/3SumWithMultiplicity.java
@@ -44,7 +44,7 @@ class ThreeSumWithMultiplicity {
         return res;
     }
 
-    public int threeSumMulti(int[] A, int target) {
+    public int threeSumMulti2(int[] A, int target) {
         int MOD = 1_000_000_007;
         long[] count = new long[101];
         for (int x : A)
@@ -88,6 +88,60 @@ class ThreeSumWithMultiplicity {
             if (0 <= x && x <= 100) {
                 ans += count[x] * (count[x] - 1) * (count[x] - 2) / 6;
                 ans %= MOD;
+            }
+        }
+
+        return (int) ans;
+    }
+
+    public int threeSumMulti3(int[] A, int target) {
+        int MOD = 1_000_000_007;
+
+        // Initializing as long saves us the trouble of
+        // managing count[x] * count[y] * count[z] overflowing later.
+        long[] count = new long[101];
+        int uniq = 0;
+        for (int x : A) {
+            count[x]++;
+            if (count[x] == 1)
+                uniq++;
+        }
+
+        int[] keys = new int[uniq];
+        int t = 0;
+        for (int i = 0; i <= 100; ++i)
+            if (count[i] > 0)
+                keys[t++] = i;
+
+        long ans = 0;
+        // Now, let's do a 3sum on "keys", for i <= j <= k.
+        // We will use count to add the correct contribution to ans.
+
+        for (int i = 0; i < keys.length; ++i) {
+            int x = keys[i];
+            int T = target - x;
+            int j = i, k = keys.length - 1;
+            while (j <= k) {
+                int y = keys[j], z = keys[k];
+                if (y + z < T) {
+                    j++;
+                } else if (y + z > T) {
+                    k--;
+                } else { // # x+y+z == T, now calc the size of the contribution
+                    if (i < j && j < k) {
+                        ans += count[x] * count[y] * count[z];
+                    } else if (i == j && j < k) {
+                        ans += count[x] * (count[x] - 1) / 2 * count[z];
+                    } else if (i < j && j == k) {
+                        ans += count[x] * count[y] * (count[y] - 1) / 2;
+                    } else { // i == j == k
+                        ans += count[x] * (count[x] - 1) * (count[x] - 2) / 6;
+                    }
+
+                    ans %= MOD;
+                    j++;
+                    k--;
+                }
             }
         }
 
