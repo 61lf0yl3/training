18a0beb2a61e6f903c93bb2f24c6596ce1e4d3c6
61lf0yl3 <samigull@usc.edu>
2022-05-17 02:56:47 -0400

    RemoveCoveredIntervals

--- algo/RemoveCoveredIntervals.java
+++ algo/RemoveCoveredIntervals.java
@@ -1,6 +1,11 @@
 import java.util.Arrays;
 
 class RemoveCoveredIntervals {
+    // 1288. Remove Covered Intervals
+
+    // Approach 1: Greedy
+    // Time Complexity: O(NlogN)
+    // Space Complexity: O(logN)
     public int removeCoveredIntervals(int[][] intervals) {
         Arrays.sort(intervals, (int[] a, int[] b) -> {
             return a[0] == b[0] ? b[1] - a[1] : a[0] - b[0];
