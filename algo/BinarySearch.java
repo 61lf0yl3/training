5295d70637c210fbb6ea64ef36494acb266a8d3d
61lf0yl3 <samigull@usc.edu>
2022-05-21 23:40:36 -0400

    search

---
+++ algo/BinarySearch.java
@@ -0,0 +1,18 @@
+class Solution {
+
+    public int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return -1;
+    }
+}
