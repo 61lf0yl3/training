0392738a35b989a511f793008f497d0fbef189b4
61lf0yl3 <samigull@usc.edu>
2022-06-14 00:32:50 -0400

    angleClock2

--- algo/AngleBetweenHandsofaClock.java
+++ algo/AngleBetweenHandsofaClock.java
@@ -8,4 +8,11 @@ public class AngleBetweenHandsofaClock {
         double diff = Math.abs(hourAngle - minutesAngle);
         return Math.min(diff, 360 - diff);
     }
+
+    public double angleClock2(int hour, int minutes) {
+        double hourHand = (hour % 12) * 30 + minutes / 2.0;
+        double minutesHand = minutes * 6.0;
+        return Math.min(Math.max(hourHand, minutesHand) - Math.min(hourHand, minutesHand),
+                360.0 - Math.max(hourHand, minutesHand) + Math.min(hourHand, minutesHand));
+    }
 }
