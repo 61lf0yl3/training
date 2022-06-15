7b2796d97fe8a7cd16b1f92d2654ff7a20f45213
61lf0yl3 <samigull@usc.edu>
2022-06-15 02:58:30 -0400

    MinimumDominoRotationsForEqualRow

--- algo/MinimumDominoRotationsForEqualRow.java
+++ algo/MinimumDominoRotationsForEqualRow.java
@@ -27,4 +27,47 @@ class MinimumDominoRotationsForEqualRow {
         }
         return Math.min(swapsTops, swapsBottoms);
     }
+
+    public int minDominoRotations(int[] tops, int[] bottoms) {
+        int swapsTops = checkTops(tops, bottoms, tops[0]);
+        int swapsBottoms = checkBottoms(tops, bottoms, bottoms[0]);
+        if (swapsTops == -1 && swapsBottoms == -1) {
+            return -1;
+        } else if (swapsTops != -1 && swapsBottoms != -1) {
+            return Math.min(swapsTops, swapsBottoms);
+        } else if (swapsTops != -1) {
+            return swapsTops;
+        } else {
+            return swapsBottoms;
+        }
+    }
+
+    private int checkTops(int[] tops, int[] bottoms, int target) {
+        int swaps = 0;
+        for (int i = 1; i < tops.length; i++) {
+            if (tops[i] == target) {
+                continue;
+            } else if (bottoms[i] == target) {
+                swaps++;
+            } else {
+                return -1;
+            }
+        }
+        return Math.min(swaps, tops.length - swaps);
+    }
+
+    // Smth wrong
+    private int checkBottoms(int[] tops, int[] bottoms, int target) {
+        int swaps = 0;
+        for (int i = 1; i < bottoms.length; i++) {
+            if (bottoms[i] == target) {
+                continue;
+            } else if (tops[i] == target) {
+                swaps++;
+            } else {
+                return -1;
+            }
+        }
+        return Math.min(swaps, bottoms.length - swaps);
+    }
 }
