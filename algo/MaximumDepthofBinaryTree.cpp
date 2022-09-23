2efa6636d908374f86b3f1e939ec1d5daf9cc58a
61lf0yl3 <samigull@usc.edu>
2022-09-22 22:04:41 -0700

    MaximumDepthofBinaryTree.cpp

---
+++ algo/MaximumDepthofBinaryTree.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    int maxDepth(TreeNode *root)
+    {
+        if (root == NULL)
+        {
+            return 0;
+        }
+        queue<TreeNode *> q;
+        q.push(root);
+        int res = 0;
+        while (!q.empty())
+        {
+            int n = q.size();
+            res++;
+            for (int i = 0; i < n; i++)
+            {
+                TreeNode *node = q.front();
+                q.pop();
+                if (node->left != NULL)
+                {
+                    q.push(node->left);
+                }
+                if (node->right != NULL)
+                {
+                    q.push(node->right);
+                }
+            }
+        }
+        return res;
+    }
+};
