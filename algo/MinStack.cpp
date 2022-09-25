1b5425d814c76d10212c0a5b04fe5c30f616d747
61lf0yl3 <samigull@usc.edu>
2022-09-24 18:56:04 -0700

    MinStack.cpp

--- algo/MinStack.cpp
+++ algo/MinStack.cpp
@@ -36,3 +36,12 @@ private:
     // val and min;
     stack<pair<int, int> > stack;
 };
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
