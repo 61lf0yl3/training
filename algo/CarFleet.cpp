86cca920be3e5a600e8fe21b1cb68f14f83c3ea1
61lf0yl3 <samigull@usc.edu>
2022-10-04 14:29:58 -0700

    CarFleet

--- algo/CarFleet.cpp
+++ algo/CarFleet.cpp
@@ -20,4 +20,33 @@ public:
         }
         return stack.size();
     }
+
+public:
+    int carFleet(int target, vector<int> &position, vector<int> &speed)
+    {
+        int n = position.size();
+
+        vector<pair<int, double> > cars;
+        for (int i = 0; i < n; i++)
+        {
+            double time = (double)(target - position[i]) / speed[i];
+            cars.push_back({position[i], time});
+        }
+        sort(cars.begin(), cars.end());
+
+        double maxTime = 0.0;
+        int result = 0;
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+            double time = cars[i].second;
+            if (time > maxTime)
+            {
+                maxTime = time;
+                result++;
+            }
+        }
+
+        return result;
+    }
 };
