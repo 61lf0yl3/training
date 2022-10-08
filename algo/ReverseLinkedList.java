225be18d06d8f0d57f0efef35321432c820bec69
61lf0yl3 <samigull@usc.edu>
2022-10-08 16:46:46 -0700

    ReverseLinkedList.java

--- algo/ReverseLinkedList.java
+++ algo/ReverseLinkedList.java
@@ -26,5 +26,25 @@ public:
         }
         return prev;
     }
+
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (head == NULL || head->next == NULL) {
+            return head;
+        }
+        
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* next = curr->next;
+        
+        while (curr != NULL) {
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        
+        return prev;
+    }
 }
 ;
