a55e2a7a22923fd96023cefffa722350178f94e6
61lf0yl3 <samigull@usc.edu>
2022-10-12 23:17:07 -0700

    BinaryTreeLevelOrderTraversal.cpp

--- algo/BinaryTreeLevelOrderTraversal.cpp
+++ algo/BinaryTreeLevelOrderTraversal.cpp
@@ -19,6 +19,32 @@ public:
         dfs(root, 0);
         return res;
     }
+
+    vector<vector<int>> levelOrder2(TreeNode* root) {
+        if (root == NULL) {
+            return {};
+        }
+        vector<vector<int>> res;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()) {
+            int size = q.size();
+            vector<int> level;
+            for (int i = 0; i< size; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+                level.push_back(node->val);
+                if (node->left != NULL) {
+                    q.push(node->left);
+                }
+                if (node->right != NULL) {
+                    q.push(node->right);
+                }
+            }
+            res.push_back(level);
+        }
+        return res;
+    }
     
 private:
     void dfs(TreeNode* node, int level) {
