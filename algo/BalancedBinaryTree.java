dec7662a136f7af487548a0bdcd1be044be3a42b
61lf0yl3 <samigull@usc.edu>
2022-10-28 23:23:57 -0700

    BalancedBinaryTree.java

---
+++ algo/BalancedBinaryTree.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    boolean res;
+    public boolean isBalanced(TreeNode root) {
+        res = true;
+        dfs(root);
+        return res;
+    }
+    
+    private int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+        if (Math.abs(left-right) > 1) {
+            res = false;
+        }
+        return Math.max(left, right)+1;
+    }
+}
